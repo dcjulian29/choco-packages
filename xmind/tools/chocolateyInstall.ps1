d7b87ad7e8db81455479fd3ce45b1f5970a983f8
Julian Easterling <julian@julianscorner.com>
2013-11-29 15:17:43 -0500

    added install script

---
+++ xmind/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = "xmind" # arbitrary name for the package, used in messages
+$installerType = "exe" #only one of these two: exe or msi
+$installerArgs = "/SILENT"
+$url = "http://www.xmind.net/xmind/downloads/xmind-windows-3.4.0.201311050558.exe"
+$url64 = $url # 64bit URL here or just use the same as $url
+
+try
+{
+    Install-ChocolateyPackage $packageName $installerType $params $url
+
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
