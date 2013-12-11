36774ad22bf630765b52798d2914b3da79628d71
Julian Easterling <julian@julianscorner.com>
2013-12-11 12:11:40 -0500

    Added sysinternals package

---
+++ sysinternals/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = 'sysinternals'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$appDir = "$(Split-Path -parent $toolsDir)\app"
+$url = 'http://download.sysinternals.com/files/SysinternalsSuite.zip'
+
+try
+{
+    if (Test-Path $appDir)
+    {
+      Write-Output "Removing previous version of package..."
+      Remove-Item "$($appDir)\*" -Recurse -Force
+    }
+
+    Install-ChocolateyZipPackage $packageName $url $appDir
+
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
---
+++ sysinternals/tools/chocolateyUninstall.ps1
@@ -0,0 +1,18 @@
+﻿$packageName = 'sysinternals'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$appDir = "$(Split-Path -parent $toolsDir)\app"
+
+try
+{
+    if (Test-Path $appDir)
+    {
+      Remove-Item "$($appDir)\*" -Recurse -Force
+    }
+
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
