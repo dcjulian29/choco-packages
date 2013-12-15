f7a0fa3ba9606380004693067c12deb2bae7b230
Julian Easterling <julian@julianscorner.com>
2013-12-15 01:24:06 -0500

    added a package to install Google Chrome web browser.

---
+++ googlechrome/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = "googlechrome" # arbitrary name for the package, used in messages
+$installerType = "exe" #only one of these two: exe or msi
+$installerArgs = "/silent /install"
+$url = "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B5467B535-A428-DC7C-783D-547BE6311078%7D%26lang%3Den%26browser%3D3%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers/update2/installers/ChromeStandaloneSetup.exe"
+$url64 = $url
+
+try
+{
+    Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
+
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
