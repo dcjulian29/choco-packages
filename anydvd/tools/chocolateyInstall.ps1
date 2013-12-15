4190b7787141b8d291d8b7dd22c74735d76cbc80
Julian Easterling <julian@julianscorner.com>
2013-12-15 03:47:28 -0500

    Added AnyDVD Package

---
+++ anydvd/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = "anydvd"
+$installerType = "exe"
+$installerArgs = "/S"
+$url = "http://static.slysoft.com/SetupAnyDVD7380.exe"
+$url64 = $url
+
+try
+{
+    Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
+
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
