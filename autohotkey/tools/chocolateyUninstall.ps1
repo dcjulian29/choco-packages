c37fce35e308add61a2fdc970acee4facb9d1aba
Julian Easterling <julian@julianscorner.com>
2013-12-15 01:23:39 -0500

    Added AutoHotkey tool package which is used by some of the other installers and tools

---
+++ autohotkey/tools/chocolateyUninstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'autohotkey'
+$appDir = "$($env:ChocolateyInstall)\apps\$($packageName)"
+
+try
+{
+    if (Test-Path $appDir)
+    {
+      Remove-Item "$($appDir)\*" -Recurse -Force
+    }
+
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
