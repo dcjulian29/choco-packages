488373eb465ebadad5aeac9e82564c762db1aaf2
Julian Easterling <julian@julianscorner.com>
2013-12-15 00:39:18 -0500

    Added WinPcap package along with an AHK script to automate the install and uninstall since the "owner" and/or "developer(s)" have decided that you must "buy" what seems to be a no longer available "pro" version instead of simply providing the /S switch that they removed from the installer to prevent "abuse". shame, shame...

---
+++ winpcap/tools/chocolateyInstall.ps1
@@ -0,0 +1,33 @@
+﻿$packageName = "winpcap" # arbitrary name for the package, used in messages
+$url = "http://www.winpcap.org/install/bin/WinPcap_4_1_3.exe"
+$toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
+$downloadPath = "$env:TEMP\chocolatey\winpcap"
+$installer = "$downloadPath\winpcapInstall.exe"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+    $ErrorActionPreference = "Stop"
+}
+
+try
+{
+    if (!(Test-Path $downloadPath)) {
+        New-Item -ItemType directory $downloadPath -Force
+    }
+
+    Get-ChocolateyWebFile $packageName $installer $url
+
+    if (Test-Path "$env:ChocolateyInstall\apps\autohotkey\AutoHotkey.exe") {
+        $ahkExe = "$env:ChocolateyInstall\apps\autohotkey\AutoHotkey.exe"
+        $ahkScript = "$toolDir\install.ahk"
+
+        Start-ChocolateyProcessAsAdmin "$ahkExe $ahkScript" -noSleep
+    }
+
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
