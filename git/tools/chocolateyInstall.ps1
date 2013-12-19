07ebdc019afd4fe8d28d978d462ea7bd9442a65a
Julian Easterling <julian@julianscorner.com>
2013-12-19 10:34:48 -0500

    Added Git SCM application package

---
+++ git/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+$packageName = "git"
+$installerType = "EXE"
+$installerArgs = '/SILENT /COMPONENTS="!ext,!ext\cheetah,!assoc,!assoc_sh"'
+$url = "https://msysgit.googlecode.com/files/Git-1.8.4-preview20130916.exe"
+
+$appDir = "$($env:ChocolateyInstall)\apps\$($packageName)"
+$toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+    $ErrorActionPreference = "Stop"
+}
+
+try
+{
+    Install-ChocolateyPackage $packageName $installerType $installerArgs $url
+
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
