b94abf955705949ab7513d9e8ad6adf614a6eb94
Julian Easterling <julian@julianscorner.com>
2013-12-19 10:28:32 -0500

    Added CrashPlan application package

---
+++ crashplan/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+$packageName = "crashplan"
+$installerType = "EXE"
+$installerArgs = "/quiet /norestart"
+$url = "http://download2.us.code42.com/installs/win/install/CrashPlan/jre/CrashPlan_3.5.3_Win.exe"
+$url64 = "http://download2.us.code42.com/installs/win/install/CrashPlan/jre/CrashPlan-x64_3.5.3_Win.exe"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+    $ErrorActionPreference = "Stop"
+}
+
+try
+{
+    Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
+
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
