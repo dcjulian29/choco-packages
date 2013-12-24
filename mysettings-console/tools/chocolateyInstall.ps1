760fb563f95f731a33d517f78baa0debf004abc1
Julian Easterling <julian@julianscorner.com>
2013-12-24 08:07:49 -0500

    Added a package to configure my console configuration such as fonts, colors, and sizes.

---
+++ mysettings-console/tools/chocolateyInstall.ps1
@@ -0,0 +1,25 @@
+$packageName = "mysettings-console"
+$toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+    $ErrorActionPreference = "Stop"
+}
+
+try
+{
+    $cmd = "$env:WINDIR\system32\reg.exe import $toolDir\registry.reg"
+    
+    if (Get-ProcessorBits -eq 64) {
+        $cmd = "$cmd /reg:64"
+    }
+    
+    cmd /c "$cmd"
+    
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
