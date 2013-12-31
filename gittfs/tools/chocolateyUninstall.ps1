76a334b9d51719d874c123d1a8d73b847e1cd470
Julian Easterling <julian@julianscorner.com>
2013-12-31 12:05:07 -0500

    Added a package to install git-tfs bridge binaries

---
+++ gittfs/tools/chocolateyUninstall.ps1
@@ -0,0 +1,18 @@
+$packageName = "gittfs"
+$packageWildCard = "*$($package)*";
+$appDir = "$($env:ChocolateyInstall)\apps\$($packageName)"
+
+try
+{
+    if (Test-Path $appDir)
+    {
+      Remove-Item "$($appDir)" -Recurse -Force
+    }
+    
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
