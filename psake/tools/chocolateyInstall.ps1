8bbf232c4fee99c603b58acaa4b32eed7cfafe1e
Julian Easterling <julian@julianscorner.com>
2014-01-02 14:26:47 -0500

    Added psake powershell module package

---
+++ psake/tools/chocolateyInstall.ps1
@@ -0,0 +1,33 @@
+$packageName = "psake"
+$release = "4.3.1"
+$url = "https://github.com/psake/psake/archive/v$($release).zip"
+$appDir = "$($env:UserProfile)\Documents\WindowsPowerShell\Modules\$packageName"
+$downloadPath = "$env:TEMP\chocolatey\$packageName"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+    $ErrorActionPreference = "Stop"
+}
+
+try
+{
+    if (Test-Path $appDir)
+    {
+      Write-Output "Removing previous version of package..."
+      Remove-Item "$($appDir)" -Recurse -Force
+    }
+
+    Get-ChocolateyWebFile $packageName "$downloadPath\v$release.zip" $url
+    Get-ChocolateyUnzip "$downloadPath\v$release.zip" "$downloadPath\"
+
+    mkdir $appDir
+
+    Copy-Item -Path "$downloadPath\$packageName-$release\*" -Destination "$appDir"
+
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
