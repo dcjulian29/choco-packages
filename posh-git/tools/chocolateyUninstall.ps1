58e16e0ef58c77b99dbfa0e98418dfcd67dabb01
Julian Easterling <julian@julianscorner.com>
2014-01-02 14:35:31 -0500

    Added posh-git powershell module package

---
+++ posh-git/tools/chocolateyUninstall.ps1
@@ -0,0 +1,17 @@
+$packageName = "posh-git"
+$appDir = "$($env:UserProfile)\Documents\WindowsPowerShell\Modules\$($packageName)"
+
+try
+{
+    if (Test-Path $appDir)
+    {
+      Remove-Item "$($appDir)" -Recurse -Force
+    }
+
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
