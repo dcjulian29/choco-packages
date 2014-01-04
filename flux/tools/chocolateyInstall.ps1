0b80091cf07aa12c4b2ebf89d4aa19170a49e1f6
Julian Easterling <julian@julianscorner.com>
2014-01-04 11:18:03 -0500

    Added a package for f.lux application

---
+++ flux/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+$packageName = "flux"
+$installerType = "EXE"
+$installerArgs = "/S"
+$url = "http://update.justgetflux.com/flux-setup.exe"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+    $ErrorActionPreference = "Stop"
+}
+
+try
+{
+    Install-ChocolateyPackage $packageName $installerType $installerArgs $url
+
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
