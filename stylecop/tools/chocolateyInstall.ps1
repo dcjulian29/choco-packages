1e4dfdad9450c900ae26d1431b35e9eb025f8597
Julian Easterling <julian@julianscorner.com>
2014-01-09 09:13:10 -0500

    Added a package to install StyleCop

---
+++ stylecop/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+$packageName = "stylecop"
+$installerType = "MSI"
+$installerArgs = "/qb"
+$url = "http://stylecop.codeplex.com/downloads/get/323236"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+    $ErrorActionPreference = "Stop"
+}
+
+try
+{
+    Install-ChocolateyPackage $packageName $installerType $installerArgs $url
+
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
