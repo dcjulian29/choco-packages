682dd789b4d4ee2728b1b796d904c18b50ef173d
unknown <julian@JULIAN.JNET.local>
2014-01-22 16:20:13 -0500

    updated to latest version 1.0.17 and add the "Reboot Needed" exit code as a valid exit code

--- npp-scriptcs/tools/chocolateyInstall.ps1
+++ npp-scriptcs/tools/chocolateyInstall.ps1
@@ -1,12 +1,13 @@
 ﻿$packageName = "npp-scriptcs" # arbitrary name for the package, used in messages
 $installerType = "msi" #only one of these two: exe or msi
 $installerArgs = "/q /NORESTART"
-$url = "https://csscriptnpp.codeplex.com/downloads/get/774670"
+$url = "https://csscriptnpp.codeplex.com/downloads/get/781204"
 $url64 = $url # 64bit URL here or just use the same as $url
+$validExitCodes = @(0,3010)
 
 try
 {
-    Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
+    Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64 -validExitCodes $validExitCodes
 
     Write-ChocolateySuccess $packageName
 }
