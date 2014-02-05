db99de809675b26bed40176cc87acd36a43ecfc8
Julian Easterling <julian@julianscorner.com>
2014-02-05 12:37:54 -0500

    Added package for network scanner tool

---
+++ netscan/tools/chocolateyInstall.ps1
@@ -0,0 +1,47 @@
+$packageName = "netscan"
+$url = "http://www.softperfect.com/download/freeware/netscan.zip"
+$downloadPath = "$env:TEMP\chocolatey\$packageName"
+$appDir = "$($env:ChocolateyInstall)\apps\$($packageName)"
+$toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+    $ErrorActionPreference = "Stop"
+}
+
+try
+{
+    if (Test-Path $appDir)
+    {
+        Write-Output "Removing previous version of package..."
+        Remove-Item "$($appDir)" -Recurse -Force
+    }
+
+    mkdir $appDir | Out-Null
+    
+    if (-not (Test-Path $downloadPath))
+    {
+        mkdir $downloadPath | Out-Null
+    }
+
+    Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url
+
+    if (Test-Path "${env:ProgramFiles}") {
+        $bits = "32-Bit"
+    }
+
+    if (Test-Path "${env:ProgramFiles(x86)}") {
+        $bits = "64-Bit"
+    }
+    
+    Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\"
+
+    Get-ChildItem -Path $downloadPath\$bits -Include $keep -Recurse | Copy-Item -Destination "$appDir"
+
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
