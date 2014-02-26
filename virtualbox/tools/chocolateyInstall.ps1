106dfc145edbea32bc851ac2692c6d489434f54d
Julian Easterling <julian@julianscorner.com>
2014-02-25 20:35:52 -0500

    updated to 4.3.8

--- virtualbox/tools/chocolateyInstall.ps1
+++ virtualbox/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = "virtualbox"
 $installerType = "EXE"
 $installerArgs = "--silent"
-$url = "http://download.virtualbox.org/virtualbox/4.3.6/VirtualBox-4.3.6-91406-Win.exe"
+$url = "http://download.virtualbox.org/virtualbox/4.3.8/VirtualBox-4.3.8-92456-Win.exe"
 
 try
 {
