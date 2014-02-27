956294ee99e861d336c9b7f49fd7e7b6d37aafab
Julian Easterling <julian@julianscorner.com>
2014-02-27 13:03:34 -0500

    Added a package to install 7zip

---
+++ 7zip/tools/chocolateyInstall.ps1
@@ -0,0 +1,31 @@
+$packageName = "7zip"
+$installerType = "MSI"
+$installerArgs = "/quiet"
+$url = "http://downloads.sourceforge.net/sevenzip/7z920.msi"
+$url64 = "http://downloads.sourceforge.net/sevenzip/7z920-x64.msi"
+$toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+    $ErrorActionPreference = "Stop"
+}
+
+try
+{
+    Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
+
+    $cmd = "reg.exe import $toolDir\registry.reg"
+    
+    if (Get-ProcessorBits -eq 64) {
+        $cmd = "$cmd /reg:64"
+    }
+
+    Start-ChocolateyProcessAsAdmin "$cmd"
+
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
