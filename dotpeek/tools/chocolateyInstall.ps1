edd1e69a9f14ea7c74632e35b6310dac9f816aba
Julian Easterling <julian@julianscorner.com>
2014-04-12 15:06:05 -0400

    Added dotPeek decompiler application.

---
+++ dotpeek/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+$packageName = "dotpeek"
+$installerType = "MSI"
+$installerArgs = "/qn"
+$url = "http://download.jetbrains.com/dotpeek/dotPeekSetup-1.1.1.33.msi"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+    $ErrorActionPreference = "Stop"
+}
+
+try
+{
+    Install-ChocolateyPackage $packageName $installerType $installerArgs $url
+
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
