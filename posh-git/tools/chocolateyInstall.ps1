298945db0aa5447d32cb29e03e80b2457f58c4f5
Julian Easterling <julian@julianscorner.com>
2014-04-29 18:00:13 -0400

    Updated install script for posh-git

--- posh-git/tools/chocolateyInstall.ps1
+++ posh-git/tools/chocolateyInstall.ps1
@@ -19,13 +19,13 @@ try
 
     if (-not (Test-Path $downloadPath))
     {
-        mkdir $downloadPath
+        New-Item -Type Directory -Path $downloadPath | Out-Null
     }
 
     Get-ChocolateyWebFile $packageName "$downloadPath\v$release.zip" $url
     Get-ChocolateyUnzip "$downloadPath\v$release.zip" "$downloadPath\"
 
-    mkdir $appDir
+    New-Item -Type Directory -Path $appDir | Out-Null
 
     Copy-Item -Path "$downloadPath\$packageName-$release\*" -Destination "$appDir"
 
