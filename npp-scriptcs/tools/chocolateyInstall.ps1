5f8ff90a089c7ed0504d97ea580db1db84037d05
Julian Easterling <julian@julianscorner.com>
2014-05-09 18:20:44 -0400

    Updated npp-cscriptcs to 1.0.24...

--- npp-scriptcs/tools/chocolateyInstall.ps1
+++ npp-scriptcs/tools/chocolateyInstall.ps1
@@ -1,12 +1,44 @@
 ﻿$packageName = "npp-scriptcs"
-$installerType = "msi"
-$installerArgs = "/q /NORESTART"
-$url = "http://csscriptnpp.codeplex.com/downloads/get/834618"
-$validExitCodes = @(0,3010)
+$downloadPath = "$env:TEMP\chocolatey\$packageName"
+$url = "http://csscriptnpp.codeplex.com/downloads/get/835038"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+    $ErrorActionPreference = "Stop"
+}
 
 try
 {
-    Install-ChocolateyPackage $packageName $installerType $installerArgs $url -validExitCodes $validExitCodes
+    if (Test-Path "C:\Program Files (x86)\Notepad++")
+    {
+        $plugin = "C:\Program Files (x86)\Notepad++\plugins"
+    }
+    
+    if (Test-Path "C:\Program Files\Notepad++")
+    {
+        $plugin = "C:\Program Files\Notepad++\plugins"
+    }
+
+    if (Test-Path "$($plugin)\CSScriptNpp")
+    {
+        Remove-Item "$($plugin)\CSScriptNpp" -Recurse -Force
+        Remove-Item "$($plugin)\CSScriptNpp.dll" -Force
+    }
+
+    if (-not (Test-Path $downloadPath)) {
+        New-Item -Type Directory -Path $downloadPath | Out-Null
+    }
+
+    Get-ChocolateyWebFile $packageName "$($downloadPath)\$($packagename).zip" $url
+    Get-ChocolateyUnzip "$($downloadPath)\$($packagename).zip" "$($downloadPath)\"
+
+    if (-not (Test-Path $($plugin))) {
+        New-Item -Type Directory -Path $plugin | Out-Null
+    }
+
+    $cmd = "Copy-Item -Path '$($downloadPath)\Plugins\*' -Recurse -Destination '$plugin\' -Container"
+
+    Start-ChocolateyProcessAsAdmin $cmd
 
     Write-ChocolateySuccess $packageName
 }
---
+++ npp-scriptcs/tools/chocolateyUninstall.ps1
@@ -0,0 +1,33 @@
+$packageName = "npp-scriptcs"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+    $ErrorActionPreference = "Stop"
+}
+
+try
+{
+    if (Test-Path "C:\Program Files (x86)\Notepad++")
+    {
+        $plugin = "C:\Program Files (x86)\Notepad++\plugins"
+    }
+    
+    if (Test-Path "C:\Program Files\Notepad++")
+    {
+        $plugin = "C:\Program Files\Notepad++\plugins"
+    }
+
+    if (Test-Path "$($plugin)\CSScriptNpp")
+    {
+        $cmd = "Remove-Item '$($plugin)\CSScriptNpp' -Recurse -Force;Remove-Item '$($plugin)\CSScriptNpp.dll' -Force"
+
+        Start-ChocolateyProcessAsAdmin $cmd
+    }
+
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
