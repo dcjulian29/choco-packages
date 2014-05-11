e8921ca8ba74b559d472dc6d5ae11c8614819020
Julian Easterling <julian@julianscorner.com>
2014-05-11 16:13:30 -0400

    Exit Code 3010 is ok as a exit code

--- dotnet/tools/chocolateyInstall.ps1
+++ dotnet/tools/chocolateyInstall.ps1
@@ -2,6 +2,7 @@ $packageName = "dotnet"
 $installerType = "EXE"
 $installerArgs = "/passive /norestart"
 $url = "http://download.microsoft.com/download/E/2/1/E21644B5-2DF2-47C2-91BD-63C560427900/NDP452-KB2901907-x86-x64-AllOS-ENU.exe"
+$errorCode = @(0, 3010)
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
@@ -10,7 +11,8 @@ if ($psISE) {
 
 try
 {
-    Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url
+    Install-ChocolateyPackage $packageName $installerType $installerArgs $url `
+        -ValidExitCodes $errorCode
 
     Write-ChocolateySuccess $packageName
 }
