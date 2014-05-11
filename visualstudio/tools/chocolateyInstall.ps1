bd822bd0ed20c6acfa74827e7685f9366dac4736
Julian Easterling <julian@julianscorner.com>
2014-05-11 11:10:38 -0400

    Changed the Visual Studio package to be a "virtual" package that other package can depend on regardless of if Premium or Ultimate is installed.

--- visualstudio/tools/chocolateyInstall.ps1
+++ visualstudio/tools/chocolateyInstall.ps1
@@ -1,8 +1,4 @@
 $packageName = "visualstudio"
-$installerType = "EXE"
-$installerArgs = "/PASSIVE /NORESTART"
-$url = "http://download.microsoft.com/download/D/B/D/DBDEE6BB-AF28-4C76-A5F8-710F610615F7/vs_premium.exe"
-$update = "http://download.microsoft.com/download/8/2/6/826E264A-729E-414A-9E67-729923083310/VSU1/VS2013.1.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
@@ -11,12 +7,9 @@ if ($psISE) {
 
 try
 {
-    if (-not (Test-Path "HKLM:\SOFTWARE\Microsoft\DevDiv\vs\Servicing\12.0")) {
-        Install-ChocolateyPackage $packageName $installerType $installerArgs $url
-    }
-    
-    Install-ChocolateyPackage $packageName $installerType $installerArgs $update
-    
+    Write-Output "Visual Studio Virtual Package."
+    Write-Output "Packages depend on this package instead of the specific 'edition' package."
+
     Write-ChocolateySuccess $packageName
 }
 catch
