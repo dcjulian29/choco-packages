ff841c3f9c44fbc6131e7175012d2259bc7363f4
Julian Easterling <julian@julianscorner.com>
2014-05-16 09:20:11 -0400

    Updated anydvd to 7.4.7.0

--- anydvd/tools/chocolateyInstall.ps1
+++ anydvd/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = "anydvd"
 $installerType = "exe"
 $installerArgs = "/S"
-$url = "http://static.slysoft.com/SetupAnyDVD7460.exe"
+$url = "http://static.slysoft.com/SetupAnyDVD7470.exe"
 
 try
 {
