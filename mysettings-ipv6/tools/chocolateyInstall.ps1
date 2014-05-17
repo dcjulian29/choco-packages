a63617e407239e51932ebeea2c56c16d6606f098
Julian Easterling <julian@julianscorner.com>
2014-05-17 15:29:10 -0400

    Changed "IPv6" settings to one "MySettings" package

--- mysettings-ipv6/tools/chocolateyInstall.ps1
+++ mysettings-ipv6/tools/chocolateyInstall.ps1
@@ -1,14 +1,13 @@
-$packageName = "ipv6-disabletunnels"
-$toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
+$packageName = "mysettings-ipv6"
+$packageDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
-    $ErrorActionPreference = "Stop"
 }
 
 try
 {
-    $cmd = "reg.exe import $toolDir\registry.reg"
+    $cmd = "reg.exe import $packageDir\registry.reg"
     
     if (Get-ProcessorBits -eq 64) {
         $cmd = "$cmd /reg:64"
