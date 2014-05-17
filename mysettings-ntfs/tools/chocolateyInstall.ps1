24a77ce974e8f402eb6e10d1181e8e49a9104edd
Julian Easterling <julian@julianscorner.com>
2014-05-17 15:34:43 -0400

    Changed the "NTFS" package to a "MySettings" package and added a few other file system tweaks.

--- mysettings-ntfs/tools/chocolateyInstall.ps1
+++ mysettings-ntfs/tools/chocolateyInstall.ps1
@@ -1,14 +1,13 @@
-$packageName = "ntfs-disable-lastaccesstime"
-$toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
+$packageName = "mysettings-ntfs"
+$packageDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
-    $ErrorActionPreference = "Stop"
 }
 
 try
 {
-    $cmd = "reg.exe import $toolDir\registry.reg"
+    $cmd = "reg.exe import $packageDir\registry.reg"
     
     if (Get-ProcessorBits -eq 64) {
         $cmd = "$cmd /reg:64"
