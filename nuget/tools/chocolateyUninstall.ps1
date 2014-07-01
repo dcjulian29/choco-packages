a9f9e1a0ec533ca0dfb11396127e3010857d95b2
Julian Easterling <julian@julianscorner.com>
2014-07-01 09:38:04 -0400

    Updated NuGet to 2.8.2 and add a symlink creation so that the NuGet.exe file is on the path without having to add the directory to the path...

--- nuget/tools/chocolateyUninstall.ps1
+++ nuget/tools/chocolateyUninstall.ps1
@@ -1,11 +1,16 @@
 $packageName = "nuget"
 $appDir = "$($env:ChocolateyInstall)\apps\$($packageName)"
+$link = "$($env:ChocolateyInstall)\bin\nuget.exe"
 
 try
 {
     if (Test-Path $appDir)
     {
       Remove-Item "$($appDir)" -Recurse -Force
+    }
+
+    if (Test-Path $link) {
+        Remove-Item $link -Force
     }
     
     Write-ChocolateySuccess $packageName
