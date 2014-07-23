f6ae10557a8b81e2d6529af45938596df72ee3e0
Julian Easterling <julian@julianscorner.com>
2014-07-23 15:59:26 -0400

    Added a package to add "Edit with Notepad (Admin)" for servers.

---
+++ mysettings-notepadedit/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+$packageName = "mysettings-notepadedit"
+$toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+}
+
+try
+{
+    Start-ChocolateyProcessAsAdmin ". $toolDir\postInstall.ps1"
+
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
