435bec49bedc266a63497f95cb593441e3355aed
Julian Easterling <julian@julianscorner.com>
2014-07-23 21:05:31 -0400

    added a package to install and configure "Server" VMs...

---
+++ myvm-server/tools/chocolateyInstall.ps1
@@ -0,0 +1,25 @@
+$packageName = "myvm-server"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+}
+
+try {
+
+    Get-AppxProvisionedPackage -Online | Remove-AppxProvisionedPackage -Online
+    Get-AppxPackage | Remove-AppxPackage -ea Silent
+
+    Write-Output "Enabling Remote Desktop ..."
+    $settings = Get-WmiObject -Class "Win32_TerminalServiceSetting" `
+        -Namespace root\cimv2\terminalservices
+    $settings.SetAllowTsConnections(1)
+
+    netsh advfirewall firewall set rule group="Remote Desktop" new enable=yes
+
+    cinst mysettings-notepadedit
+    
+    Write-ChocolateySuccess $packageName
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
