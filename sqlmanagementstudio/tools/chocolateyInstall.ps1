6836aeddc922ecdf7dd69259a7677da243c80a28
Julian Easterling <julian@julianscorner.com>
2014-07-26 01:50:44 -0400

    Apparently, Microsoft changed the download link for SQL Server Management Studio...

--- sqlmanagementstudio/tools/chocolateyInstall.ps1
+++ sqlmanagementstudio/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName = "sqlmanagementstudio"
 $installerType = "EXE"
 $installerArgs = "/QUIETSIMPLE /IACCEPTSQLSERVERLICENSETERMS /ACTION=install /FEATURES=Tools"
-$url = "http://download.microsoft.com/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/SQLManagementStudio_x86_ENU.exe"
-$url64 = "http://download.microsoft.com/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/SQLManagementStudio_x64_ENU.exe"
+$url = "http://download.microsoft.com/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/MgmtStudio%2032BIT/SQLManagementStudio_x86_ENU.exe"
+$url64 = "http://download.microsoft.com/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/MgmtStudio%2064BIT/SQLManagementStudio_x64_ENU.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
