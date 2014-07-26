583dbe66bc0efa4a4b423e64e92d1482824f93bf
Julian Easterling <julian@julianscorner.com>
2014-07-25 22:03:09 -0400

    updated python to 2.7.8

--- python/tools/chocolateyInstall.ps1
+++ python/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $url = "https://www.python.org/ftp/python/2.7.8/python-2.7.8.msi"
 $url64 = "https://www.python.org/ftp/python/2.7.8/python-2.7.8.amd64.msi"
 $downloadPath = "$env:TEMP\chocolatey\$packageName"
 $toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
-$ahkExe = "$env:ChocolateyInstall\apps\autohotkey\AutoHotkey.exe"
+$ahkExe = "$env:SYSTEMDRIVE\tools\apps\autohotkey\AutoHotkey.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
@@ -19,8 +19,6 @@ try
         New-Item -Type Directory -Path $downloadPath | Out-Null
     }
 
-    Push-Location $downloadPath
-
     # Install ez_setup
     $version = "0.9"
     $ezsetup = "https://pypi.python.org/packages/source/e/ez_setup/ez_setup-$version.tar.gz"
@@ -32,14 +30,16 @@ try
     cmd /c "$env:SYSTEMDRIVE\python\python.exe $downloadPath\ez_setup-$version\ez_setup.py"
 
     # Install pip
-    Get-ChocolateyWebFile "pip" "get-pip.py" "https://raw.github.com/pypa/pip/master/contrib/get-pip.py"
-    cmd /c "$env:SYSTEMDRIVE\python\python.exe get-pip.py"
+    Get-ChocolateyWebFile "pip" "$downloadPath\get-pip.py" `
+        "https://raw.githubusercontent.com/pypa/pip/master/contrib/get-pip.py"
+
+    cmd /c "$env:SYSTEMDRIVE\python\python.exe $downloadPath\get-pip.py"
 
     # Install Pywin32
     $pywin32 = "http://sourceforge.net/projects/pywin32/files/pywin32/Build%20219/pywin32-219.win32-py2.7.exe/download"
     $pywin64 = "http://sourceforge.net/projects/pywin32/files/pywin32/Build%20219/pywin32-219.win-amd64-py2.7.exe/download"
 
-    Get-ChocolateyWebFile "pywin32" "pywin32.exe" $pywin32 $pywin64 
+    Get-ChocolateyWebFile "pywin32" "$downloadPath\pywin32.exe" $pywin32 $pywin64 
 
     $ahkScript = "$toolDir\install-pywin32.ahk"
 
