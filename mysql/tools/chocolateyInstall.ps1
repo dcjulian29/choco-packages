bf65e67661b295134019688f7924bc99c019760e
Julian Easterling <julian@julianscorner.com>
2014-08-04 00:19:09 -0400

    Added a package to install mysql server

---
+++ mysql/tools/chocolateyInstall.ps1
@@ -0,0 +1,64 @@
+$packageName = "mysql"
+$url = "http://cdn.mysql.com/Downloads/MySQL-5.6/mysql-5.6.20-win32.zip"
+$url64 = "http://cdn.mysql.com/Downloads/MySQL-5.6/mysql-5.6.20-winx64.zip"
+$appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
+$downloadPath = "$env:TEMP\chocolatey\$packageName"
+$dataDir = "$($env:SYSTEMDRIVE)\data\mysql"
+$sn = "MySQL"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+}
+
+try
+{
+    if (Get-Service | Where-Object { $_.Name -eq $sn }) {
+        $cmd = "Stop-Service -ErrorAction 0 -Name $sn;sc.exe delete $sn"
+        Start-ChocolateyProcessAsAdmin $cmd
+    }
+
+    if (Test-Path $appDir)
+    {
+        Write-Output "Removing previous version of package..."
+        Remove-Item "$($appDir)" -Recurse -Force
+    }
+
+    New-Item -Type Directory -Path $appDir | Out-Null
+    
+    if (-not (Test-Path $downloadPath))
+    {
+        New-Item -Type Directory -Path $downloadPath | Out-Null
+    }
+
+    Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url $url64
+    Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\"
+
+    $extractPath = $(Get-ChildItem -Directory -Path $downloadPath | Select-Object -First 1).Name
+
+    New-Item -Type Directory -Path $appDir\bin | Out-Null
+    Copy-Item -Path "$($downloadPath)\$($extractPath)\bin\*" -Destination "$appDir\bin" -Recurse
+    New-Item -Type Directory -Path $appDir\bin\share | Out-Null
+    Copy-Item -Path "$($downloadPath)\$($extractPath)share\*" -Destination "$appDir\bin\share" -Recurse
+
+    if (-not (Test-Path $dataDir))
+    {
+        New-Item -Type Directory -Path $dataDir | Out-Null
+        Copy-Item -Path "$($downloadPath)\$($extractPath)\data\*" -Destination "$dataDir" -Recurse
+    }
+
+    $config = "$appDir\my.ini"
+
+    Set-Content -Path $config -Encoding Ascii -Value "[mysqld]"
+    Add-Content -Path $config -Encoding Ascii -Value "basedir=$(""$appDir\bin"" -replace '\\', '\\')"
+    Add-Content -Path $config -Encoding Ascii -Value "datadir=$($dataDir -replace '\\', '\\')"
+
+    $cmd = "& $appDir\bin\mysqld.exe --install"
+    Start-ChocolateyProcessAsAdmin $cmd
+
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
