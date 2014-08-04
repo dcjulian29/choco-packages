133eee73a7607560b39ee63109890346240e5720
Julian Easterling <julian@julianscorner.com>
2014-08-04 00:17:43 -0400

    Added FFmpeg package for video and audio manipulations

---
+++ ffmpeg/tools/chocolateyInstall.ps1
@@ -0,0 +1,40 @@
+$packageName = "ffmpeg"
+$url = "http://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-20140727-git-ad91bf8-win32-static.7z"
+$url64 = "http://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20140727-git-ad91bf8-win64-static.7z"
+$downloadPath = "$($env:TEMP)\chocolatey\$($packageName)"
+$appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+}
+
+try {
+    if (Test-Path $appDir)
+    {
+        Write-Output "Removing previous version of package..."
+        Remove-Item "$($appDir)" -Recurse -Force
+    }
+
+    New-Item -Type Directory -Path $appDir | Out-Null
+    
+    if (-not (Test-Path $downloadPath))
+    {
+        New-Item -Type Directory -Path $downloadPath | Out-Null
+    }
+
+    Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.7z" $url $url64
+    Get-ChocolateyUnzip "$downloadPath\$packageName.7z" "$downloadPath\"
+
+    $extractPath = $(Get-ChildItem -Directory -Path $downloadPath | Select-Object -First 1).Name
+
+    Copy-Item -Path "$($downloadPath)\$($extractPath)\bin\*" -Destination "$appDir" -Recurse
+    Copy-Item -Path "$($downloadPath)\$($extractPath)\licenses" -Destination "$appDir" -Recurse
+    Copy-Item -Path "$($downloadPath)\$($extractPath)\doc" -Destination "$appDir" -Recurse
+    Copy-Item -Path "$($downloadPath)\$($extractPath)\presets" -Destination "$appDir" -Recurse
+    Copy-Item -Path "$($downloadPath)\$($extractPath)\README.txt" -Destination "$appDir"
+
+    Write-ChocolateySuccess $packageName
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
