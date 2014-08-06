9e03e64857a823a1cd73ddb94b3be39cdb675c93
Julian Easterling <julian@julianscorner.com>
2014-08-06 00:02:41 -0400

    Added a package for the Atom IDE from GitHub

---
+++ atom/tools/chocolateyInstall.ps1
@@ -0,0 +1,35 @@
+$packageName = "atom"
+$url = "https://github.com/atom/atom/releases/download/v0.120.0/atom-windows.zip"
+
+$downloadPath = "$($env:TEMP)\chocolatey\$($packageName)"
+$appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+}
+
+try {
+    if (Test-Path $appDir)
+    {
+        Write-Output "Removing previous version of package..."
+        Remove-Item "$($appDir)" -Recurse -Force
+    }
+
+    New-Item -Type Directory -Path $appDir | Out-Null
+    
+    if (-not (Test-Path $downloadPath))
+    {
+        New-Item -Type Directory -Path $downloadPath | Out-Null
+    }
+
+    Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url
+    Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\"
+
+
+    Copy-Item -Path "$($downloadPath)\$($packageName)\*" -Destination "$appDir" -Recurse
+
+    Write-ChocolateySuccess $packageName
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
