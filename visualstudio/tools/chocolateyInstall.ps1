0ed7570d057dd03227e253462e0ac28ab4b74efa
Julian Easterling <julian@julianscorner.com>
2014-08-09 01:59:47 -0400

    Everytime Visual Studio is "upgraded" the phone components reappear... so the other extensions in this package were moved to their own package and the phone development components moved to the Visual Studio "meta" package so that these components are removed after each update.

--- visualstudio/tools/chocolateyInstall.ps1
+++ visualstudio/tools/chocolateyInstall.ps1
@@ -1,4 +1,5 @@
 $packageName = "visualstudio"
+$toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
@@ -6,9 +7,25 @@ if ($psISE) {
 
 try
 {
-    Write-Output "Visual Studio Virtual Package."
-    Write-Output "Packages depend on this package instead of the specific 'edition' package."
+    Write-Verbose "Visual Studio Virtual Package."
+    Write-Verbose "Packages depend on this package instead of the specific 'edition' package."
 
+    # Remove Windows Phone Emulator
+    if (Test-Path "$($env:ALLUSERSPROFILE)\Package Cache\{940596e5-652a-4970-8a5a-492e73ed0fbb}") {
+        Push-Location "$($env:ALLUSERSPROFILE)\Package Cache\{940596e5-652a-4970-8a5a-492e73ed0fbb}" 
+        If (Test-Path "MobileTools_EmulatorWP81.exe") {
+            Start-ChocolateyProcessAsAdmin "./MobileTools_EmulatorWP81.exe /Uninstall /Passive /NoRestart"
+        }
+        Pop-Location
+    }
+    
+    Remove-Item "$($env:ALLUSERSPROFILE)\Package Cache\{940596e5-652a-4970-8a5a-492e73ed0fbb}" -Force -Recurse
+
+    # Remove Visual Studio Components that I don't want or use...
+    Start-ChocolateyProcessAsAdmin "$toolDir\uninstallPhonePackages.bat"
+    
+	Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"    
+    
     Write-ChocolateySuccess $packageName
 }
 catch
