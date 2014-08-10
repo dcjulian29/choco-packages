5422281ccc5a967c56caefa8943fb5e13edaba1c
Julian Easterling <julian@julianscorner.com>
2014-08-10 12:22:34 -0400

    Split the DEVVM package out to individual packages based on component and using Chocolatey and/or NuGet's dependency management.

---
+++ devvm-dotnet/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = "devvm-visualstudio"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+}
+
+try {
+    cinst -source webpi WebMatrixWeb
+    cinst -source webpi WindowsAzureXPlatCLI
+    cinst -source webpi WindowsAzurePowershell
+    cinst -source webpi VWDOrVs2013AzurePack.2.4
+
+    Write-ChocolateySuccess $packageName
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
--- myvm-development/tools/chocolateyInstall.ps1
+++ myvm-development/tools/chocolateyInstall.ps1
@@ -4,22 +4,24 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-Function Run-Package {
-    try {
-        if (-not (Test-Path $env:SYSTEMDRIVE\home\projects))
-        {
-            New-Item -Type Directory -Path $env:SYSTEMDRIVE\home\projects | Out-Null
-        }
+try {
+    if (-not (Test-Path $env:SYSTEMDRIVE\home\projects))
+    {
+        New-Item -Type Directory -Path $env:SYSTEMDRIVE\home\projects | Out-Null
+    }
 
+    if (-not (Test-Path "$env:SYSTEMDRIVE\home\vm\.SyncID")) {
         if (-not (Test-Path $env:SYSTEMDRIVE\home\vm))
         {
             New-Item -Type Directory -Path $env:SYSTEMDRIVE\home\vm | Out-Null
-        }
 
-        if (-not (Test-Path $env:SYSTEMDRIVE\home\vm\etc))
-        {
-            New-Item -Type Directory -Path $env:SYSTEMDRIVE\home\vm\etc | Out-Null
+            if (-not (Test-Path $env:SYSTEMDRIVE\home\vm\etc))
+            {
+                New-Item -Type Directory -Path $env:SYSTEMDRIVE\home\vm\etc | Out-Null
+            }
         }
+        
+        if (
 
         if (Test-Path $env:SYSTEMDRIVE\etc) {
             # Test if etc is already SYMLINKD to the "home" etc            
@@ -39,97 +41,6 @@ Function Run-Package {
         Pop-Location
         cmd /c "attrib +S $($env:SYSTEMDRIVE)\home"
 
-        ##############################################################################################
-        
-        cinst mysettings-devwallpaper
-
-        cinst myscripts-development
-        cinst myscripts-powershell
-        
-        cinst java
-        cinst pdfcreator
-        cinst winpcap
-        cinst sumatrapdf
-        cinst keepass
-        cinst 7zip
-        cinst executor
-
-        cinst xplorer2
-        cinst mysettings-xplorer2
-        
-        cinst git
-        cinst gitflow
-        cinst gittfs
-        cinst posh-git
-        cinst tortoisegit
-        cinst svn
-
-        cinst nuget
-        cinst nugetexplorer
-        cinst baretail
-        cinst kdiff
-        cinst wmiexplorer
-        cinst xmlquire
-        cinst argouml
-        cinst soapui
-        cinst cygwin
-        cinst bind-tools
-        cinst python
-        cinst searchmyfiles
-        cinst wireshark
-        cinst jperf
-        cinst fiddler
-        cinst regexbuilder
-
-        cinst posh-go
-
-        Import-Module go
-        go -key "projects" -selectedPath "C:\home\projects" -add
-        go -key "etc" -selectedPath "C:\home\vm\etc" -add
-        go -key "home" -selectedPath "$($env:USERPROFILE)" -add
-        go -key "downloads" -selectedPath "$($env:USERPROFILE)\Downloads" -add
-        go -key "docs" -selectedPath "$($env:USERPROFILE)\Documents" -add
-        go -key "documents" -selectedPath "$($env:USERPROFILE)\Documents" -add
-        go -key "pics" -selectedPath "$($env:USERPROFILE)\Pictures" -add
-        go -key "pictures" -selectedPath "$($env:USERPROFILE)\Pictures" -add
-        go -key "videos" -selectedPath "$($env:USERPROFILE)\Videos" -add
-        go -key "desktop" -selectedPath "$($env:USERPROFILE)\Desktop" -add
-        
-        cinst posh-psake
-
-        
-        cinst vsultimate
-        cinst resharper
-        cinst stylecop
-
-        cinst mysettings-vsix
-        cinst mysettings-stylecop
-        
-        cinst webpi
-        cinst -source webpi WebMatrixWeb
-        cinst -source webpi WindowsAzureXPlatCLI
-        cinst -source webpi WindowsAzurePowershell
-        cinst -source webpi VWDOrVs2013AzurePack.2.4
-
-        cinst xmind
-        cinst dotpeek
-        cinst expresso
-        cinst linqpad
-        cinst nant
-        cinst nsis
-        cinst octopusdeploy-tool
-
-        # Install Chrome    
-        $chrome = "https://dl.google.com/tag/s/appguid={8A69D345-D564-463C-AFF1-A69D9E530F96}&iid={00000000-0000-0000-0000-000000000000}&lang=en&browser=3&usagestats=0&appname=Google%2520Chrome&needsadmin=prefers/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi"
-        Install-ChocolateyPackage "Chrome" "MSI" "/quiet" $chrome
-        Start-ChocolateyProcessAsAdmin "Remove-Item '$($env:PUBLIC)\Desktop\Google Chrome.lnk' -Force"
-        
-        # Install Firefox
-        $firefox = "https://download.mozilla.org/?product=firefox-31.0-SSL&os=win&lang=en-US"
-        Install-ChocolateyPackage "Firefox" "EXE" "-ms" $firefox
-        Start-ChocolateyProcessAsAdmin "Remove-Item '$($env:PUBLIC)\Desktop\Mozilla Firefox.lnk' -Force"
-
-        
         Remove-Item -Path "$($env:SYSTEMDRIVE)\home\vm\*" -Recurse -Force
         
         if (Get-ProcessorBits -eq 64) {
@@ -139,29 +50,10 @@ Function Run-Package {
         }
         
         Write-Warning "After configuring BTSync, Restart-Computer to get started developing! :)"
-        
-        Write-ChocolateySuccess $packageName
-    } catch {
-        Write-ChocolateyFailure $packageName $($_.Exception.Message)
-        throw
     }
-}
-
-#######################################################################################
-
-Start-Transcript "$($env:TEMP)\myvm-development-transcript.log" -Append
-
-$package = (Get-ChildItem "$($env:ChocolateyInstall)\lib" | Select-Object basename).basename `
-    | Where-Object { $_.StartsWith("myvm-development") }
-
-if ($package.Count -gt 1) {
-    Write-Warning "This package has already been installed."
-    Write-Warning "This package though upgraded is not designed to run multiple times."
-    Write-Warning "Please review the differences between This package though upgraded is not designed to run multiple times."
 
     Write-ChocolateySuccess $packageName
-} else {
-    Run-Package
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
 }
-
-Stop-Transcript
