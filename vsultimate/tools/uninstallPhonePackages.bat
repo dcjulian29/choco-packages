41b4dc6e86e9d1a99ee6fffbc67ddcb0cb3e2cfa
Julian Easterling <julian@julianscorner.com>
2014-08-10 06:27:13 -0400

    The "meta" visual studio package gets executed first so i had to move the component removal to the individual packages

---
+++ vsultimate/tools/uninstallPhonePackages.bat
@@ -0,0 +1,19 @@
+:: Remove Window Phone Development Stuff since I'm just not into it...
+msiexec.exe /X{5CBE79CC-5F78-4AC2-AEB0-62F939D869CC} /passive /norestart
+msiexec.exe /X{18164F64-DC06-4578-9160-A5A95F87D3FF} /passive /norestart
+msiexec.exe /X{2B22F6A1-5E8E-49D6-B754-84FF69354314} /passive /norestart
+msiexec.exe /X{49D0DBA7-4D22-3EBF-93DD-3569917DD82E} /passive /norestart
+msiexec.exe /X{7F83A5E9-89C3-48EE-9740-BBC2096BB167} /passive /norestart
+msiexec.exe /X{C611D804-7450-3C3D-943E-C2C96A12911B} /passive /norestart
+msiexec.exe /X{DB9620B6-CBBE-433C-A769-5617C40862A8} /passive /norestart
+msiexec.exe /X{E66B6040-B567-4DA7-8DA7-AC5AC3CD2E4B} /passive /norestart
+msiexec.exe /X{E719AF7A-FBD9-45F8-AD4F-EBD1EFD985BB} /passive /norestart
+msiexec.exe /X{F57F75C9-4DE8-3C3A-AED0-344C353EB945} /passive /norestart
+msiexec.exe /X{F58FA66E-86F8-4BEA-9E6D-FAE8E0FB86BA} /passive /norestart
+msiexec.exe /X{FDEED0E7-C5A0-4293-B2B8-E83E40825F0D} /passive /norestart
+
+:: Remove Other Components I Don't Need...
+msiexec.exe /X{3674F088-9B90-473A-AAC3-20A00D8D810C} /passive /norestart
+msiexec.exe /X{6AB13C21-C3EC-46E1-8009-6FD5EBEE515B} /passive /norestart
+msiexec.exe /X{6AC81125-8485-463D-9352-3F35A2508C11} /passive /norestart
+msiexec.exe /X{EBD9DB6D-180B-4C59-9622-B75CC4B32C94} /passive /norestart
