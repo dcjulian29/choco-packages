293f0dffaeeb01ce60a9d3f4e50cf455e02f6278
Julian Easterling <julian@julianscorner.com>
2014-08-10 21:42:43 -0400

    moved the dependencies to the nuspec file and added an update for psremoting on family computer

--- mycomputers-family/tools/chocolateyInstall.ps1
+++ mycomputers-family/tools/chocolateyInstall.ps1
@@ -1,24 +1,16 @@
 $packageName = "mycomputers-family"
 
-Start-Transcript -Path "$($env:TEMP)\mycomputers-family.transcript.log" -Append
-
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
 try {    
-    cinst mysettings-homeserverclient
-    cinst flux
-    cinst java
-    cinst notepadplusplus
-    cinst sumatrapdf
-    cinst tightvnc
 
+    Start-ChocolateyProcessAsAdmin "Enable-PSRemoting -Force"
+    
     Write-ChocolateySuccess $packageName        
+
 } catch {
     Write-ChocolateyFailure $packageName $($_.Exception.Message)
     throw
 }
-
-Stop-Transcript
-
