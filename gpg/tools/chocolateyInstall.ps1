40717293c6c90f3a83c31fc2953ea32210d8ff31
Julian Easterling <julian@julianscorner.com>
2014-09-09 09:46:31 -0400

    Updated GPG to 2.2.2

--- gpg/tools/chocolateyInstall.ps1
+++ gpg/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@ $packageName = "gpg"
 $downloadPath = "$env:TEMP\chocolatey\$packageName"
 $installerType = "EXE"
 $installerArgs = "/S /C=$downloadPath\gpg4win.ini"
-$url = "http://files.gpg4win.org/gpg4win-light-2.2.1.exe"
+$url = "http://files.gpg4win.org/gpg4win-light-2.2.2.exe"
 $toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
 
 $install = @"
@@ -29,6 +29,14 @@ try
     }
 
     Set-Content -Value "$install" -Path $downloadPath\gpg4win.ini
+
+    if (Test-Path "C:\Program Files (x86)\GNU\GnuPG") {
+        & 'C:\Program Files (x86)\GNU\GnuPG\gpg4win-uninstall.exe' /S
+    }
+    
+    if (Test-Path "C:\Program Files\GNU\GnuPG"){
+        & 'C:\Program Files\GNU\GnuPG\gpg4win-uninstall.exe' /S
+    }
 
     Install-ChocolateyPackage $packageName $installerType $installerArgs $url
 
