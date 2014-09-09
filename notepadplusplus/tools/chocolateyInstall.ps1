6810415a4c2115e1a2a73d8a99a3736d8a5719e1
Julian Easterling <julian@julianscorner.com>
2014-09-09 09:47:40 -0400

    updated notepad++ to 6.6.9

--- notepadplusplus/tools/chocolateyInstall.ps1
+++ notepadplusplus/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = "notepadplusplus"
 $installerType = "EXE"
 $installerArgs = "/S"
-$url = "http://download.tuxfamily.org/notepadplus/6.6.8/npp.6.6.8.Installer.exe"
+$url = "http://download.tuxfamily.org/notepadplus/6.6.9/npp.6.6.9.Installer.exe"
 
 try
 {
