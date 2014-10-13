b16f1d15c30b843a43035c694817ba032dc10722
Julian Easterling <julian@julianscorner.com>
2014-10-12 23:56:39 -0400

    Updated Atom to v 0.136

--- atom/tools/chocolateyInstall.ps1
+++ atom/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = "atom"
-$url = "https://github.com/atom/atom/releases/download/v0.127.0/atom-windows.zip"
+$url = "https://github.com/atom/atom/releases/download/v0.136.0/atom-windows.zip"
 
 $downloadPath = "$($env:TEMP)\chocolatey\$($packageName)"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
