441a52bb249025c916a2ed00faef5ca40e26f348
Julian Easterling <julian@julianscorner.com>
2014-10-18 12:22:08 -0400

    Added Sando Code Search Visual Studio plugin and added it to VSIX packages.

---
+++ vsix-sandocodesearch/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = "vsix-sandocodesearch"
+$vsgallery = "http://visualstudiogallery.msdn.microsoft.com"
+$vsix = "06f39a31-20ce-408c-afee-8a02b484db1c/file/77601/73/UI.vsix"
+$url = "$vsgallery/$vsix"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+}
+
+try {
+    Install-ChocolateyVsixPackage $packageName $url
+
+    Write-ChocolateySuccess $packageName
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
