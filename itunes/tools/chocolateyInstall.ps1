e41e1adb0aa88153e5080f07223fa9049785f724
Julian Easterling <julian@julianscorner.com>
2014-10-19 13:07:13 -0400

    Updated iTunes to 12.0.1

--- itunes/tools/chocolateyInstall.ps1
+++ itunes/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName = "itunes"
 $installerArgs = "/quiet /passive /norestart"
 
-$url = "https://secure-appldnld.apple.com/iTunes11/031-06979.20140909.Ppfef/iTunesSetup.exe"
-$url64 = "https://secure-appldnld.apple.com/iTunes11/031-06980.20140909.Ffgt9/iTunes64Setup.exe"
+$url = "https://secure-appldnld.apple.com/iTunes12/031-08058.20141016.34epp/iTunesSetup.exe"
+$url64 = "https://secure-appldnld.apple.com/iTunes12/031-08059.20141016.FrPFF/iTunes64Setup.exe"
 
 $downloadPath = "$env:TEMP\chocolatey\$packageName"
 $errorCode = @(0, 3010)
