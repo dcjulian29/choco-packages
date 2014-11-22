9a2f0bcf2da45dbb5b65f06d8104ef466c986383
Julian Easterling <julian@julianscorner.com>
2014-11-21 19:45:42 -0500

    updated Sando Code Search extension to 1.7.89

--- vsix-sandocodesearch/tools/chocolateyInstall.ps1
+++ vsix-sandocodesearch/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "vsix-sandocodesearch"
 $vsgallery = "http://visualstudiogallery.msdn.microsoft.com"
-$vsix = "06f39a31-20ce-408c-afee-8a02b484db1c/file/77601/73/UI.vsix"
+$vsix = "06f39a31-20ce-408c-afee-8a02b484db1c/file/77601/76/UI.vsix"
 $url = "$vsgallery/$vsix"
 
 if ($psISE) {
