9e4df62a0a0bbec77f425b2cb7ef0fb1cc73ff95
Julian Easterling <julian@julianscorner.com>
2014-11-29 21:39:53 -0500

    Updated handbrake to 0.10.0

--- handbrake/tools/chocolateyInstall.ps1
+++ handbrake/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName = "handbrake"
 $installerType = "EXE"
 $installerArgs = "/S"
-$url = "http://handbrake.fr/rotation.php?file=HandBrake-0.9.9-1_i686-Win_GUI.exe"
-$url64 = "http://handbrake.fr/rotation.php?file=HandBrake-0.9.9-1_x86_64-Win_GUI.exe"
+$url = "https://handbrake.fr/rotation.php?file=HandBrake-0.10.0-i686-Win_GUI.exe"
+$url64 = "https://handbrake.fr/rotation.php?file=HandBrake-0.10.0-x86_64-Win_GUI.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
