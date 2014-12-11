9b3e13bc736eb5b67e00b62e4c4f37bc988c749c
Julian Easterling <julian@julianscorner.com>
2014-12-10 23:42:15 -0500

    Updated dotpeek to a stand-alone 1.3 version which does not go through the "new web installer"...

--- dotpeek/tools/chocolateyInstall.ps1
+++ dotpeek/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName = "dotpeek"
-$installerType = "EXE"
-$installerArgs = "/qn"
 $url = "http://download.jetbrains.com/resharper/dotPeek32_1.3.exe"
 $url64 = "http://download.jetbrains.com/resharper/dotPeek64_1.3.exe"
+$downloadPath = "$env:TEMP\chocolatey\$packageName"
+$appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
@@ -10,6 +10,21 @@ if ($psISE) {
 
 try
 {
+    if (Test-Path $appDir) {
+        Write-Output "Removing previous version of package..."
+        Remove-Item "$($appDir)" -Recurse -Force
+    }
+
+    New-Item -Type Directory -Path $appDir | Out-Null
+    
+    if (-not (Test-Path $downloadPath)) {
+        New-Item -Type Directory -Path $downloadPath | Out-Null
+    }
+
+    Get-ChocolateyWebFile $packageName "$appDir\$packageName.exe" $url $url64
+
+
+
     Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
 
     Write-ChocolateySuccess $packageName
