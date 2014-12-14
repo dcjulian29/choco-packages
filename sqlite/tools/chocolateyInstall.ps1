e58fa209d1c90c644ed548577b0250177878094d
Julian Easterling <julian@julianscorner.com>
2014-12-13 22:41:58 -0500

    Updated sqlite to 3.8.7.4

--- sqlite/tools/chocolateyInstall.ps1
+++ sqlite/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "sqlite"
-$sqliteShell = "https://www.sqlite.org/2014/sqlite-shell-win32-x86-3080702.zip"
-$sqliteAnalyzer = "https://www.sqlite.org/2014/sqlite-analyzer-win32-x86-3080702.zip"
+$sqliteShell = "https://www.sqlite.org/2014/sqlite-shell-win32-x86-3080704.zip"
+$sqliteAnalyzer = "https://www.sqlite.org/2014/sqlite-analyzer-win32-x86-3080704.zip"
 
 $downloadPath = "$($env:TEMP)\chocolatey\$($packageName)"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
