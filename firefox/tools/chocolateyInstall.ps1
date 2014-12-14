fa63387949227fddc73262be0ad89e69d66855c0
Julian Easterling <julian@julianscorner.com>
2014-12-14 09:18:39 -0500

    Updated Firefox to version 34

--- firefox/tools/chocolateyInstall.ps1
+++ firefox/tools/chocolateyInstall.ps1
@@ -1,16 +1,25 @@
 $packageName = "firefox"
 $installerType = "EXE"
 $installerArgs = "-ms"
-$url = "https://download.mozilla.org/?product=firefox-33.0-SSL&os=win&lang=en-US"
+$url = "https://download.mozilla.org/?product=firefox-34.0-SSL&os=win&lang=en-US"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
 try {
-    $app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -eq "Google Chrome" }
-    
-    if ($app) {
+    $uninstall64 = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\'
+    $uninstall32 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\'
+     
+    $uninstall = $(Get-ChildItem $uninstall).Name
+     
+    if (Test-Path $uninstall32) {
+        $uninstall += $(Get-ChildItem $uninstall32).Name
+    }
+     
+    $uninstall = $uninstall -match "Mozilla Firefox [\d\.]+ \([^\s]+ [a-zA-Z\-]+\)" | Select -First 1
+      
+    if ($uninstall) {
         Write-Output "Firefox is already installed and will update itself, no need to update via package..."
     } else {
         Install-ChocolateyPackage $packageName $installerType $installerArgs $url
