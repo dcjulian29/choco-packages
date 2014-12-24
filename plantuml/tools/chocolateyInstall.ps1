a509a80eb94223caf94d7cfa0bed57306f9185e8
Julian Easterling <julian@julianscorner.com>
2014-12-24 09:23:03 -0500

    Added the PlantUML package which allows you to create text files which can be source controlled which describe various UML constructs and then "compile" them into a UML diagram.

---
+++ plantuml/tools/chocolateyInstall.ps1
@@ -0,0 +1,40 @@
+$packageName = "plantuml"
+$url = "http://sourceforge.net/projects/plantuml/files/plantuml.jar/download"
+$appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+}
+
+try {
+    if (Test-Path $appDir) {
+        Write-Output "Removing previous version of package..."
+        Remove-Item -Path $appDir -Recurse -Force
+    }
+
+    New-Item -Type Directory -Path $appDir | Out-Null
+
+    Get-ChocolateyWebFile $packageName "$appDir\$packageName.jar" $url
+
+    Set-Content -Path "$appDir\plantuml.bat" -Value @"
+@echo off
+
+setlocal
+ 
+SET DIR=%~dp0%
+path %SYSTEMDRIVE%\tools\apps\graphviz\bin;%JAVA_HOME%\bin;%PATH% 
+
+java -jar %DIR%\plantuml.jar %*
+
+endlocal
+"@
+
+    $shimgen = "$env:ChocolateyInstall\chocolateyinstall\tools\shimgen.exe"
+
+    & $shimgen -o "$env:ChocolateyInstall\bin\plantuml.bat" -p "$appDir\plantuml.bat"
+
+    Write-ChocolateySuccess $packageName
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
