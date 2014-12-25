7486b951f618f3acae953049f5b6b59aab7cf9f3
Julian Easterling <julian@julianscorner.com>
2014-12-25 11:42:49 -0500

    Updated GreenShot to 1.2.4

--- greenshot/tools/chocolateyInstall.ps1
+++ greenshot/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = "greenshot"
 $installerType = "EXE"
 $installerArgs = "/SILENT"
-$url = "http://sourceforge.net/projects/greenshot/files/Greenshot/Greenshot%201.1/Greenshot-INSTALLER-1.1.9.13.exe/download?use_mirror=iweb"
+$url = "http://sourceforge.net/projects/greenshot/files/Greenshot/Greenshot%201.2/Greenshot-INSTALLER-1.2.4.9-RELEASE.exe/download?use_mirror=iweb"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
@@ -9,18 +9,19 @@ if ($psISE) {
 
 try
 {
-    if (Get-Process 'Greenshot' -ea SilentlyContinue) {
-        Stop-Process -processname Greenshot
+    if (Get-Process "Greenshot" -ErrorAction SilentlyContinue) {
+        Stop-Process -ProcessName "Greenshot"
     }
 
     if (-not (Test-Path "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5")) {
-        # cinst netfx3 -source windowsfeatures
-        # chocolatey uses the 32-bit version of DISM on a 64-bit OS which results in error...
-        # until they fix, or I submit a pull request for "this" fix... I'll do the work-around here...
-
         $dism = "$env:WinDir\sysnative\dism.exe"
         $args = "/Online /NoRestart /Enable-Feature /FeatureName:NetFx3"
-        Start-ChocolateyProcessAsAdmin "cmd /c `"$dism $args`""
+
+        if (Test-ProcessAdminRights) {
+            cmd /c "$dism $args"
+        } else {
+            Start-ChocolateyProcessAsAdmin "cmd /c `"$dism $args`""
+        }
     } else {
         Write-Verbose "Microsoft .Net 3.5 Framework is already installed on this system..."
     } 
