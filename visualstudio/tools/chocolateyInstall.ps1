8eba3afadc2254d5a6289d9acaf8446b00d1e676
Julian Easterling <julian@julianscorner.com>
2014-12-26 14:24:15 -0500

    If Visual Studio is not installed, do not allow the virtual package to succeed.

--- visualstudio/tools/chocolateyInstall.ps1
+++ visualstudio/tools/chocolateyInstall.ps1
@@ -9,6 +9,18 @@ try
 {
     Write-Verbose "Visual Studio Virtual Package."
     Write-Verbose "Packages depend on this package instead of the specific 'edition' package."
+
+    Write-Output "Checking for Visual Studio 2013..."
+    
+    if (Test-Path "$env:SYSTEMDRIVE\Program Files (x86)") {
+        $pf = "$env:SYSTEMDRIVE\Program Files (x86)"
+    } else {
+        $pf = "$env:SYSTEMDRIVE\Program Files (x86)"
+    }
+    
+    if (-not (Test-Path "$pf\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe")) {
+        throw "Visual Studio is not installed, please install one of the 'edition' packages."
+    }
     
     Write-ChocolateySuccess $packageName
 }
