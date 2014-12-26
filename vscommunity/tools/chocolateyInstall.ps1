eafbdf9767599654c973b5a8ca5b4f5da7457cde
Julian Easterling <julian@julianscorner.com>
2014-12-25 22:10:52 -0500

    Added some code to stop the "Windows 8.1 Phone Emulator" update and to remove it.

--- vscommunity/tools/chocolateyInstall.ps1
+++ vscommunity/tools/chocolateyInstall.ps1
@@ -11,7 +11,24 @@ try
 {
     Install-ChocolateyPackage $packageName $installerType $installerArgs $url
     
-    Write-ChocolateySuccess $packageName
+    Start-Sleep -Seconds 5
+
+    $cmd = @"
+
+Get-Process -Name 'Windows Phone 8.1 Emulators - ENU' -ErrorAction SilentlyContinue `
+    | Stop-Process -Force
+
+Push-Location "C:\ProgramData\Package Cache\{166a69f6-6512-47ea-a342-17d954fc059a}"
+& "MobileTools_EmulatorWP81.exe" /uninstall /quiet
+Pop-Location
+
+"@    
+    
+    if (Test-ProcessAdminRights) {
+        Invoke-Expression $cmd
+    } else {
+        Start-ChocolateyProcessAsAdmin $cmd
+    }    
 }
 catch
 {
