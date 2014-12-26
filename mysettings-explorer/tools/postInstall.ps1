997e49c9e38cd235b548d16c68ef976fd8a545d1
Julian Easterling <julian@julianscorner.com>
2014-12-25 20:01:47 -0500

    Fixed a bug where the script would fail if OneDrive wasn't installed on the computer.

--- mysettings-explorer/tools/postInstall.ps1
+++ mysettings-explorer/tools/postInstall.ps1
@@ -17,55 +17,56 @@ Add-Type -TypeDefinition $definition -PassThru  | Out-Null
 New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT  -Name HKCR | Out-Null
 
 $enabled = $false
-
 $rule = New-Object System.Security.AccessControl.RegistryAccessRule ($env:USERNAME,"FullControl","Allow")
-
-# One Drive
-
 $res = [Win32.Api]::RtlAdjustPrivilege(9, $true, $false, [ref]$enabled)
+
+# OneDrive
 $key = [Microsoft.Win32.Registry]::ClassesRoot.OpenSubKey( `
     "CLSID\{8E74D236-7F35-4720-B138-1FED0B85EA75}\ShellFolder", `
     [Microsoft.Win32.RegistryKeyPermissionCheck]::ReadWriteSubTree, `
     [System.Security.AccessControl.RegistryRights]::takeownership)
 
-$acl = $key.GetAccessControl()
-$acl.SetOwner([System.Security.Principal.NTAccount]$env:USERNAME)
-$key.SetAccessControl($acl)
+if ($key) {
 
-$acl = $key.GetAccessControl()
-$acl.SetAccessRule($rule)
-$key.SetAccessControl($acl)
+    $acl = $key.GetAccessControl()
+    $acl.SetOwner([System.Security.Principal.NTAccount]$env:USERNAME)
+    $key.SetAccessControl($acl)
 
-$key.close()
+    $acl = $key.GetAccessControl()
+    $acl.SetAccessRule($rule)
+    $key.SetAccessControl($acl)
 
-$Path = "HKCR:\CLSID\{8E74D236-7F35-4720-B138-1FED0B85EA75}\ShellFolder"
+    $key.close()
 
-if ((Get-ItemProperty -path $Path -Name Attributes).Attributes -eq 4034920525) {
-    Set-ItemProperty -Path $path -Name Attributes -value 2668101709 
+    $Path = "HKCR:\CLSID\{8E74D236-7F35-4720-B138-1FED0B85EA75}\ShellFolder"
+
+    if ((Get-ItemProperty -path $Path -Name Attributes).Attributes -eq 4034920525) {
+        Set-ItemProperty -Path $path -Name Attributes -value 2668101709 
+    }
 }
 
-# Home Group
-
-$res = [Win32.Api]::RtlAdjustPrivilege(9, $true, $false, [ref]$enabled)
+# HomeGroup
 $key = [Microsoft.Win32.Registry]::ClassesRoot.OpenSubKey( `
     "CLSID\{B4FB3F98-C1EA-428D-A78A-D1F5659CBA93}\ShellFolder", `
     [Microsoft.Win32.RegistryKeyPermissionCheck]::ReadWriteSubTree, `
     [System.Security.AccessControl.RegistryRights]::takeownership)
 
-$acl = $key.GetAccessControl()
-$acl.SetOwner([System.Security.Principal.NTAccount]$env:USERNAME)
-$key.SetAccessControl($acl)
+if ($key) {
+    $acl = $key.GetAccessControl()
+    $acl.SetOwner([System.Security.Principal.NTAccount]$env:USERNAME)
+    $key.SetAccessControl($acl)
 
-$acl = $key.GetAccessControl()
-$acl.SetAccessRule($rule)
-$key.SetAccessControl($acl)
+    $acl = $key.GetAccessControl()
+    $acl.SetAccessRule($rule)
+    $key.SetAccessControl($acl)
 
-$key.close()
+    $key.close()
 
-$Path = "HKCR:\CLSID\{B4FB3F98-C1EA-428D-A78A-D1F5659CBA93}\ShellFolder"
+    $Path = "HKCR:\CLSID\{B4FB3F98-C1EA-428D-A78A-D1F5659CBA93}\ShellFolder"
 
-if ((Get-ItemProperty -path $Path -Name Attributes).Attributes -eq 2961441036) {
-    Set-ItemProperty -Path $path -Name Attributes -value 2962489612 
+    if ((Get-ItemProperty -path $Path -Name Attributes).Attributes -eq 2961441036) {
+        Set-ItemProperty -Path $path -Name Attributes -value 2962489612 
+    }
 }
 
 & sc.exe config HomeGroupProvider start= disabled
