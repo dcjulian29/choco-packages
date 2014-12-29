9bda2674992cf6689d7960fa0e0f3fcaec300b8a
Julian Easterling <julian@julianscorner.com>
2014-12-29 13:53:17 -0500

    I'd stopped using SmartGit a while back because I was working on non-open-source software, but after several months with TortoiseGit, I've decided that I'll purchase a license... The explorer integration in TortoiseGit is VERY buggy and caused me a lots of headaches.

---
+++ smartgit/tools/chocolateyInstall.ps1
@@ -0,0 +1,54 @@
+$packageName = "smartgit"
+$downloadPath = "$env:TEMP\chocolatey\$packageName"
+$appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
+$url = "http://www.syntevo.com/download/smartgit/smartgit-portable-6_5_3.7z"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+}
+
+try
+{
+    if (-not (Test-Path $downloadPath)) {
+        New-Item -Type Directory -Path $downloadPath | Out-Null
+    }
+
+    Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.7z" $url
+    Get-ChocolateyUnzip "$downloadPath\$packageName.7z" "$downloadPath\"
+
+    if (Test-Path $appDir) {
+        Move-Item -Path "$appDir\.settings" -Destination "$downloadPath\.settings"
+        Remove-Item "$($appDir)" -Recurse -Force
+    }
+
+    New-Item -Type Directory -Path $appDir | Out-Null
+    New-Item -Type Directory -Path "$appDir\bin" | Out-Null
+    New-Item -Type Directory -Path "$appDir\lib" | Out-Null
+    New-Item -Type Directory -Path "$appDir\licenses" | Out-Null
+
+    Copy-Item -Path "$downloadPath\SmartGit\bin\*" -Destination "$appDir\bin" -Recurse -Container
+    Copy-Item -Path "$downloadPath\SmartGit\lib\*" -Destination "$appDir\lib" -Recurse -Container
+    Copy-Item -Path "$downloadPath\SmartGit\licenses\*" -Destination "$appDir\licenses" -Recurse -Container
+
+    if (Test-Path "$downloadPath\.settings") {
+        Move-Item -Path "$downloadPath\.settings" -Destination "$appDir\.settings"
+    }
+
+    if (Test-Path "C:\Program Files (x86)\Java\jre7") {
+        $java = "C:\Program Files (x86)\Java\jre7"
+
+        if (Test-ProcessAdminRights) {
+            [Environment]::SetEnvironmentVariable('SMARTGIT_JAVA_HOME',"$java", 'Machine')
+        } else {
+            Start-ChocolateyProcessAsAdmin `
+                "[Environment]::SetEnvironmentVariable('SMARTGIT_JAVA_HOME','$java', 'Machine')"
+        }
+    }
+
+    Write-ChocolateySuccess $packageName
+}
+catch
+{
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
---
+++ smartgit/tools/chocolateyUninstall.ps1
@@ -0,0 +1,14 @@
+$packageName = "smartgit"
+$appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
+
+try {
+    if (Test-Path $appDir)
+    {
+      Remove-Item "$($appDir)" -Recurse -Force
+    }
+
+    Write-ChocolateySuccess $packageName
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
