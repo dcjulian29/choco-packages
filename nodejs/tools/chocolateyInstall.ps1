282dd03d2d000fefdb22ee15b08807459a748c64
Julian Easterling <julian@julianscorner.com>
2015-01-05 00:21:19 -0500

    Updated node.js to 0.10.35

--- nodejs/tools/chocolateyInstall.ps1
+++ nodejs/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = "nodejs"
 $installerType = "MSI"
 $installerArgs = "/qb"
-$version = "0.10.34"
+$version = "0.10.35"
 $url = "http://nodejs.org/dist/v$version/node-v$version-x86.msi"
 $url64 = "http://nodejs.org/dist/v$version/x64/node-v$version-x64.msi"
 
