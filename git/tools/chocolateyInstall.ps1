a96f4059c8f34f06010eb7ece36a472ed42469fb
Julian Easterling <julian@julianscorner.com>
2015-01-14 14:43:41 -0500

    Removed the symbolic link creation during the git install. git configuration will be replaced by a package.

--- git/tools/chocolateyInstall.ps1
+++ git/tools/chocolateyInstall.ps1
@@ -2,26 +2,16 @@ $packageName = "git"
 $installerType = "EXE"
 $installerArgs = '/SILENT /COMPONENTS="!ext,!ext\cheetah,!assoc,!assoc_sh"'
 $url = "https://github.com/msysgit/msysgit/releases/download/Git-1.9.5-preview20141217/Git-1.9.5-preview20141217.exe"
-$toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try
-{
+try {
     Install-ChocolateyPackage $packageName $installerType $installerArgs $url
 
-    if (Test-ProcessAdminRights) {
-        . "$toolDir\postInstall.ps1"
-    } else {
-        Start-ChocolateyProcessAsAdmin ". $toolDir\postInstall.ps1"
-    }
-
     Write-ChocolateySuccess $packageName
-}
-catch
-{
+} catch {
     Write-ChocolateyFailure $packageName $($_.Exception.Message)
     throw
 }
