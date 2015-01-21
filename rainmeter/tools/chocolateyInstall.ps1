eb2dbbd2008b910d6740c9324f4cd3e3090a2390
Julian Easterling <julian@julianscorner.com>
2015-01-21 12:12:34 -0500

    Updated rainmeter to 3.1.0.2290

--- rainmeter/tools/chocolateyInstall.ps1
+++ rainmeter/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = "rainmeter"
 $installerType = "EXE"
 $installerArgs = "/S /STARTUP=1 /ALLUSERS=1"
-$url = "https://github.com/rainmeter/rainmeter/releases/download/v3.1.0.2190/Rainmeter-3.1.exe"
+$url = "https://github.com/rainmeter/rainmeter/releases/download/v3.1.0.2290/Rainmeter-3.1.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
