74446b5a274687e9e1b4489049fbe289a6d43280
Julian Easterling <julian@julianscorner.com>
2015-02-18 10:29:46 -0500

    Added tcptrace and proxytrace packages.

---
+++ proxytrace/tools/chocolateyUninstall.ps1
@@ -0,0 +1,14 @@
+$packageName = "proxytrace"
+$appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
+
+try {
+    if (Test-Path $appDir)
+    {
+      Remove-Item "$($appDir)" -Recurse -Force
+    }
+
+    Write-ChocolateySuccess $packageName
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
---
+++ tcptrace/tools/chocolateyInstall.ps1
@@ -0,0 +1,31 @@
+$packageName = "tcptrace"
+$url = "http://www.pocketsoap.com/tcptrace/tcpTrace081.zip"
+$downloadPath = "$($env:TEMP)\chocolatey\$($packageName)"
+$appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+}
+
+try {
+    if (-not (Test-Path $downloadPath)) {
+        New-Item -Type Directory -Path $downloadPath | Out-Null
+    }
+
+    Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url
+    Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\$packageName"
+
+    if (Test-Path $appDir) {
+        Write-Output "Removing previous version of package..."
+        Remove-Item -Path $appDir -Recurse -Force
+    }
+
+    New-Item -Type Directory -Path $appDir | Out-Null
+
+    Copy-Item -Path "$downloadPath\$packageName\*" -Destination "$appDir\"
+
+    Write-ChocolateySuccess $packageName
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
