ec63d48da0b3697f6f83dc8e6494262e0e8815a9
Julian Easterling <julian@julianscorner.com>
2015-02-21 10:07:13 -0500

    updated myscripts-powershell to 2015.2.20

--- myscripts-powershell/tools/chocolateyInstall.ps1
+++ myscripts-powershell/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "myscripts-powershell"
 $appDir = "$($env:SYSTEMDRIVE)\tools\powershell"
-$version = "2015.2.19"
+$version = "2015.2.20"
 $repo = "scripts-powershell"
 $url = "https://github.com/dcjulian29/$repo/archive/$version.zip"
 $toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
