4ae4fcb7f97bf4b9ce1786d3e71f7fd9b331170a
Julian Easterling <julian@julianscorner.com>
2015-03-06 12:41:28 -0500

    during an upgrade, Chocolatey doesn't always seem to delete old version of the downloaded installer, so let's explicity delete the files...

--- btsync/tools/chocolateyInstall.ps1
+++ btsync/tools/chocolateyInstall.ps1
@@ -2,6 +2,7 @@ $packageName = "btsync"
 $installerType = "EXE"
 $installerArgs = "/PERFORMINSTALL /AUTOMATION"
 $url = "http://download.usyncapp.com/updates/BTSync.exe"
+$downloadPath = "$($env:TEMP)\chocolatey\$($packageName)"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
@@ -12,6 +13,12 @@ try {
         Get-Process -Name BTSync | Stop-Process
     }
 
+    if (Test-Path $downloadPath) {
+        Remove-Item -Path $downloadPath -Recurse -Force
+    }
+
+    New-Item -Type Directory -Path $downloadPath | Out-Null
+    
     Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64 -validExitCodes 1
 
     if (Test-ProcessAdminRights) {
