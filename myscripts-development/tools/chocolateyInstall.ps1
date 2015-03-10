3ca66c60f910412a5a453e785aa4fe288a10799c
Julian Easterling <julian@julianscorner.com>
2015-03-10 11:13:27 -0400

    updated my scripts development and PowerShell to 2015.3.10

--- myscripts-development/tools/chocolateyInstall.ps1
+++ myscripts-development/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "myscripts-development"
 $appDir = "$($env:SYSTEMDRIVE)\tools\development"
-$version = "2015.2.23"
+$version = "2015.3.10"
 $repo = "scripts-development"
 $url = "https://github.com/dcjulian29/$repo/archive/$version.zip"
 
