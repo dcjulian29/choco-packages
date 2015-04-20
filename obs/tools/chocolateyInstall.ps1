649d290b340a4680306236f61ea8c1a1f5c398d3
Julian Easterling <julian@julianscorner.com>
2015-04-20 14:03:32 -0400

    updated obs to 0.651

--- obs/tools/chocolateyInstall.ps1
+++ obs/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = "obs"
 $installerType = "EXE"
 $installerArgs = "/S"
-$url = "https://github.com/jp9000/OBS/releases/download/0.638b/OBS_0_638b_Installer.exe"
+$url = "https://github.com/jp9000/OBS/releases/download/0.651b/OBS_0_651b_Installer.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
