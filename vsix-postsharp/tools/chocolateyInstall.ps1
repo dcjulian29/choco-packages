d727930dfb0054c3ea866631b13098d4ee650cdd
Julian Easterling <julian@julianscorner.com>
2015-04-20 23:46:47 -0400

    updated postsharp to 4.1.11

--- vsix-postsharp/tools/chocolateyInstall.ps1
+++ vsix-postsharp/tools/chocolateyInstall.ps1
@@ -1,17 +1,10 @@
 $packageName = "vsix-postsharp"
 $vsgallery = "http://visualstudiogallery.msdn.microsoft.com"
-$vsix = "a058d5d3-e654-43f8-a308-c3bdfdd0be4a/file/89212/52/PostSharp-4.0.41.vsix"
+$vsix = "a058d5d3-e654-43f8-a308-c3bdfdd0be4a/file/89212/55/PostSharp-4.1.11.exe"
 $url = "$vsgallery/$vsix"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try {
-    Install-ChocolateyVsixPackage $packageName $url
-
-    Write-ChocolateySuccess $packageName
-} catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
-}
+Install-ChocolateyVsixPackage $packageName $url
