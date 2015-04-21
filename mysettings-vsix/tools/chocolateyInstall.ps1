10c423290e7db5efbf419abc38f4812f31ccf873
Julian Easterling <julian@julianscorner.com>
2015-04-20 23:47:13 -0400

    added the CodeMaid VS extension.

--- mysettings-vsix/tools/chocolateyInstall.ps1
+++ mysettings-vsix/tools/chocolateyInstall.ps1
@@ -4,14 +4,4 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try
-{
-    Write-Output "Visual Studio VSIX Virtual Package."
-
-    Write-ChocolateySuccess $packageName
-}
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
-}
+Write-Output "Visual Studio VSIX Virtual Package."
