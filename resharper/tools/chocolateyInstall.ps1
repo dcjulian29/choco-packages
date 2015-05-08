d61b91edfc5d3b54262fdbfdf299480218c1ddfa
Julian Easterling <julian@julianscorner.com>
2015-05-07 22:03:23 -0400

    updated resharper to 9.1

--- resharper/tools/chocolateyInstall.ps1
+++ resharper/tools/chocolateyInstall.ps1
@@ -1,19 +1,10 @@
 $packageName = "resharper"
 $installerType = "EXE"
 $installerArgs = "/SpecificProductNames=ReSharper /Silent=True"
-$url = 'http://download.jetbrains.com/resharper/ReSharperAndToolsPacked01Update1.exe'
+$url = "https://download.jetbrains.com/resharper/JetBrains.ReSharperUltimate.2015.1.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try {
-    Install-ChocolateyPackage $packageName $installerType $installerArgs $url
-
-    Write-ChocolateySuccess $packageName
-}
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
-}
+Install-ChocolateyPackage $packageName $installerType $installerArgs $url
--- resharper/tools/chocolateyUninstall.ps1
+++ resharper/tools/chocolateyUninstall.ps1
@@ -1,22 +1,13 @@
 $packageName = "resharper"
 $installerType = "EXE"
 $installerArgs = "/SpecificProductNames=ReSharper /HostsToRemove=ReSharperPlatformVs12;ReSharperPlatformVs14 /Hive=* /ReSharper9PlusMsi=True"
-$url = "http://download.jetbrains.com/resharper/ReSharperAndToolsPacked01Update1.exe"
+$url = "https://download.jetbrains.com/resharper/JetBrains.ReSharperUltimate.2015.1.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try {
-    Push-Location $env:TEMP
-    Get-WebFile $url
-    Uninstall-ChocolateyPackage $packageName $installerType $installerArgs "ReSharperAndToolsPacked01Update1.exe"
-    Pop-Location
-
-    Write-ChocolateySuccess $packageName
-}
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
-}
+Push-Location $env:TEMP
+Get-WebFile $url
+Uninstall-ChocolateyPackage $packageName $installerType $installerArgs "ReSharperAndToolsPacked01Update1.exe"
+Pop-Location
