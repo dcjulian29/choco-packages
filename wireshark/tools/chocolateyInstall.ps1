bfe7f18b6c33096eeea04c734ca5cb56b1fc1100
Julian Easterling <julian@julianscorner.com>
2015-05-15 09:17:47 -0400

    updated wireshark to 1.12.5

--- wireshark/tools/chocolateyInstall.ps1
+++ wireshark/tools/chocolateyInstall.ps1
@@ -1,18 +1,11 @@
 $packageName = "wireshark"
 $installerType = "EXE"
 $installerArgs = "/S"
-$url = "https://2.na.dl.wireshark.org/win32/Wireshark-win32-1.12.4.exe"
-$url64 = "https://2.na.dl.wireshark.org/win64/Wireshark-win64-1.12.4.exe"
+$url = "https://2.na.dl.wireshark.org/win32/Wireshark-win32-1.12.5.exe"
+$url64 = "https://2.na.dl.wireshark.org/win64/Wireshark-win64-1.12.5.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try {
-    Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
-
-    Write-ChocolateySuccess $packageName
-} catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
-}
+Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
