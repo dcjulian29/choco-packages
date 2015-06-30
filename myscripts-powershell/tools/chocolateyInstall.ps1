0f3af9ee92b8ae1ca3d2e0fb41ad13c1f89a55c8
Julian Easterling <julian@julianscorner.com>
2015-06-30 15:40:07 -0400

    updated my PowerShell Scripts

--- myscripts-powershell/tools/chocolateyInstall.ps1
+++ myscripts-powershell/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "myscripts-powershell"
 $appDir = "$($env:SYSTEMDRIVE)\tools\powershell"
-$version = "2015.6.27"
+$version = "2015.6.30"
 $repo = "scripts-powershell"
 $url = "https://github.com/dcjulian29/$repo/archive/$version.zip"
 $toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
@@ -9,45 +9,37 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try {
-    if (Test-Path $appDir\Modules) {
-        cmd /c rmdir "$appdir\Modules"
-    }
+if (Test-Path $appDir\Modules) {
+    cmd /c rmdir "$appdir\Modules"
+}
 
-    if (Test-Path $appDir)
-    {
-        Write-Output "Removing previous version of package..."
-        Remove-Item "$($appDir)\*" -Recurse -Force
-    }
+if (Test-Path $appDir)
+{
+    Write-Output "Removing previous version of package..."
+    Remove-Item "$($appDir)\*" -Recurse -Force
+}
 
-    if (-not (Test-Path $appDir))
-    {
-        New-Item -Type Directory -Path $appDir | Out-Null
-    }
+if (-not (Test-Path $appDir))
+{
+    New-Item -Type Directory -Path $appDir | Out-Null
+}
 
+$file = "$repo-$version"
 
-    $file = "$repo-$version"
+(New-Object System.Net.WebClient).DownloadFile("$url", "$env:TEMP\$file.zip")
 
-    (New-Object System.Net.WebClient).DownloadFile("$url", "$env:TEMP\$file.zip")
+[System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
+[System.IO.Compression.ZipFile]::ExtractToDirectory("$env:TEMP\$file.zip", $env:TEMP)
 
-    [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
-    [System.IO.Compression.ZipFile]::ExtractToDirectory("$env:TEMP\$file.zip", $env:TEMP)
-    
-    Copy-Item -Path "$($env:TEMP)\$file\*" -Destination $appdir -Recurse -Force
+Copy-Item -Path "$($env:TEMP)\$file\*" -Destination $appdir -Recurse -Force
 
-    Remove-Item -Path "$($env:TEMP)\$file" -Recurse -Force
-    Remove-Item -Path "$($env:TEMP)\$file.zip" -Force
+Remove-Item -Path "$($env:TEMP)\$file" -Recurse -Force
+Remove-Item -Path "$($env:TEMP)\$file.zip" -Force
 
-    $cmd = ". $toolDir\postInstall.bat"
+$cmd = ". $toolDir\postInstall.bat"
 
-    if (Test-ProcessAdminRights) {
-        Invoke-Expression $cmd
-    } else {
-        Start-ChocolateyProcessAsAdmin $cmd
-    }
-
-    Write-ChocolateySuccess $packageName
-} catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+if (Test-ProcessAdminRights) {
+    Invoke-Expression $cmd
+} else {
+    Start-ChocolateyProcessAsAdmin $cmd
 }
