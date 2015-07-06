dcd2df38c43b01b6a22ecfe61cdb07825a6c1aea
Julian Easterling <julian@julianscorner.com>
2015-07-06 14:04:10 -0400

    updated the way nuget explorer is install as the Chocolatey way "Install-ChocolateyZipPackage" stopped working recently.

--- nugetexplorer/tools/chocolateyInstall.ps1
+++ nugetexplorer/tools/chocolateyInstall.ps1
@@ -1,42 +1,47 @@
 $packageName = "nugetexplorer"
 $url = "http://npe.codeplex.com/downloads/get/827368"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
+$downloadPath = "$env:TEMP\chocolatey\$packageName"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try {
-    if (Test-Path $appDir) {
-      Write-Output "Removing previous version of package..."
-        Remove-Item "$($appDir)" -Recurse -Force
-    }
+if (Test-Path $appDir) {
+    Write-Output "Removing previous version of package..."
+    Remove-Item "$($appDir)" -Recurse -Force
+}
 
-    Install-ChocolateyZipPackage $packageName $url $appDir
+if (-not (Test-Path $downloadPath)) {
+    New-Item -Type Directory -Path $downloadPath | Out-Null
+}
 
-    $testType = (cmd /c assoc ".nupkg")
-    if ($testType -ne $null) {
-        $fileType = $testType.Split("=")[1]
-    } else {
-        $fileType = "Nuget.Package"
-        $cmd = "cmd /c assoc .nupkg=$fileType"
-        if (Test-ProcessAdminRights) {
-            Invoke-Expression $cmd
-        } else {
-            Start-ChocolateyProcessAsAdmin $cmd
-        }
-    }
+Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url $url
+Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\$packageName" | Out-Null
 
-    $cmd = "cmd /c ftype $fileType=`"$($appDir)\NuGetPackageExplorer.exe`" %1"
+if (-not (Test-Path $appDir)) {
+    New-Item -Type Directory -Path $appDir | Out-Null
+}
 
+Get-ChildItem -Path $downloadPath\$packageName | Copy-Item -Destination "$appDir"
+
+$testType = (cmd /c assoc ".nupkg")
+if ($testType -ne $null) {
+    $fileType = $testType.Split("=")[1]
+} else {
+    $fileType = "Nuget.Package"
+    $cmd = "cmd /c assoc .nupkg=$fileType"
     if (Test-ProcessAdminRights) {
         Invoke-Expression $cmd
     } else {
         Start-ChocolateyProcessAsAdmin $cmd
     }
+}
 
-    Write-ChocolateySuccess $packageName
-} catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+$cmd = "cmd /c ftype $fileType=`"$($appDir)\NuGetPackageExplorer.exe`" %1"
+
+if (Test-ProcessAdminRights) {
+    Invoke-Expression $cmd
+} else {
+    Start-ChocolateyProcessAsAdmin $cmd
 }
--- nugetexplorer/tools/chocolateyUninstall.ps1
+++ nugetexplorer/tools/chocolateyUninstall.ps1
@@ -1,26 +1,16 @@
 $packageName = "nugetexplorer"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
 
-try
+if (Test-Path $appDir)
 {
-    if (Test-Path $appDir)
-    {
-      Remove-Item "$($appDir)" -Recurse -Force
-    }
-
-    $testType = (cmd /c assoc ".nupkg")
-    if ($testType -ne $null) {
-        $fileType = $testType.Split("=")[1]
-    } else {
-        $fileType = "Nuget.Package"
-    }
-
-    Start-ChocolateyProcessAsAdmin "cmd /c ftype $fileType="
-
-    Write-ChocolateySuccess $packageName
+    Remove-Item "$($appDir)" -Recurse -Force
 }
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+
+$testType = (cmd /c assoc ".nupkg")
+if ($testType -ne $null) {
+    $fileType = $testType.Split("=")[1]
+} else {
+    $fileType = "Nuget.Package"
 }
+
+Start-ChocolateyProcessAsAdmin "cmd /c ftype $fileType="
