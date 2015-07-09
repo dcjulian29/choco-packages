ee2def417b7b71051edaa2bcfe2507bdabe42f96
Julian Easterling <julian@julianscorner.com>
2015-07-08 20:15:50 -0400

    updated teamcity to 9.0.5

--- teamcity/tools/chocolateyUninstall.ps1
+++ teamcity/tools/chocolateyUninstall.ps1
@@ -1,24 +1,17 @@
 $packageName = "teamcity"
 $appDir = "$($env:SYSTEMDRIVE)\TeamCity"
 
-try {
-    net stop TCBuildAgent
+net stop TCBuildAgent
 
-    Push-Location $appdir\buildAgent\bin
-    & ./service.uninstall.bat
-    Pop-Location
+Push-Location $appdir\buildAgent\bin
+& ./service.uninstall.bat
+Pop-Location
 
-    net stop TeamCity
+net stop TeamCity
 
-    & $appdir\bin\teamcity-server.bat service delete
-    
-    if (Test-Path $appDir)
-    {
-      Remove-Item "$($appDir)" -Recurse -Force
-    }
-    
-    Write-ChocolateySuccess $packageName
-} catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+& $appdir\bin\teamcity-server.bat service delete
+
+if (Test-Path $appDir)
+{
+  Remove-Item "$($appDir)" -Recurse -Force
 }
