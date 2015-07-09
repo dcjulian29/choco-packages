ef4737b1a32736f0cbb413f89c37572d739ca04e
Julian Easterling <julian@julianscorner.com>
2015-07-08 20:16:11 -0400

    updated jslint VSIX package to 1.8.3

--- vsix-jslint/tools/chocolateyInstall.ps1
+++ vsix-jslint/tools/chocolateyInstall.ps1
@@ -1,17 +1,10 @@
 $packageName = "vsix-jslint"
 $vsgallery = "http://visualstudiogallery.msdn.microsoft.com"
-$vsix = "ede12aa8-0f80-4e6f-b15c-7a8b3499370e/file/111592/20/JSLintNet.VisualStudio.1.8.1.vsix"
+$vsix = "ede12aa8-0f80-4e6f-b15c-7a8b3499370e/file/111592/20/JSLintNet.VisualStudio.1.8.3.vsix"
 $url = "$vsgallery/$vsix"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try {
-    Install-ChocolateyVsixPackage $packageName $url
-
-    Write-ChocolateySuccess $packageName
-} catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
-}
+Install-ChocolateyVsixPackage $packageName $url
