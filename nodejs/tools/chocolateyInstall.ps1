5b391aa26dac8166d8c1c994f1b2a24aac5a5e2c
Julian Easterling <julian@julianscorner.com>
2015-07-12 10:39:17 -0400

    updated nodejs to 0.12.7

--- nodejs/tools/chocolateyInstall.ps1
+++ nodejs/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = "nodejs"
 $installerType = "MSI"
 $installerArgs = "/qb"
-$version = "0.12.5"
+$version = "0.12.7"
 $url = "http://nodejs.org/dist/v$version/node-v$version-x86.msi"
 $url64 = "http://nodejs.org/dist/v$version/x64/node-v$version-x64.msi"
 
@@ -9,14 +9,4 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try
-{
-    Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
-
-    Write-ChocolateySuccess $packageName
-}
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
-}
+Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
