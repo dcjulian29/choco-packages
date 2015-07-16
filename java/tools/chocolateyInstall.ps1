3f99c582d9b5695da6933f9c6612c416ca1d5050
Julian Easterling <julian@julianscorner.com>
2015-07-15 21:18:08 -0400

    updated java to 8.0.51

--- java/tools/chocolateyInstall.ps1
+++ java/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 $installerType = "exe"
 $installerArgs = "/s REBOOT=Suppress WEB_JAVA=0"
 
-$url = "http://javadl.sun.com/webapps/download/AutoDL?BundleId=106246"
-$url64 = "http://javadl.sun.com/webapps/download/AutoDL?BundleId=106248"
+$url = "http://javadl.sun.com/webapps/download/AutoDL?BundleId=107943"
+$url64 = "http://javadl.sun.com/webapps/download/AutoDL?BundleId=107944"
 
 $toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
 
@@ -11,40 +11,33 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try {
-    Write-Output "Checking for and uninstalling previous versions of Java..."
+Write-Output "Checking for and uninstalling previous versions of Java..."
 
-    . $toolDir\chocolateyUninstall.ps1
+. $toolDir\chocolateyUninstall.ps1
 
-    Install-ChocolateyPackage $packageName $installerType $installerArgs $url
-  
-    $path = Join-Path $env:ProgramFiles "Java"
-    
-    if ((Get-WmiObject Win32_Processor).AddressWidth -eq 64) { 
-        Install-ChocolateyPackage $packageName $installerType $installerArgs "" $url64
-        $path = Join-Path $env:ProgramW6432 "Java"
-    }
+Install-ChocolateyPackage $packageName $installerType $installerArgs $url
 
-    $path  = (Get-ChildItem -Path $path -Recurse -Include "bin").FullName `
-        | Sort-Object | Select-Object -Last 1
+$path = Join-Path $env:ProgramFiles "Java"
 
-    $path = Split-Path -Path $path -Parent
-    $cmd = "[Environment]::SetEnvironmentVariable('JAVA_HOME','$path', 'Machine')"
+if ((Get-WmiObject Win32_Processor).AddressWidth -eq 64) { 
+    Install-ChocolateyPackage $packageName $installerType $installerArgs "" $url64
+    $path = Join-Path $env:ProgramW6432 "Java"
+}
 
-    if (Test-ProcessAdminRights) {
-        Invoke-Expression $cmd
-    } else {
-        Start-ChocolateyProcessAsAdmin $cmd
-    }
+$path  = (Get-ChildItem -Path $path -Recurse -Include "bin").FullName `
+    | Sort-Object | Select-Object -Last 1
 
-    if (Test-ProcessAdminRights) {
-        . $toolDir\postInstall.ps1
-    } else {
-        Start-ChocolateyProcessAsAdmin ". $toolDir\postInstall.ps1"
-    }
+$path = Split-Path -Path $path -Parent
+$cmd = "[Environment]::SetEnvironmentVariable('JAVA_HOME','$path', 'Machine')"
 
-    Write-ChocolateySuccess $packageName
-} catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+if (Test-ProcessAdminRights) {
+    Invoke-Expression $cmd
+} else {
+    Start-ChocolateyProcessAsAdmin $cmd
+}
+
+if (Test-ProcessAdminRights) {
+    . $toolDir\postInstall.ps1
+} else {
+    Start-ChocolateyProcessAsAdmin ". $toolDir\postInstall.ps1"
 }
--- java/tools/chocolateyUninstall.ps1
+++ java/tools/chocolateyUninstall.ps1
@@ -6,30 +6,23 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try {
-    $uninstallPaths = @(
-        'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall', 
-        'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall') 
+$uninstallPaths = @(
+    'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall', 
+    'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall') 
 
-    foreach ($path in $uninstallPaths) { 
-        if (Test-Path $path) { 
-            Get-ChildItem $path | Where-Object $searchFilter | `
-                Where-Object { $_.GetValue('UninstallString') } | `
-                Foreach-Object { 
-                    Start-Process -Wait `
-                        "${env:WINDIR}\System32\msiexec.exe" "/x $($_.PSChildName) /qb"
-                }
-        } 
+foreach ($path in $uninstallPaths) { 
+    if (Test-Path $path) { 
+        Get-ChildItem $path | Where-Object $searchFilter | `
+            Where-Object { $_.GetValue('UninstallString') } | `
+            Foreach-Object { 
+                Start-Process -Wait `
+                    "${env:WINDIR}\System32\msiexec.exe" "/x $($_.PSChildName) /qb"
+            }
     } 
+} 
 
-    if (Test-ProcessAdminRights) {
-        . $toolDir\postUninstall.ps1
-    } else {
-        Start-ChocolateyProcessAsAdmin ". $toolDir\postUninstall.ps1"
-    }
-
-    Write-ChocolateySuccess $packageName
-} catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+if (Test-ProcessAdminRights) {
+    . $toolDir\postUninstall.ps1
+} else {
+    Start-ChocolateyProcessAsAdmin ". $toolDir\postUninstall.ps1"
 }
