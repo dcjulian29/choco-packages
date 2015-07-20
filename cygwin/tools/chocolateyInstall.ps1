30d52ab5d60add106b306119a17bf62837912e0b
Julian Easterling <julian@julianscorner.com>
2015-07-20 12:13:04 -0400

    Updated Cygwin to 2.1.0

--- cygwin/tools/chocolateyInstall.ps1
+++ cygwin/tools/chocolateyInstall.ps1
@@ -12,95 +12,85 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try {
-    $upgrade = $false
+$upgrade = $false
 
-    if (Test-Path $appDir)
-    {
-        # Check to see if the Cygwin package was installed via chocolatey
-        $package = (Get-ChildItem "$($env:ChocolateyInstall)\lib" | Select-Object basename).basename `
-            | Where-Object { $_.StartsWith($packageName) }
+if (Test-Path $appDir)
+{
+    # Check to see if the Cygwin package was installed via chocolatey
+    $package = (Get-ChildItem "$($env:ChocolateyInstall)\lib" | Select-Object basename).basename `
+        | Where-Object { $_.StartsWith($packageName) }
 
-        if ($package.Count -gt 1) {
-            Write-Warning "This package has already been installed, will attempt to upgrade..."
-            $upgrade = $true
-        } else {
-            throw "Cygwin appears to already be installed on this system but not by this packaging system... Removed the Cygwin directory and try again."
-        }
+    if ($package.Count -gt 1) {
+        Write-Warning "This package has already been installed, will attempt to upgrade..."
+        $upgrade = $true
+    } else {
+        throw "Cygwin appears to already be installed on this system but not by this packaging system... Removed the Cygwin directory and try again."
     }
+}
 
-    if (-not $upgrade) {
-        New-Item $appDir -Type Directory
-
-        if (Test-ProcessAdminRights) {
-            cmd /c mklink /J "$env:SYSTEMDRIVE\cygwin\home" "$env:SYSTEMDRIVE\Users"
-        } else {
-            Start-ChocolateyProcessAsAdmin "cmd /c mklink /J '$env:SYSTEMDRIVE\cygwin\home' '$env:SYSTEMDRIVE\Users'"
-        }
-    }
-
-    if (-not (Test-Path $cygwinSetupDir)) {
-        New-Item $cygwinSetupDir -Type Directory
-    }
-
-    if (Test-Path "$cygwinSetupDir\setup.exe") {
-        Remove-Item "$cygwinSetupDir\setup.exe" -Force
-    }
-
-    Get-ChocolateyWebFile $packageName "$cygwinSetupDir\setup.exe" $url $url64
-
-    $cygwinSetupArgs = "--root $appDir --no-admin --no-shortcuts --site $cygwinMirror --quiet-mode"
-
-    Push-Location $cygwinSetupDir
-
-    cmd.exe /c "setup.exe $($cygwinSetupArgs)"
-
-    if (-not $upgrade) {
-        cmd.exe /c "setup.exe $($cygwinSetupArgs) -P bcrypt"
-        cmd.exe /c "setup.exe $($cygwinSetupArgs) -P curl"
-        cmd.exe /c "setup.exe $($cygwinSetupArgs) -P dos2unix"
-        cmd.exe /c "setup.exe $($cygwinSetupArgs) -P rxvt"
-        cmd.exe /c "setup.exe $($cygwinSetupArgs) -P rsync"
-        cmd.exe /c "setup.exe $($cygwinSetupArgs) -P openssh"
-        cmd.exe /c "setup.exe $($cygwinSetupArgs) -P nc"
-        cmd.exe /c "setup.exe $($cygwinSetupArgs) -P nc6"
-        cmd.exe /c "setup.exe $($cygwinSetupArgs) -P ncurses"
-        cmd.exe /c "setup.exe $($cygwinSetupArgs) -P inetutils"
-        cmd.exe /c "setup.exe $($cygwinSetupArgs) -P util-linux"
-        cmd.exe /c "setup.exe $($cygwinSetupArgs) -P unzip"
-        cmd.exe /c "setup.exe $($cygwinSetupArgs) -P wget"
-        cmd.exe /c "setup.exe $($cygwinSetupArgs) -P zip"
-
-        Write-Output "Generating User Account Entries..."
-
-        if (Test-Path $appDir\etc\passwd) {
-            Remove-Item "$appDir\etc\passwd" -Force
-        }
-
-        cmd.exe /c "$appDir\bin\mkpasswd.exe -l -d > $appDir\etc\passwd"
-
-        Write-Output "Generating Group Entries..."
-
-        if (Test-Path $appDir\etc\group) {
-            Remove-Item "$appDir\etc\group" -Force
-        }
-
-        cmd.exe /c "$appDir\bin\mkgroup.exe -l -d > $appDir\etc\group"
-    }
-
-    Pop-Location
+if (-not $upgrade) {
+    New-Item $appDir -Type Directory
 
     if (Test-ProcessAdminRights) {
-        . $toolDir\postInstall.ps1
+        cmd /c mklink /J "$env:SYSTEMDRIVE\cygwin\home" "$env:SYSTEMDRIVE\Users"
     } else {
-        Start-ChocolateyProcessAsAdmin ". $toolDir\postInstall.ps1"
+        Start-ChocolateyProcessAsAdmin "cmd /c mklink /J '$env:SYSTEMDRIVE\cygwin\home' '$env:SYSTEMDRIVE\Users'"
     }
-    
-
-    Write-ChocolateySuccess $packageName
 }
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+
+if (-not (Test-Path $cygwinSetupDir)) {
+    New-Item $cygwinSetupDir -Type Directory
+}
+
+if (Test-Path "$cygwinSetupDir\setup.exe") {
+    Remove-Item "$cygwinSetupDir\setup.exe" -Force
+}
+
+Get-ChocolateyWebFile $packageName "$cygwinSetupDir\setup.exe" $url $url64
+
+$cygwinSetupArgs = "--root $appDir --no-admin --no-shortcuts --site $cygwinMirror --quiet-mode"
+
+Push-Location $cygwinSetupDir
+
+cmd.exe /c "setup.exe $($cygwinSetupArgs)"
+
+if (-not $upgrade) {
+    cmd.exe /c "setup.exe $($cygwinSetupArgs) -P bcrypt"
+    cmd.exe /c "setup.exe $($cygwinSetupArgs) -P curl"
+    cmd.exe /c "setup.exe $($cygwinSetupArgs) -P dos2unix"
+    cmd.exe /c "setup.exe $($cygwinSetupArgs) -P rxvt"
+    cmd.exe /c "setup.exe $($cygwinSetupArgs) -P rsync"
+    cmd.exe /c "setup.exe $($cygwinSetupArgs) -P openssh"
+    cmd.exe /c "setup.exe $($cygwinSetupArgs) -P nc"
+    cmd.exe /c "setup.exe $($cygwinSetupArgs) -P nc6"
+    cmd.exe /c "setup.exe $($cygwinSetupArgs) -P ncurses"
+    cmd.exe /c "setup.exe $($cygwinSetupArgs) -P inetutils"
+    cmd.exe /c "setup.exe $($cygwinSetupArgs) -P util-linux"
+    cmd.exe /c "setup.exe $($cygwinSetupArgs) -P unzip"
+    cmd.exe /c "setup.exe $($cygwinSetupArgs) -P wget"
+    cmd.exe /c "setup.exe $($cygwinSetupArgs) -P zip"
+
+    Write-Output "Generating User Account Entries..."
+
+    if (Test-Path $appDir\etc\passwd) {
+        Remove-Item "$appDir\etc\passwd" -Force
+    }
+
+    cmd.exe /c "$appDir\bin\mkpasswd.exe -l -d > $appDir\etc\passwd"
+
+    Write-Output "Generating Group Entries..."
+
+    if (Test-Path $appDir\etc\group) {
+        Remove-Item "$appDir\etc\group" -Force
+    }
+
+    cmd.exe /c "$appDir\bin\mkgroup.exe -l -d > $appDir\etc\group"
+}
+
+Pop-Location
+
+if (Test-ProcessAdminRights) {
+    . $toolDir\postInstall.ps1
+} else {
+    Start-ChocolateyProcessAsAdmin ". $toolDir\postInstall.ps1"
 }
--- cygwin/tools/chocolateyUninstall.ps1
+++ cygwin/tools/chocolateyUninstall.ps1
@@ -2,97 +2,87 @@ $packageName = "cygwin"
 $appDir = "$($env:SYSTEMDRIVE)\$($packageName)"
 $toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
 
-try
+if (Test-Path $appDir)
 {
-    if (Test-Path $appDir)
-    {
-        $remove = "Remove-Item '$($appDir)' -Recurse -Force"
-        Start-ChocolateyProcessAsAdmin $remove
+    $remove = "Remove-Item '$($appDir)' -Recurse -Force"
+    Start-ChocolateyProcessAsAdmin $remove
+
+    $cmd = "reg.exe import $toolDir\registry.reg"
     
-        $cmd = "reg.exe import $toolDir\registry.reg"
-        
+    if (Test-ProcessAdminRights) {
+        Invoke-Expression $cmd
+    } else {
+        Start-ChocolateyProcessAsAdmin "$cmd"
+    }
+
+    if (Get-ProcessorBits -eq 64) {
+        $cmd = "$cmd /reg:64"
         if (Test-ProcessAdminRights) {
             Invoke-Expression $cmd
         } else {
             Start-ChocolateyProcessAsAdmin "$cmd"
         }
-
-        if (Get-ProcessorBits -eq 64) {
-            $cmd = "$cmd /reg:64"
-            if (Test-ProcessAdminRights) {
-                Invoke-Expression $cmd
-            } else {
-                Start-ChocolateyProcessAsAdmin "$cmd"
-            }
-        }
     }
-
-    if (Test-Path "$($env:USERPROFILE)\.ssh") {
-        (Get-Item "$($env:USERPROFILE)\.ssh").Delete()
-    }
-
-    (Get-Item "$($env:USERPROFILE)\.bash_history").Delete()
-    (Get-Item "$($env:USERPROFILE)\.bash_logout").Delete()
-    (Get-Item "$($env:USERPROFILE)\.bash_profile").Delete()
-    (Get-Item "$($env:USERPROFILE)\.bashrc").Delete()
-    (Get-Item "$($env:USERPROFILE)\.inputrc").Delete()
-    (Get-Item "$($env:USERPROFILE)\.minttyrc").Delete()
-    (Get-Item "$($env:USERPROFILE)\.profile").Delete()
-    (Get-Item "$($env:USERPROFILE)\.Xresources").Delete()
-
-    $links = @(
-    "base64"
-    "bcrypt"
-    "bunzip2"
-    "bzcat"
-    "bzip2"
-    "cat"
-    "cksum"
-    "curl"
-    "diff"
-    "dos2unix"
-    "gawk"
-    "grep"
-    "gzip"
-    "head"
-    "hexdump"
-    "hostid"
-    "hostname"
-    "id"
-    "lynx"
-    "md5sum"
-    "nc"
-    "nc6"
-    "openssl"
-    "rsync"
-    "scp"
-    "sed"
-    "setfacl"
-    "sftp"
-    "sha1sum"
-    "sha256sum"
-    "sha512sum"
-    "ssh"
-    "tar"
-    "tail"
-    "telnet"
-    "touch"
-    "unix2dos"
-    "unzip"
-    "uuidgen"
-    "wget"
-    "xargs"
-    "zip"
-    )
-
-    foreach ($link in $links) {
-        (Get-Item "${env:ChocolateyInstall}\bin\$link.bat").Delete()
-    }
-
-    Write-ChocolateySuccess $packageName
 }
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+
+if (Test-Path "$($env:USERPROFILE)\.ssh") {
+    (Get-Item "$($env:USERPROFILE)\.ssh").Delete()
+}
+
+(Get-Item "$($env:USERPROFILE)\.bash_history").Delete()
+(Get-Item "$($env:USERPROFILE)\.bash_logout").Delete()
+(Get-Item "$($env:USERPROFILE)\.bash_profile").Delete()
+(Get-Item "$($env:USERPROFILE)\.bashrc").Delete()
+(Get-Item "$($env:USERPROFILE)\.inputrc").Delete()
+(Get-Item "$($env:USERPROFILE)\.minttyrc").Delete()
+(Get-Item "$($env:USERPROFILE)\.profile").Delete()
+(Get-Item "$($env:USERPROFILE)\.Xresources").Delete()
+
+$links = @(
+"base64"
+"bcrypt"
+"bunzip2"
+"bzcat"
+"bzip2"
+"cat"
+"cksum"
+"curl"
+"diff"
+"dos2unix"
+"gawk"
+"grep"
+"gzip"
+"head"
+"hexdump"
+"hostid"
+"hostname"
+"id"
+"lynx"
+"md5sum"
+"nc"
+"nc6"
+"openssl"
+"rsync"
+"scp"
+"sed"
+"setfacl"
+"sftp"
+"sha1sum"
+"sha256sum"
+"sha512sum"
+"ssh"
+"tar"
+"tail"
+"telnet"
+"touch"
+"unix2dos"
+"unzip"
+"uuidgen"
+"wget"
+"xargs"
+"zip"
+)
+
+foreach ($link in $links) {
+    (Get-Item "${env:ChocolateyInstall}\bin\$link.bat").Delete()
 }
