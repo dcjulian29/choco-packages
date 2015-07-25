71c08456aa49ce07d11fcc8f7ad5e95b16a50c04
Julian Easterling <julian@julianscorner.com>
2015-07-25 15:07:14 -0400

    updated nmap to 6.49

--- nmap/tools/chocolateyInstall.ps1
+++ nmap/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = "nmap"
-$version = "6.47"
+$version = "6.49BETA4"
 $url = "http://nmap.org/dist/nmap-$($version)-win32.zip"
 $downloadPath = "$env:TEMP\chocolatey\$packageName"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
@@ -8,38 +8,28 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try
+if (Test-Path $appDir)
 {
-    if (Test-Path $appDir)
-    {
-        Write-Output "Removing previous version of package..."
-        Remove-Item "$($appDir)" -Recurse -Force
-    }
-
-    New-Item -Type Directory -Path $appDir | Out-Null
-    
-    if (-not (Test-Path $downloadPath))
-    {
-        New-Item -Type Directory -Path $downloadPath | Out-Null
-    }
-
-    Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url
-    Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\"
-
-    Copy-Item -Path "$($downloadPath)\nmap-$($version)\*" -Destination "$appDir" -Recurse -Container
-
-    $cmd = "$env:WINDIR\system32\reg.exe import $appDir\nmap_performance.reg"
-    
-    if (Get-ProcessorBits -eq 64) {
-        $cmd = "$cmd /reg:64"
-    }
-    
-    Start-ChocolateyProcessAsAdmin "$cmd"
-
-    Write-ChocolateySuccess $packageName
+    Write-Output "Removing previous version of package..."
+    Remove-Item "$($appDir)" -Recurse -Force
 }
-catch
+
+New-Item -Type Directory -Path $appDir | Out-Null
+
+if (-not (Test-Path $downloadPath))
 {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+    New-Item -Type Directory -Path $downloadPath | Out-Null
 }
+
+Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url
+Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\"
+
+Copy-Item -Path "$($downloadPath)\nmap-$($version)\*" -Destination "$appDir" -Recurse -Container
+
+$cmd = "$env:WINDIR\system32\reg.exe import $appDir\nmap_performance.reg"
+
+if (Get-ProcessorBits -eq 64) {
+    $cmd = "$cmd /reg:64"
+}
+
+Start-ChocolateyProcessAsAdmin "$cmd"
