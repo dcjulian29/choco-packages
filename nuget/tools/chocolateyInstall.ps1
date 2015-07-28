672ca794124b3031bdf2c90f7ab56ad705e11c22
Julian Easterling <julian@julianscorner.com>
2015-07-28 13:34:08 -0400

    update nuget to 2.8.6

--- nuget/tools/chocolateyInstall.ps1
+++ nuget/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = "nuget"
-$url = "http://nuget.codeplex.com/downloads/get/1441482"
+$url = "https://az320820.vo.msecnd.net/downloads/nuget.exe"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
 $toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
 $exe = "$($appDir)\$($packageName).exe"
@@ -8,28 +8,18 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try
+if (Test-Path $appDir)
 {
-    if (Test-Path $appDir)
-    {
-      Write-Output "Removing previous version of package..."
-        Remove-Item "$($appDir)" -Recurse -Force
-    }
-
-    New-Item -Type Directory -Path $appDir | Out-Null
-    
-    Get-ChocolateyWebFile $packageName $exe $url
-
-    if (Test-ProcessAdminRights) {
-        . $toolDir\postInstall.ps1
-    } else {
-        Start-ChocolateyProcessAsAdmin ". $toolDir\postInstall.ps1"
-    }
-
-    Write-ChocolateySuccess $packageName
+  Write-Output "Removing previous version of package..."
+    Remove-Item "$($appDir)" -Recurse -Force
 }
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+
+New-Item -Type Directory -Path $appDir | Out-Null
+
+Get-ChocolateyWebFile $packageName $exe $url
+
+if (Test-ProcessAdminRights) {
+    . $toolDir\postInstall.ps1
+} else {
+    Start-ChocolateyProcessAsAdmin ". $toolDir\postInstall.ps1"
 }
--- nuget/tools/chocolateyUninstall.ps1
+++ nuget/tools/chocolateyUninstall.ps1
@@ -2,21 +2,11 @@ $packageName = "nuget"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
 $link = "$($env:ChocolateyInstall)\bin\nuget.exe"
 
-try
+if (Test-Path $appDir)
 {
-    if (Test-Path $appDir)
-    {
-      Remove-Item "$($appDir)" -Recurse -Force
-    }
-
-    if (Test-Path $link) {
-        Remove-Item $link -Force
-    }
-    
-    Write-ChocolateySuccess $packageName
+  Remove-Item "$($appDir)" -Recurse -Force
 }
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+
+if (Test-Path $link) {
+    Remove-Item $link -Force
 }
