ce7973393a90c78fa39f7f82434cd9e8ac7274af
Julian Easterling <julian@julianscorner.com>
2015-07-28 00:11:33 -0400

    updated i hate regions extension to 1.4.1

--- vsix-ihateregions/tools/chocolateyInstall.ps1
+++ vsix-ihateregions/tools/chocolateyInstall.ps1
@@ -1,17 +1,10 @@
 $packageName = "vsix-ihateregions"
 $vsgallery = "http://visualstudiogallery.msdn.microsoft.com"
-$vsix = "0ca60d35-1e02-43b7-bf59-ac7deb9afbca/file/69113/7/DisableRegions.vsix"
+$vsix = "0ca60d35-1e02-43b7-bf59-ac7deb9afbca/file/69113/8/DisableRegions.vsix"
 $url = "$vsgallery/$vsix"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try {
-    Install-ChocolateyVsixPackage $packageName $url
-
-    Write-ChocolateySuccess $packageName
-} catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
-}
+Install-ChocolateyVsixPackage $packageName $url
