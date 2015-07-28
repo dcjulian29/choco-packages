5c443e3ebb9da63e7e7f3cefb149ba2a55587a32
Julian Easterling <julian@julianscorner.com>
2015-07-28 00:05:50 -0400

    Updated SQL Server Development Tools to Visual Studio 2015

--- sqlserverssdt/tools/chocolateyInstall.ps1
+++ sqlserverssdt/tools/chocolateyInstall.ps1
@@ -1,17 +1,10 @@
 $packageName = "sqlserverssdt"
 $installerType = "EXE"
 $installerArgs = "/PASSIVE /NORESTART"
-$url = "http://download.microsoft.com/download/6/5/8/65876378-90A7-4442-A038-6E15E675BAC8/EN/SSDTSetup.exe"
+$url = "http://download.microsoft.com/download/6/5/4/654E87D1-924A-45FF-A9FE-F0D5BF11B9E5/EN/SSDTSetup.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try {
-    Install-ChocolateyPackage $packageName $installerType $installerArgs $url
-
-    Write-ChocolateySuccess $packageName
-} catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
-}
+Install-ChocolateyPackage $packageName $installerType $installerArgs $url
