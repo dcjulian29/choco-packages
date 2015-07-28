51f76e6f5ac5a3edaa64e3c4cdf114ed9d56fe99
Julian Easterling <julian@julianscorner.com>
2015-07-28 00:00:34 -0400

    Added a VSIX package for NuGet Package Manager 3.1.60724

---
+++ vsix-nuget/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+$packageName = "vsix-nuget"
+$vsgallery = "http://visualstudiogallery.msdn.microsoft.com"
+$vsix = "5d345edc-2e2d-4a9c-b73b-d53956dc458d/file/146283/7/NuGet.Tools.vsix"
+$url = "$vsgallery/$vsix"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+}
+
+Install-ChocolateyVsixPackage $packageName $url
