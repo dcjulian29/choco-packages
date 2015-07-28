8714d52d9a872f9dd79aa8951f7ba82017e54fd3
Julian Easterling <julian@julianscorner.com>
2015-07-28 00:11:55 -0400

    updated codemaid extension to 0.8.0

--- vsix-codemaid/tools/chocolateyInstall.ps1
+++ vsix-codemaid/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "vsix-codemaid"
 $vsgallery = "http://visualstudiogallery.msdn.microsoft.com"
-$vsix = "76293c4d-8c16-4f4a-aee6-21f83a571496/file/9356/30/CodeMaid_v0.7.6.vsix"
+$vsix = "76293c4d-8c16-4f4a-aee6-21f83a571496/file/9356/31/CodeMaid_v0.8.0.vsix"
 $url = "$vsgallery/$vsix"
 
 if ($psISE) {
