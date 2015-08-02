c927f7603426cd307fd45f58420cbf90a88d583c
Julian Easterling <julian@julianscorner.com>
2015-08-02 15:09:25 -0400

    fixed a syntax error in the stylecop settings package uninstall script.

--- mysettings-stylecop/tools/chocolateyUninstall.ps1
+++ mysettings-stylecop/tools/chocolateyUninstall.ps1
@@ -1,20 +1,13 @@
 $packageName = "mysettings-stylecop"
-if (Test-Path "${$env:ProgramFiles(x86)}\StyleCop 4.7")
+if (Test-Path "${$env:ProgramFiles(x86)}\StyleCop 4.7") {
     $appDir = "$({$env:ProgramFiles(x86)})\StyleCop 4.7"
 }
 
-if (Test-Path "$($env:ProgramFiles)\StyleCop 4.7")
+if (Test-Path "$($env:ProgramFiles)\StyleCop 4.7") {
     $appDir = "$($env:ProgramFiles)\StyleCop 4.7"
 }
 
-try {
-    if (Test-Path $appDir)
-    {
-        Remove-Item "$($appDir)\Settings.StyleCop" -Force
-    }
-
-    Write-ChocolateySuccess $packageName
-} catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+if (Test-Path $appDir)
+{
+    Remove-Item "$($appDir)\Settings.StyleCop" -Force
 }
