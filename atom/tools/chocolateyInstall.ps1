9dee12842c3508971d9c41ebb0b00d40f7b2698e
Julian Easterling <julian@julianscorner.com>
2015-08-02 15:01:08 -0400

    updated atom to 1.0.3

--- atom/tools/chocolateyInstall.ps1
+++ atom/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = "atom"
-$url = "https://github.com/atom/atom/releases/download/v0.199.0/atom-windows.zip"
+$url = "https://github.com/atom/atom/releases/download/v1.0.3/atom-windows.zip"
 
 $downloadPath = "$($env:TEMP)\chocolatey\$($packageName)"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
@@ -8,28 +8,21 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try {
-    if (Test-Path $appDir)
-    {
-        Write-Output "Removing previous version of package..."
-        Remove-Item "$($appDir)" -Recurse -Force
-    }
-
-    New-Item -Type Directory -Path $appDir | Out-Null
-    
-    if (-not (Test-Path $downloadPath))
-    {
-        New-Item -Type Directory -Path $downloadPath | Out-Null
-    }
-
-    Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url
-    Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\"
-
-
-    Copy-Item -Path "$($downloadPath)\$($packageName)\*" -Destination "$appDir" -Recurse
-
-    Write-ChocolateySuccess $packageName
-} catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+if (Test-Path $appDir)
+{
+    Write-Output "Removing previous version of package..."
+    Remove-Item "$($appDir)" -Recurse -Force
 }
+
+New-Item -Type Directory -Path $appDir | Out-Null
+
+if (-not (Test-Path $downloadPath))
+{
+    New-Item -Type Directory -Path $downloadPath | Out-Null
+}
+
+Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url
+Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\"
+
+
+Copy-Item -Path "$($downloadPath)\$($packageName)\*" -Destination "$appDir" -Recurse
--- atom/tools/chocolateyUninstall.ps1
+++ atom/tools/chocolateyUninstall.ps1
@@ -1,14 +1,7 @@
 $packageName = "atom"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
 
-try {
-    if (Test-Path $appDir)
-    {
-      Remove-Item "$($appDir)" -Recurse -Force
-    }
-
-    Write-ChocolateySuccess $packageName
-} catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+if (Test-Path $appDir)
+{
+  Remove-Item "$($appDir)" -Recurse -Force
 }
