0a88120ef08bcfb6520152838e48f7459eeacf77
Julian Easterling <julian@julianscorner.com>
2015-08-02 15:03:35 -0400

    updated fiddler to 4.5.1.5

--- fiddler/tools/chocolateyInstall.ps1
+++ fiddler/tools/chocolateyInstall.ps1
@@ -1,20 +1,10 @@
 $packageName = "fiddler"
 $installerType = "EXE"
 $installerArgs = "/S"
-$url = "http://www.telerik.com/docs/default-source/fiddler/fiddler4setup.exe?sfvrsn=2"
+$url = "http://d585tldpucybw.cloudfront.net/docs/default-source/fiddler/fiddler4setup.exe?sfvrsn=46"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try
-{
-    Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url
-
-    Write-ChocolateySuccess $packageName
-}
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
-}
+Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url
