46f8e351bac4e60131784ecf27cbc0fcc346439c
Julian Easterling <julian@julianscorner.com>
2015-08-02 15:04:30 -0400

    updated expresso to 3.0.5

--- expresso/tools/chocolateyInstall.ps1
+++ expresso/tools/chocolateyInstall.ps1
@@ -7,20 +7,14 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try
-{
-    if (-not (Test-Path "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5")) {
-        Start-ChocolateyProcessAsAdmin "Enable-WindowsOptionalFeature -All -FeatureName NetFx3 -Online"
+if (-not (Test-Path "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5")) {
+    if (Test-ProcessAdminRights) {
+        & Enable-WindowsOptionalFeature -All -FeatureName NetFx3 -Online
     } else {
-        Write-Host "Microsoft .Net 3.5 Framework is already installed on this system..."
-    } 
+        Start-ChocolateyProcessAsAdmin "Enable-WindowsOptionalFeature -All -FeatureName NetFx3 -Online"
+    }
+} else {
+    Write-Host "Microsoft .Net 3.5 Framework is already installed on this system..."
+} 
 
-    Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url
-
-    Write-ChocolateySuccess $packageName
-}
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
-}
+Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url
