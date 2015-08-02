e7be6b173cdfc03269689e18af9f54d299e6a050
Julian Easterling <julian@julianscorner.com>
2015-08-02 15:02:36 -0400

    updated package to check to see if 4.6 is already installed...

--- dotnet/tools/chocolateyInstall.ps1
+++ dotnet/tools/chocolateyInstall.ps1
@@ -8,15 +8,22 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try
-{
-    Install-ChocolateyPackage $packageName $installerType $installerArgs $url `
-        -ValidExitCodes $errorCode
+$path = 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full'
+$release = "393295"
 
-    Write-ChocolateySuccess $packageName
-}
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+$compare = (Get-ItemProperty -LiteralPath $Path).psbase.members | `
+                ForEach-Object { $_.name } | `
+                Compare-Object "Release" -IncludeEqual -ExcludeDifferent
+
+if ($compare -ne $null) {
+    if ($compare.SideIndicator -like "==") {
+        $installed = (Get-ItemProperty $path -Name Release -ErrorAction SilentlyContinue).Release
+
+        if ($release.Contains($installed)) {
+            Write-Output ".Net Framework 4.6 is already installed on this system."
+        } else {
+            Install-ChocolateyPackage $packageName $installerType $installerArgs $url `
+                -ValidExitCodes $errorCode
+        }
+    }
 }
