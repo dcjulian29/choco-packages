e27cd86d65a65c8d2a22ecb0a2211a0a39d45145
Julian Easterling <julian@julianscorner.com>
2015-08-02 15:05:28 -0400

    Added expresso to mydev-tools package

--- mydev-tools/tools/chocolateyInstall.ps1
+++ mydev-tools/tools/chocolateyInstall.ps1
@@ -4,9 +4,4 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try {
-    Write-ChocolateySuccess $packageName
-} catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
-}
+Write-Output "Installed package: $packageName"
