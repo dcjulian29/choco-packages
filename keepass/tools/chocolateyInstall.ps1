169670a4c280cad3eb76c6b4ac7926e111b25f34
Julian Easterling <julian@julianscorner.com>
2015-08-18 18:38:30 -0400

    updated keepass to 2.30

--- keepass/tools/chocolateyInstall.ps1
+++ keepass/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = "keepass"
-$version = "2.29"
+$version = "2.30"
 $url = "http://superb-dca2.dl.sourceforge.net/project/keepass/KeePass%202.x/$version/KeePass-$version.zip"
 $downloadPath = "$env:TEMP\chocolatey\$packageName"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
@@ -8,28 +8,19 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try
+if (-not (Test-Path $downloadPath))
 {
-    if (Test-Path $appDir)
-    {
-        Write-Output "Removing previous version of package..."
-        Remove-Item "$($appDir)" -Recurse -Force
-    }
-
-    New-Item -Type Directory -Path $appDir | Out-Null
-    
-    if (-not (Test-Path $downloadPath))
-    {
-        New-Item -Type Directory -Path $downloadPath | Out-Null
-    }
-
-    Get-ChocolateyWebFile $packageName "$downloadPath\keepass.zip" $url
-    Get-ChocolateyUnzip "$downloadPath\keepass.zip" "$appDir\"
-
-    Write-ChocolateySuccess $packageName
+    New-Item -Type Directory -Path $downloadPath | Out-Null
 }
-catch
+
+Get-ChocolateyWebFile $packageName "$downloadPath\keepass.zip" $url
+
+if (Test-Path $appDir)
 {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+    Write-Output "Removing previous version of package..."
+    Remove-Item "$($appDir)" -Recurse -Force
 }
+
+New-Item -Type Directory -Path $appDir | Out-Null
+
+Get-ChocolateyUnzip "$downloadPath\keepass.zip" "$appDir\"
