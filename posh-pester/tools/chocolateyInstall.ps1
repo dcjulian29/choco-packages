d8bb4c2a92c488b5adf6cb96e9ef020dc0d13323
Julian Easterling <julian@julianscorner.com>
2015-08-17 21:50:40 -0400

    updated pester to 3.3.10

--- posh-pester/tools/chocolateyInstall.ps1
+++ posh-pester/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = "posh-pester"
-$version = "3.3.9"
+$version = "3.3.10"
 $url = "https://codeload.github.com/pester/Pester/zip/$version"
 $appDir = "$($env:UserProfile)\Documents\WindowsPowerShell\Modules\pester"
 $downloadPath = "$env:TEMP\chocolatey\$packageName"
@@ -8,32 +8,25 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try {
-    if (Test-Path $appDir)
-    {
-      Write-Output "Removing previous version of package..."
-      Remove-Item "$($appDir)" -Recurse -Force
-    }
+Get-ChocolateyWebFile $packageName "$downloadPath\$version.zip" $url
+Get-ChocolateyUnzip "$downloadPath\$version.zip" "$downloadPath\"
 
-    if (-not (Test-Path $downloadPath))
-    {
-        New-Item -Type Directory -Path $downloadPath | Out-Null
-    }
-
-    Get-ChocolateyWebFile $packageName "$downloadPath\$version.zip" $url
-    Get-ChocolateyUnzip "$downloadPath\$version.zip" "$downloadPath\"
-
-    $source = "$downloadPath\Pester-$version"
-    New-Item -Type Directory -Path $appDir | Out-Null
-
-    Copy-Item -Path "$source\Pester.ps*" -Destination "$appDir"
-    New-Item -Type Directory -Path "$appDir\en-US" | Out-Null
-    Copy-Item -Path "$source\en-US\*" -Destination "$appDir\en-US"
-    New-Item -Type Directory -Path "$appDir\Functions" | Out-Null
-    Copy-Item -Path "$source\Functions\*" -Destination "$appDir\Functions" -Recurse
-
-    Write-ChocolateySuccess $packageName
-} catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+if (Test-Path $appDir)
+{
+  Write-Output "Removing previous version of package..."
+  Remove-Item "$($appDir)" -Recurse -Force
 }
+
+if (-not (Test-Path $downloadPath))
+{
+    New-Item -Type Directory -Path $downloadPath | Out-Null
+}
+
+$source = "$downloadPath\Pester-$version"
+New-Item -Type Directory -Path $appDir | Out-Null
+
+Copy-Item -Path "$source\Pester.ps*" -Destination "$appDir"
+New-Item -Type Directory -Path "$appDir\en-US" | Out-Null
+Copy-Item -Path "$source\en-US\*" -Destination "$appDir\en-US"
+New-Item -Type Directory -Path "$appDir\Functions" | Out-Null
+Copy-Item -Path "$source\Functions\*" -Destination "$appDir\Functions" -Recurse
--- posh-pester/tools/chocolateyUninstall.ps1
+++ posh-pester/tools/chocolateyUninstall.ps1
@@ -1,14 +1,7 @@
 $packageName = "posh-pester"
 $appDir = "$($env:UserProfile)\Documents\WindowsPowerShell\Modules\pester"
 
-try {
-    if (Test-Path $appDir)
-    {
-      Remove-Item "$($appDir)" -Recurse -Force
-    }
-    
-    Write-ChocolateySuccess $packageName
-} catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+if (Test-Path $appDir)
+{
+  Remove-Item "$($appDir)" -Recurse -Force
 }
