283f11d430b9b713196fd3aed825b3ab4db77738
Julian Easterling <julian@julianscorner.com>
2015-08-18 18:38:13 -0400

    updated atom to 1.0.7

--- atom/tools/chocolateyInstall.ps1
+++ atom/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = "atom"
-$url = "https://github.com/atom/atom/releases/download/v1.0.3/atom-windows.zip"
+$url = "https://github.com/atom/atom/releases/download/v1.0.7/atom-windows.zip"
 
 $downloadPath = "$($env:TEMP)\chocolatey\$($packageName)"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
@@ -7,6 +7,13 @@ $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
+
+if (-not (Test-Path $downloadPath))
+{
+    New-Item -Type Directory -Path $downloadPath | Out-Null
+}
+
+Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url
 
 if (Test-Path $appDir)
 {
@@ -16,12 +23,6 @@ if (Test-Path $appDir)
 
 New-Item -Type Directory -Path $appDir | Out-Null
 
-if (-not (Test-Path $downloadPath))
-{
-    New-Item -Type Directory -Path $downloadPath | Out-Null
-}
-
-Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url
 Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\"
 
 
