52317062e0c49ce64212df36bbe02b512c094ad5
Julian Easterling <julian@julianscorner.com>
2015-08-27 15:03:11 -0400

    updated dotpeek to 1.5

--- dotpeek/tools/chocolateyInstall.ps1
+++ dotpeek/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "dotpeek"
-$url = "http://download.jetbrains.com/resharper/dotPeek32_1.4.3.exe"
-$url64 = "http://download.jetbrains.com/resharper/dotPeek64_1.4.3.exe"
+$url = "http://download.jetbrains.com/resharper/dotPeek32_1.5.exe"
+$url64 = "http://download.jetbrains.com/resharper/dotPeek64_1.5.exe"
 $downloadPath = "$env:TEMP\chocolatey\$packageName"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
 
@@ -8,29 +8,17 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try
-{
-    if (Test-Path $appDir) {
-        Write-Output "Removing previous version of package..."
-        Remove-Item "$($appDir)" -Recurse -Force
-    }
-
-    New-Item -Type Directory -Path $appDir | Out-Null
-    
-    if (-not (Test-Path $downloadPath)) {
-        New-Item -Type Directory -Path $downloadPath | Out-Null
-    }
-
-    Get-ChocolateyWebFile $packageName "$appDir\$packageName.exe" $url $url64
-
-
-
-    Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
-
-    Write-ChocolateySuccess $packageName
+if (Test-Path $appDir) {
+    Write-Output "Removing previous version of package..."
+    Remove-Item "$($appDir)" -Recurse -Force
 }
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+
+New-Item -Type Directory -Path $appDir | Out-Null
+
+if (-not (Test-Path $downloadPath)) {
+    New-Item -Type Directory -Path $downloadPath | Out-Null
 }
+
+Get-ChocolateyWebFile $packageName "$appDir\$packageName.exe" $url $url64
+
+Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
