afaf59493e8914d9607e89091ec749e96d0b3ce8
Julian Easterling <julian@julianscorner.com>
2015-09-20 22:13:49 -0400

    updated to git 2.5.3

--- git/tools/chocolateyInstall.ps1
+++ git/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName = "git"
 $installerType = "EXE"
 $installerArgs = '/SILENT /COMPONENTS="!ext,!ext\cheetah,!assoc,!assoc_sh"'
-$url = "https://github.com/git-for-windows/git/releases/download/v2.5.2.windows.1/Git-2.5.2-32-bit.exe"
-$url64 = "https://github.com/git-for-windows/git/releases/download/v2.5.2.windows.1/Git-2.5.2-64-bit.exe"
+$url = "https://github.com/git-for-windows/git/releases/download/v2.5.3.windows.1/Git-2.5.2-32-bit.exe"
+$url64 = "https://github.com/git-for-windows/git/releases/download/v2.5.3.windows.1/Git-2.5.3-64-bit.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
