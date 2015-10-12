bd7036eed0a01794fdd3d9dba5fa3755b7de3bee
Julian Easterling <julian@julianscorner.com>
2015-10-12 15:20:31 -0400

    updated nodejs to 4.1.1

--- nodejs/tools/chocolateyInstall.ps1
+++ nodejs/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@ $packageName = "nodejs"
 $installerType = "MSI"
 $installerArgs = "/qb"
 
-$version = "4.1.0"
+$version = "4.1.1"
 $rootUrl = "https://nodejs.org/download/release/v$version"
 
 $url = "$rootUrl/node-v$version-x86.msi"
