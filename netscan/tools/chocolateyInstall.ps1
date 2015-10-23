08d4d54c21f49f64f1060bc8a15b8dba765ce20f
Julian Easterling <julian@julianscorner.com>
2015-10-23 09:59:36 -0400

    updated netscan 6.0.8

--- netscan/tools/chocolateyInstall.ps1
+++ netscan/tools/chocolateyInstall.ps1
@@ -7,47 +7,39 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try
+if (Test-Path $appDir)
 {
-    if (Test-Path $appDir)
-    {
-        Write-Output "Removing previous version of package..."
-        Remove-Item "$($appDir)" -Recurse -Force
-    }
-
-    New-Item -Type Directory -Path $appDir | Out-Null
-    
-    if (-not (Test-Path $downloadPath))
-    {
-        New-Item -Type Directory -Path $downloadPath | Out-Null
-    }
-
-    # Need to "Open Web Page" since they seem to have a redirect when you go directly to URL...
-    $web = New-Object Net.WebClient
-    $web.DownloadString("http://www.softperfect.com/products/networkscanner/") | Out-Null
-
-    Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url
-
-    if (Test-Path "${env:ProgramFiles}") {
-        $bits = "32-Bit"
-    }
-
-    if (Test-Path "${env:ProgramFiles(x86)}") {
-        $bits = "64-Bit"
-    }
-    
-    Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\"
-
-    if (-not (Test-Path $downloadPath\$bits)) {
-        Write-ChocolateyFailure $packageName "Zip file downloaded is corrupt!"
-    } else {
-        Get-ChildItem -Path $downloadPath\$bits -Include $keep -Recurse | Copy-Item -Destination "$appDir"
-
-        Write-ChocolateySuccess $packageName
-    }
+    Write-Output "Removing previous version of package..."
+    Remove-Item "$($appDir)" -Recurse -Force
 }
-catch
+
+New-Item -Type Directory -Path $appDir | Out-Null
+
+if (-not (Test-Path $downloadPath))
 {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+    New-Item -Type Directory -Path $downloadPath | Out-Null
+}
+
+# Need to "Open Web Page" since they seem to have a redirect when you go directly to URL...
+$web = New-Object Net.WebClient
+$web.DownloadString("http://www.softperfect.com/products/networkscanner/") | Out-Null
+
+Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url
+
+if (Test-Path "${env:ProgramFiles}") {
+    $bits = "32-Bit"
+}
+
+if (Test-Path "${env:ProgramFiles(x86)}") {
+    $bits = "64-Bit"
+}
+
+Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\"
+
+if (-not (Test-Path $downloadPath\$bits)) {
+    Write-ChocolateyFailure $packageName "Zip file downloaded is corrupt!"
+} else {
+    Get-ChildItem -Path $downloadPath\$bits -Include $keep -Recurse | Copy-Item -Destination "$appDir"
+
+    Write-ChocolateySuccess $packageName
 }
