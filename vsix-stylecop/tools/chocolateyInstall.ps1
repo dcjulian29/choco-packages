431bf399cb5a7440f039db0758208aae493b9f2d
Julian Easterling <julian@julianscorner.com>
2015-10-23 10:44:59 -0400

    Moved StyleCop into a VSIX package and settings directly into projects.

---
+++ vsix-stylecop/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+$packageName = "vsix-stylecop"
+$vsgallery = "http://visualstudiogallery.msdn.microsoft.com"
+$vsix = "cac2a05b-6eb6-4fa2-95b9-1f8d011e6cae/file/173746/13/VSIXPackage.vsix"
+$url = "$vsgallery/$vsix"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+}
+
+Install-ChocolateyVsixPackage $packageName $url
