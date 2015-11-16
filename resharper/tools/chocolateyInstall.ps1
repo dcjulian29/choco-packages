f4c90b8afba7bfac62dfdbefd4b5301b6ce52c95
Julian Easterling <julian@julianscorner.com>
2015-11-16 10:17:58 -0500

    updated Resharper to 10.0.0

--- resharper/tools/chocolateyInstall.ps1
+++ resharper/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = "resharper"
 $installerType = "EXE"
 $installerArgs = "/SpecificProductNames=ReSharper /Silent=True"
-$url = "http://download.jetbrains.com/resharper/JetBrains.ReSharperUltimate.2015.2.exe"
+$url = "http://download.jetbrains.com/resharper/JetBrains.ReSharperUltimate.10.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
