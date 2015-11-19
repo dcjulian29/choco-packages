4a85d3da8c2fdec1f962b09fb6f1c56e64df3a3d
Julian Easterling <julian@julianscorner.com>
2015-11-19 13:27:50 -0500

    added visual studio code

--- mydev-visualstudio/tools/chocolateyInstall.ps1
+++ mydev-visualstudio/tools/chocolateyInstall.ps1
@@ -4,16 +4,8 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try {
+$webpi = "$($env:ChocolateyInstall)\bin\webpicmd.bat"
 
-    $webpi = "$($env:ChocolateyInstall)\bin\webpicmd.bat"
-
-    & $webpi /Install /AcceptEula /SuppressReboot /Products:WebMatrixWeb
-    & $webpi /Install /AcceptEula /SuppressReboot /Products:WindowsAzurePowershell
-    & $webpi /Install /AcceptEula /SuppressReboot /Products:VWDOrVs2015AzurePack.2.7
-
-    Write-ChocolateySuccess $packageName
-} catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
-}
+& $webpi /Install /AcceptEula /SuppressReboot /Products:WebMatrixWeb
+& $webpi /Install /AcceptEula /SuppressReboot /Products:WindowsAzurePowershell
+& $webpi /Install /AcceptEula /SuppressReboot /Products:VWDOrVs2015AzurePack.2.7
