610f1da68d69eb9e6ee5334782d361c27a058478
Julian Easterling <julian@julianscorner.com>
2015-11-22 20:42:47 -0500

    Added dotTrace package

---
+++ dottrace/tools/chocolateyUninstall.ps1
@@ -0,0 +1,13 @@
+$packageName = "dottrace"
+$installerType = "EXE"
+$installerArgs = "/SpecificProductNamesToRemove=dotTrace /Silent=True /ReSharper9PlusMsi=True"
+$url = "http://download.jetbrains.com/resharper/JetBrains.ReSharperUltimate.10.0.1.exe"
+
+if ($psISE) {
+    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+}
+
+Push-Location $env:TEMP
+Get-WebFile $url
+Uninstall-ChocolateyPackage $packageName $installerType $installerArgs "JetBrains.ReSharperUltimate.10.0.1.exe"
+Pop-Location
