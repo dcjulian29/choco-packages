6fa997666939a7a42663627f5d2e91612706a605
Julian Easterling <julian@julianscorner.com>
2015-12-08 15:59:35 -0500

    updated xmind to 3.6.0

--- xmind/tools/chocolateyInstall.ps1
+++ xmind/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageName = "xmind"
 $installerType = "EXE"
 $installerArgs = "/SILENT"
-$url = "http://dl2.xmind.net/xmind-downloads/xmind-windows-3.5.3.201506180105.exe"
+$url = "http://www.xmind.net/xmind/downloads/xmind7-windows-3.6.0.R-201511090408.exe"
 
 Install-ChocolateyPackage $packageName $installerType $installerArgs $url
 
-if (Test-Path "$($env:USERPROFILE)\Desktop\XMind 6.lnk") {
-    Remove-Item "$($env:USERPROFILE)\Desktop\XMind 6.lnk" -force
+if (Test-Path "$($env:USERPROFILE)\Desktop\XMind 7.lnk") {
+    Remove-Item "$($env:USERPROFILE)\Desktop\XMind 7.lnk" -force
 }
