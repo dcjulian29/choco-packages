12ce71b0c7eb35020042d32b6b326893138ef7c1
Julian Easterling <julian@julianscorner.com>
2015-12-08 15:59:53 -0500

    updated vscode to 0.10.3

--- vscode/tools/chocolateyInstall.ps1
+++ vscode/tools/chocolateyInstall.ps1
@@ -1,10 +1,16 @@
 $packageName = "vscode"
 $installerType = "EXE"
 $installerArgs = "/SILENT /NORESTART"
-$url = "https://az764295.vo.msecnd.net/public/0.10.1-release/VSCodeSetup.exe"
+$url = "https://az764295.vo.msecnd.net/public/0.10.3/VSCodeSetup.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
 Install-ChocolateyPackage $packageName $installerType $installerArgs $url
+
+if (Test-ProcessAdminRights) {
+    Remove-Item "$($env:PUBLIC)\Desktop\Visual Studio Code.lnk" -Force
+} else {
+    Start-ChocolateyProcessAsAdmin "Remove-Item '$($env:PUBLIC)\Desktop\Visual Studio Code.lnk' -Force"
+}
