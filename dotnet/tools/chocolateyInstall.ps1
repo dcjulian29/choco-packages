0357d0e6f7654225093cf1f851def91c40be8387
Julian Easterling <julian@julianscorner.com>
2015-12-08 16:02:52 -0500

    updated dotnet to 4.6.1

--- dotnet/tools/chocolateyInstall.ps1
+++ dotnet/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = "dotnet"
 $installerType = "EXE"
 $installerArgs = "/passive /norestart"
-$url = "http://download.microsoft.com/download/6/F/9/6F9673B1-87D1-46C4-BF04-95F24C3EB9DA/enu_netfx/NDP46-KB3045557-x86-x64-AllOS-ENU_exe/NDP46-KB3045557-x86-x64-AllOS-ENU.exe"
+$url = "https://download.microsoft.com/download/E/4/1/E4173890-A24A-4936-9FC9-AF930FE3FA40/NDP461-KB3102436-x86-x64-AllOS-ENU.exe"
 $errorCode = @(0, 3010)
 
 if ($psISE) {
@@ -9,7 +9,9 @@ if ($psISE) {
 }
 
 $path = 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full'
-$release = "393295"
+
+$release = "394271"
+$release10 = "394254"
 
 $compare = (Get-ItemProperty -LiteralPath $Path).psbase.members | `
                 ForEach-Object { $_.name } | `
@@ -19,8 +21,8 @@ if ($compare -ne $null) {
     if ($compare.SideIndicator -like "==") {
         $installed = (Get-ItemProperty $path -Name Release -ErrorAction SilentlyContinue).Release
 
-        if ($release.Contains($installed)) {
-            Write-Output ".Net Framework 4.6 is already installed on this system."
+        if ($release.Contains($installed) -or $release10.Contains($installed)) {
+            Write-Output ".Net Framework 4.6.1 is already installed on this system."
         } else {
             Install-ChocolateyPackage $packageName $installerType $installerArgs $url `
                 -ValidExitCodes $errorCode
