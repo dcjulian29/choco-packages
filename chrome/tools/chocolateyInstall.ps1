e948d79213cec6567878af8e42dc71fa51eae805
Julian Easterling <julian@julianscorner.com>
2015-12-08 15:58:57 -0500

    updated chrome to 47

--- chrome/tools/chocolateyInstall.ps1
+++ chrome/tools/chocolateyInstall.ps1
@@ -7,23 +7,16 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try {
-    $app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -eq "Google Chrome" }
-    
-    if ($app) {
-        Write-Output "Chrome is already installed and will update itself, no need to update via package..."
-    } else {
-        Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
-        
-        if (Test-ProcessAdminRights) {
-            Remove-Item "$($env:PUBLIC)\Desktop\Google Chrome.lnk" -Force
-        } else {
-            Start-ChocolateyProcessAsAdmin "Remove-Item '$($env:PUBLIC)\Desktop\Google Chrome.lnk' -Force"
-        }
-    }
+$app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -eq "Google Chrome" }
 
-    Write-ChocolateySuccess $packageName
-} catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+if ($app) {
+    Write-Output "Chrome is already installed and will update itself, no need to update via package..."
+} else {
+    Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
+    
+    if (Test-ProcessAdminRights) {
+        Remove-Item "$($env:PUBLIC)\Desktop\Google Chrome.lnk" -Force
+    } else {
+        Start-ChocolateyProcessAsAdmin "Remove-Item '$($env:PUBLIC)\Desktop\Google Chrome.lnk' -Force"
+    }
 }
