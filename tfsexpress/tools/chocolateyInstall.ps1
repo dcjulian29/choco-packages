5adf86e5799a12625a6203724a2e6ea0105cc957
Julian Easterling <julian@julianscorner.com>
2015-12-10 13:20:23 -0500

    updated TFS Express to Update 1

--- tfsexpress/tools/chocolateyInstall.ps1
+++ tfsexpress/tools/chocolateyInstall.ps1
@@ -1,7 +1,8 @@
 $packageName = "tfsexpress"
 $installerType = "EXE"
 $installerArgs = "/quiet"
-$url = "http://download.microsoft.com/download/C/A/F/CAF8D1B5-3065-4FEB-B9F0-3E6DAD0C03C8/tfs_express.exe"
+$url = "http://download.microsoft.com/download/F/7/4/F7485C1E-3E67-4F1D-B2DB-20098E26D087/tfs_express.exe"
+
 $toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
 
 if ($psISE) {
