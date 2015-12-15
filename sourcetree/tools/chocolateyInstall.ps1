b9899d84f58a50414b71aa820b59a0c0aa5e75bb
Julian Easterling <julian@julianscorner.com>
2015-12-15 09:58:40 -0500

    updated sourcetree to 1.7.0

--- sourcetree/tools/chocolateyInstall.ps1
+++ sourcetree/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = "sourcetree"
 $installerType = "EXE"
 $installerArgs = "/passive"
-$url = "http://downloads.atlassian.com/software/sourcetree/windows/SourceTreeSetup_1.6.25.exe"
+$url = "http://downloads.atlassian.com/software/sourcetree/windows/SourceTreeSetup_1.7.0.32509.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
