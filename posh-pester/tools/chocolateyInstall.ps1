401d68211c3cda45394ab132e4ec9a24145a6f23
Julian Easterling <julian@julianscorner.com>
2016-01-04 21:15:55 -0500

    updated pester to 3.3.14

--- posh-pester/tools/chocolateyInstall.ps1
+++ posh-pester/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = "posh-pester"
-$version = "3.3.13"
+$version = "3.3.14"
 $url = "https://codeload.github.com/pester/Pester/zip/$version"
 $appDir = "$($env:UserProfile)\Documents\WindowsPowerShell\Modules\pester"
 $downloadPath = "$env:TEMP\chocolatey\$packageName"
