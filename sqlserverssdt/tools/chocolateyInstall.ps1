1c9ec8cdf581a5cccb8b585d9352ace4088d31f9
Julian Easterling <julian@julianscorner.com>
2016-01-12 14:52:55 -0500

    updated SQL Server Data Tools to 14.0.50730

--- sqlserverssdt/tools/chocolateyInstall.ps1
+++ sqlserverssdt/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = "sqlserverssdt"
 $installerType = "EXE"
 $installerArgs = "/PASSIVE /NORESTART"
-$url = "http://download.microsoft.com/download/6/5/4/654E87D1-924A-45FF-A9FE-F0D5BF11B9E5/EN/SSDTSetup.exe"
+$url = "http://download.microsoft.com/download/4/D/3/4D39DA54-DF09-4628-B63D-685BFCE523EA/Dev14/EN/SSDTSetup.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
