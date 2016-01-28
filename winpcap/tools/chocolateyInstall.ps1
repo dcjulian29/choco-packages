3dc120de5c862440ea8f253eaddb2eaa5cf9e29d
Julian Easterling <julian@julianscorner.com>
2016-01-28 10:42:50 -0500

    updated the winpcap package to look in the correct location for auto hotkey

--- winpcap/tools/chocolateyInstall.ps1
+++ winpcap/tools/chocolateyInstall.ps1
@@ -3,34 +3,23 @@ $url = "http://www.winpcap.org/install/bin/WinPcap_4_1_3.exe"
 $toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
 $downloadPath = "$env:TEMP\chocolatey\winpcap"
 $installer = "$downloadPath\winpcapInstall.exe"
+$ahkExe = "C:\tools\apps\autohotkey\AutoHotkey.exe"
+$ahkScript = "$toolDir\install.ahk"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try
-{
-    if (!(Test-Path $downloadPath)) {
-        New-Item -ItemType directory $downloadPath -Force | Out-Null
-    }
-
-    Get-ChocolateyWebFile $packageName $installer $url
-
-    if (Test-Path "$env:ChocolateyInstall\apps\autohotkey\AutoHotkey.exe") {
-        $ahkExe = "$env:ChocolateyInstall\apps\autohotkey\AutoHotkey.exe"
-        $ahkScript = "$toolDir\install.ahk"
-
-        if (Test-ProcessAdminRights) {
-            Invoke-Expression "$ahkExe $ahkScript"
-        } else {
-            Start-ChocolateyProcessAsAdmin "$ahkExe $ahkScript" -noSleep
-        }
-    }
-
-    Write-ChocolateySuccess $packageName
+if (Test-Path $downloadPath) {
+    Remove-Item $downloadPath -Recurse -Force | Out-Null
 }
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+
+New-Item -ItemType directory $downloadPath -Force | Out-Null
+
+Get-ChocolateyWebFile $packageName $installer $url
+
+if (Test-ProcessAdminRights) {
+    Invoke-Expression "$ahkExe $ahkScript"
+} else {
+    Start-ChocolateyProcessAsAdmin "$ahkExe $ahkScript" -noSleep
 }
--- winpcap/tools/chocolateyUninstall.ps1
+++ winpcap/tools/chocolateyUninstall.ps1
@@ -1,27 +1,14 @@
 ﻿$packageName = "winpcap"
 $toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
+$ahkExe = "C:\tools\apps\autohotkey\AutoHotkey.exe"
+$ahkScript = "$toolDir\uninstall.ahk"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try
-{
-    if (Test-Path "$env:ChocolateyInstall\apps\autohotkey\AutoHotkey.exe") {
-        $ahkExe = "$env:ChocolateyInstall\apps\autohotkey\AutoHotkey.exe"
-        $ahkScript = "$toolDir\uninstall.ahk"
-
-        if (Test-ProcessAdminRights) {
-            Invoke-Expression "$ahkExe $ahkScript"
-        } else {
-            Start-ChocolateyProcessAsAdmin "$ahkExe $ahkScript" -noSleep
-        }
-    }
-
-    Write-ChocolateySuccess $packageName
-}
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+if (Test-ProcessAdminRights) {
+    Invoke-Expression "$ahkExe $ahkScript"
+} else {
+    Start-ChocolateyProcessAsAdmin "$ahkExe $ahkScript" -noSleep
 }
