f0cf4a9e3fb5b3feccca187862f40008869547b3
Julian Easterling <julian@julianscorner.com>
2016-01-28 10:33:24 -0500

    Updated bind tools to 9.10.3

--- bind-tools/tools/chocolateyInstall.ps1
+++ bind-tools/tools/chocolateyInstall.ps1
@@ -2,8 +2,11 @@ $packageName = "bind-tools"
 $downloadPath = "$env:TEMP\chocolatey\$packageName"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
 
-$url = "http://ftp.isc.org/isc/bind9/9.10.2/BIND9.10.2.x86.zip"
-$url64 = "http://ftp.isc.org/isc/bind9/9.10.2/BIND9.10.2.x64.zip"
+$url = "http://ftp.isc.org/isc/bind9/9.10.3-P3/BIND9.10.3-P3.x86.zip"
+$url64 = "http://ftp.isc.org/isc/bind9/9.10.3-P3/BIND9.10.3-P3.x64.zip"
+
+$vcredist = "https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x86.exe"
+$vcredist64 = "https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x64.exe"
 
 $keep = @(
   "dig.exe",
@@ -21,30 +24,24 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try
-{
-    if (Test-Path $appDir)
-    {
-      Remove-Item "$($appDir)" -Recurse -Force
-    }
-
-    if (-not (Test-Path $downloadPath)) {
-        New-Item -Type Directory -Path $downloadPath | Out-Null
-    }
-
-    Get-ChocolateyWebFile $packageName "$downloadPath\bind.zip" $url $url64
-    Get-ChocolateyUnzip "$downloadPath\bind.zip" "$downloadPath\"
-
-    if (-not (Test-Path $appDir)) {
-        New-Item -Type Directory -Path $appDir | Out-Null
-    }
-
-    Get-ChildItem -Path $downloadPath -Include $keep -Recurse | Copy-Item -Destination "$appDir"
-
-    Write-ChocolateySuccess $packageName
+if (Test-Path $downloadPath) {
+    Remove-Item $downloadPath -Recurse -Force | Out-Null
 }
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+
+New-Item -Type Directory -Path $downloadPath | Out-Null
+
+if (-not (Test-Path "HKLM:SOFTWARE\Microsoft\DevDiv\vc\Servicing\11.0")) {
+    Install-ChocolateyPackage "vcredist2012" "EXE" $installerArgs $vcredist $vcredist64
 }
+
+Get-ChocolateyWebFile $packageName "$downloadPath\bind.zip" $url $url64
+Get-ChocolateyUnzip "$downloadPath\bind.zip" "$downloadPath\"
+
+if (Test-Path $appDir)
+{
+  Remove-Item "$($appDir)" -Recurse -Force
+}
+
+New-Item -Type Directory -Path $appDir | Out-Null
+
+Get-ChildItem -Path $downloadPath -Include $keep -Recurse | Copy-Item -Destination "$appDir"
