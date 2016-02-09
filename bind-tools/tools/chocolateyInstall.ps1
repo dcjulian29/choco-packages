4cdd49960c10d075aa0c1d7734416d652284eb78
Julian Easterling <julian@julianscorner.com>
2016-02-08 20:18:17 -0500

    Corrected a type in the installer arguments that prevented silent install.

--- bind-tools/tools/chocolateyInstall.ps1
+++ bind-tools/tools/chocolateyInstall.ps1
@@ -5,6 +5,7 @@ $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
 $url = "http://ftp.isc.org/isc/bind9/9.10.3-P3/BIND9.10.3-P3.x86.zip"
 $url64 = "http://ftp.isc.org/isc/bind9/9.10.3-P3/BIND9.10.3-P3.x64.zip"
 
+$installArgs = "/install /passive /norestart"
 $vcredist = "https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x86.exe"
 $vcredist64 = "https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x64.exe"
 
@@ -31,7 +32,7 @@ if (Test-Path $downloadPath) {
 New-Item -Type Directory -Path $downloadPath | Out-Null
 
 if (-not (Test-Path "HKLM:SOFTWARE\Microsoft\DevDiv\vc\Servicing\11.0")) {
-    Install-ChocolateyPackage "vcredist2012" "EXE" $installerArgs $vcredist $vcredist64
+    Install-ChocolateyPackage "vcredist2012" "EXE" $installArgs $vcredist $vcredist64
 }
 
 Get-ChocolateyWebFile $packageName "$downloadPath\bind.zip" $url $url64
