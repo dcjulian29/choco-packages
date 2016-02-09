63bde28dbdc96a37b4478894f021a444c6fc800b
Julian Easterling <julian@julianscorner.com>
2016-02-08 20:22:38 -0500

    removed webmatrix and moved azure sdk and powershell cmdlets to separate packages.

--- mydev-visualstudio/tools/chocolateyInstall.ps1
+++ mydev-visualstudio/tools/chocolateyInstall.ps1
@@ -4,8 +4,3 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-$webpi = "$($env:ChocolateyInstall)\bin\webpicmd.bat"
-
-& $webpi /Install /AcceptEula /SuppressReboot /Products:WebMatrixWeb
-& $webpi /Install /AcceptEula /SuppressReboot /Products:WindowsAzurePowershell
-& $webpi /Install /AcceptEula /SuppressReboot /Products:VWDOrVs2015AzurePack.2.8.1
