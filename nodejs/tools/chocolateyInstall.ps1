61d681e2293b9e70346ff259fa3c869ceda3c61a
Julian Easterling <julian@julianscorner.com>
2016-02-08 20:24:10 -0500

    updated nodejs to 5.5.0

--- nodejs/tools/chocolateyInstall.ps1
+++ nodejs/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@ $packageName = "nodejs"
 $installerType = "MSI"
 $installerArgs = "/qb"
 
-$version = "5.4.0"
+$version = "5.5.0"
 $rootUrl = "https://nodejs.org/download/release/v$version"
 
 $url = "$rootUrl/node-v$version-x86.msi"
