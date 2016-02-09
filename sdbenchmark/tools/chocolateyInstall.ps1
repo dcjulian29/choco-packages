439ef620ea4095910ed694d8b84f9757911a0f52
Julian Easterling <julian@julianscorner.com>
2016-02-08 20:28:22 -0500

    updated sdbenchmark to 1.8.5636

--- sdbenchmark/tools/chocolateyInstall.ps1
+++ sdbenchmark/tools/chocolateyInstall.ps1
@@ -7,28 +7,18 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try
+if (Test-Path $appDir)
 {
-    if (Test-Path $appDir)
-    {
-        Write-Output "Removing previous version of package..."
-        Remove-Item "$($appDir)" -Recurse -Force
-    }
-
-    New-Item -Type Directory -Path $appDir | Out-Null
-    
-    if (-not (Test-Path $downloadPath))
-    {
-        New-Item -Type Directory -Path $downloadPath | Out-Null
-    }
-
-    Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url
-    Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$appDir\"
-
-    Write-ChocolateySuccess $packageName
+    Write-Output "Removing previous version of package..."
+    Remove-Item "$($appDir)" -Recurse -Force
 }
-catch
+
+New-Item -Type Directory -Path $appDir | Out-Null
+
+if (-not (Test-Path $downloadPath))
 {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+    New-Item -Type Directory -Path $downloadPath | Out-Null
 }
+
+Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url
+Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$appDir\"
