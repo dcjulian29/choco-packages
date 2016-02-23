4ea9657156048ef9e61f4f6664e205bf741584d3
Julian Easterling <julian@julianscorner.com>
2016-02-23 12:44:55 -0500

    updated Azure SDK to 2.8.2

--- azuresdk/tools/chocolateyInstall.ps1
+++ azuresdk/tools/chocolateyInstall.ps1
@@ -1,9 +1,8 @@
 $packageName = "azuresdk"
 $webpi = "$($env:ChocolateyInstall)\bin\webpicmd.bat"
-$version = "2.8.1"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-& $webpi /Install /AcceptEula /SuppressReboot /Products:VWDOrVs2015AzurePack.$version
+& $webpi /Install /AcceptEula /SuppressReboot /Products:VWDOrVs2015AzurePack.2.8
