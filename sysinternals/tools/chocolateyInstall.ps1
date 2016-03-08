9d3874df6f6a7aed12553818f108871e526fd7db
Julian Easterling <julian@julianscorner.com>
2016-03-08 14:33:30 -0500

    looks like the web server that hosts the sysinternal tools has gotten case sensitive...

--- sysinternals/tools/chocolateyInstall.ps1
+++ sysinternals/tools/chocolateyInstall.ps1
@@ -2,44 +2,32 @@
 $downloadPath = "$env:TEMP\chocolatey\$packageName"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
 $toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
-$url = 'https://download.sysinternals.com/files/sysinternalssuite.zip'
+$url = 'https://download.sysinternals.com/files/SysinternalsSuite.zip'
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try
+if (-not (Test-Path $downloadPath)) {
+    New-Item -Type Directory -Path $downloadPath | Out-Null
+}
+
+Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url $url
+Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\"
+
+if (Test-Path $appDir)
 {
-    if (Test-Path $appDir)
-    {
-      Write-Output "Removing previous version of package..."
-      Remove-Item "$($appDir)\*" -Recurse -Force
-    }
+  Write-Output "Removing previous version of package..."
+  Remove-Item "$($appDir)\*" -Recurse -Force
+}
 
-    if (-not (Test-Path $downloadPath)) {
-        New-Item -Type Directory -Path $downloadPath | Out-Null
-    }
+New-Item -Type Directory -Path $appDir | Out-Null
 
-    Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url $url
-    Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\"
-
-    if (-not (Test-Path $appDir)) {
-        New-Item -Type Directory -Path $appDir | Out-Null
-    }
-
-    Get-ChildItem -Path $downloadPath -Exclude "$packageName.zip" | Copy-Item -Destination "$appDir"
+Get-ChildItem -Path $downloadPath -Exclude "$packageName.zip" | Copy-Item -Destination "$appDir"
     
 
-    if (Test-ProcessAdminRights) {
-        . $toolDir\postInstall.ps1
-    } else {
-        Start-ChocolateyProcessAsAdmin ". $toolDir\postInstall.ps1"
-    }
-
-    Write-ChocolateySuccess $packageName
-}
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+if (Test-ProcessAdminRights) {
+    . $toolDir\postInstall.ps1
+} else {
+    Start-ChocolateyProcessAsAdmin ". $toolDir\postInstall.ps1"
 }
