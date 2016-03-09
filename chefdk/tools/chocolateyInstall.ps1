bbaa2fd5090598e6473326aeb1111b1d731bd00d
Julian Easterling <julian@julianscorner.com>
2016-03-09 00:41:39 -0500

    updated chefdk to 0.11.2.1

--- chefdk/tools/chocolateyInstall.ps1
+++ chefdk/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = "chefdk"
 $installerType = "MSI"
 $installerArgs = '/passive'
-$url = "https://opscode-omnibus-packages.s3.amazonaws.com/windows/2008r2/i386/chefdk-0.10.0-1-x86.msi"
+$url = "https://opscode-omnibus-packages.s3.amazonaws.com/windows/2008r2/i386/chefdk-0.11.2-1-x86.msi"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
