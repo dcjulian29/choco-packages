6039ba86175a73bc2f11498a5fd7364698573bd2
Julian Easterling <julian@julianscorner.com>
2016-03-09 01:06:14 -0500

    Updated VisualStudio Code to 0.10.8

--- vscode/tools/chocolateyInstall.ps1
+++ vscode/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = "vscode"
 $installerType = "EXE"
 $installerArgs = "/SILENT /NORESTART"
-$url = "https://az764295.vo.msecnd.net/public/0.10.3/VSCodeSetup.exe"
+$url = "https://az764295.vo.msecnd.net/public/0.10.8/VSCodeSetup.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
