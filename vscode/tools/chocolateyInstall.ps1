465f21615a1006f3aa0337331e8422a93123dba4
Julian Easterling <julian@julianscorner.com>
2016-03-12 17:53:51 -0500

    Fixed the download link for visual studio code

--- vscode/tools/chocolateyInstall.ps1
+++ vscode/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = "vscode"
 $installerType = "EXE"
 $installerArgs = "/SILENT /NORESTART"
-$url = "https://az764295.vo.msecnd.net/public/0.10.8/VSCodeSetup.exe"
+$url = "https://az764295.vo.msecnd.net/stable/5b5f4db87c10345b9d5c8d0bed745bcad4533135/VSCodeSetup-stable.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
