9ed829da7406c134a7d22b01ed89cac56c8eb06c
Julian Easterling <julian@julianscorner.com>
2016-03-28 12:09:32 -0400

    Updated sourcetree to 1.8.3

--- sourcetree/tools/chocolateyInstall.ps1
+++ sourcetree/tools/chocolateyInstall.ps1
@@ -1,10 +1,18 @@
 $packageName = "sourcetree"
 $installerType = "EXE"
 $installerArgs = "/passive"
-$url = "https://downloads.atlassian.com/software/sourcetree/windows/SourceTreeSetup_1.8.2.11.exe"
+$url = "https://downloads.atlassian.com/software/sourcetree/windows/SourceTreeSetup_1.8.3.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
 Install-ChocolateyPackage $packageName $installerType $installerArgs $url
+
+# On my Dev VMs, I have a pre-configured folder with licensing and configuration...
+if (-not (Test-Path "$env:USERPROFILE\AppData\Local\Atlassian")) {
+    if (Test-Path "C:\etc\sourcetree") {
+        New-Item -ItemType Directory "$env:USERPROFILE\AppData\Local\Atlassian" | Out-Null
+        Copy-Item -Recurse "C:\etc\sourcetree\*" $appProfile
+    }
+}
