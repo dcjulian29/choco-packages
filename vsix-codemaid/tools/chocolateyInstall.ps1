a0ae966a6f040afefdd2189d80e5fae6188b2b22
Julian Easterling <julian@julianscorner.com>
2016-05-02 21:03:48 -0400

    update codemaid VSIX to 10.1.93

--- vsix-codemaid/tools/chocolateyInstall.ps1
+++ vsix-codemaid/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "vsix-codemaid"
-$vsgallery = "http://visualstudiogallery.msdn.microsoft.com"
-$vsix = "76293c4d-8c16-4f4a-aee6-21f83a571496/file/9356/31/CodeMaid_v0.8.0.vsix"
+$vsgallery = "https://visualstudiogallery.msdn.microsoft.com"
+$vsix = "76293c4d-8c16-4f4a-aee6-21f83a571496/file/9356/37/CodeMaid%20v10.1.93.vsix"
 $url = "$vsgallery/$vsix"
 
 if ($psISE) {
