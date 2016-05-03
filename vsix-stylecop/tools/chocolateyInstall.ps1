15f32d8d391fa5b96ccd260a78bb44f544af22e3
Julian Easterling <julian@julianscorner.com>
2016-05-02 21:12:27 -0400

    updated stylecop VSIX to 4.7.59

--- vsix-stylecop/tools/chocolateyInstall.ps1
+++ vsix-stylecop/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "vsix-stylecop"
-$vsgallery = "http://visualstudiogallery.msdn.microsoft.com"
-$vsix = "cac2a05b-6eb6-4fa2-95b9-1f8d011e6cae/file/173746/13/VSIXPackage.vsix"
+$vsgallery = "https://visualstudiogallery.msdn.microsoft.com"
+$vsix = "cac2a05b-6eb6-4fa2-95b9-1f8d011e6cae/file/173746/14/VSIXPackage.vsix"
 $url = "$vsgallery/$vsix"
 
 if ($psISE) {
