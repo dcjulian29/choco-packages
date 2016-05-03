d36bd21132d6a805bf90b006116135b7c47850da
Julian Easterling <julian@julianscorner.com>
2016-05-02 21:11:28 -0400

    updated sqlite toolbox VSIX to 4.5.0.2

--- vsix-sqlitetoolbox/tools/chocolateyInstall.ps1
+++ vsix-sqlitetoolbox/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "vsix-sqlitetoolbox"
 $vsgallery = "https://visualstudiogallery.msdn.microsoft.com"
-$vsix = "0e313dfd-be80-4afb-b5e9-6e74d369f7a1/file/29445/75/SqlCeToolbox.4.3.0.vsix"
+$vsix = "0e313dfd-be80-4afb-b5e9-6e74d369f7a1/file/29445/85/SqlCeToolbox.4.5.0.3.vsix"
 $url = "$vsgallery/$vsix"
 
 if ($psISE) {
