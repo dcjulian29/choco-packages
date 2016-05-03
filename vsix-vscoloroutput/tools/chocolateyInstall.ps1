1481d39c3015bab1fd219e261d00cafe3984a512
Julian Easterling <julian@julianscorner.com>
2016-05-02 21:13:04 -0400

    updated vscoloroutput VSIX to 3.4

--- vsix-vscoloroutput/tools/chocolateyInstall.ps1
+++ vsix-vscoloroutput/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "vsix-vscoloroutput"
-$vsgallery = "http://visualstudiogallery.msdn.microsoft.com"
-$vsix = "f4d9c2b5-d6d7-4543-a7a5-2d7ebabc2496/file/63103/9/VSColorOutput.vsix"
+$vsgallery = "https://visualstudiogallery.msdn.microsoft.com"
+$vsix = "f4d9c2b5-d6d7-4543-a7a5-2d7ebabc2496/file/63103/18/VSColorOutput.vsix"
 $url = "$vsgallery/$vsix"
 
 if ($psISE) {
