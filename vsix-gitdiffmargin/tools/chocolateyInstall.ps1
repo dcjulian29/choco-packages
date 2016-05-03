862b7cb9cd498b336ceefcfa94647c4bb1e1b271
Julian Easterling <julian@julianscorner.com>
2016-05-02 21:06:16 -0400

    updated gitdiffmargin VSIX to 3.1.2

--- vsix-gitdiffmargin/tools/chocolateyInstall.ps1
+++ vsix-gitdiffmargin/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "vsix-gitdiffmargin"
-$vsgallery = "http://visualstudiogallery.msdn.microsoft.com"
-$vsix = "cf49cf30-2ca6-4ea0-b7cc-6a8e0dadc1a8/file/101267/11/GitDiffMargin.vsix"
+$vsgallery = "https://visualstudiogallery.msdn.microsoft.com"
+$vsix = "cf49cf30-2ca6-4ea0-b7cc-6a8e0dadc1a8/file/101267/14/GitDiffMargin.vsix"
 $url = "$vsgallery/$vsix"
 
 if ($psISE) {
