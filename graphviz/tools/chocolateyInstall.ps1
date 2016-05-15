fdaaaf6bc85d12aa54d42265a156d9a694c991a1
Julian Easterling <julian@julianscorner.com>
2016-05-15 19:04:16 -0400

    Just goes to show... don't depend on anyone else for dependencies...  moved a copy of the 2.38 zip file to my server so that my install script stops failing due to their server timeouts...

--- graphviz/tools/chocolateyInstall.ps1
+++ graphviz/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = "graphviz"
-$url = "http://www.graphviz.org/pub/graphviz/stable/windows/graphviz-2.38.zip"
+$url = "https://julianscorner.com/downloads/graphviz-2.38.zip"
 $downloadPath = "${env:TEMP}\chocolatey\$packageName"
 $appDir = "${env:SYSTEMDRIVE}\tools\apps\$packageName"
 
@@ -7,25 +7,18 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try {
-    if (-not (Test-Path $downloadPath)) {
-        New-Item -Type Directory -Path $downloadPath | Out-Null
-    }
-
-    Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url
-    Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\"
-
-    if (Test-Path $appDir) {
-        Write-Output "Removing previous version of package..."
-        Remove-Item -Path $appDir -Recurse -Force
-    }
-
-    New-Item -Type Directory -Path $appDir | Out-Null
-
-    Copy-Item -Path "$downloadPath\Release\*" -Destination "$appDir\" -Recurse -Container
-
-    Write-ChocolateySuccess $packageName
-} catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+if (-not (Test-Path $downloadPath)) {
+    New-Item -Type Directory -Path $downloadPath | Out-Null
 }
+
+Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url
+Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\"
+
+if (Test-Path $appDir) {
+    Write-Output "Removing previous version of package..."
+    Remove-Item -Path $appDir -Recurse -Force
+}
+
+New-Item -Type Directory -Path $appDir | Out-Null
+
+Copy-Item -Path "$downloadPath\Release\*" -Destination "$appDir\" -Recurse -Container
