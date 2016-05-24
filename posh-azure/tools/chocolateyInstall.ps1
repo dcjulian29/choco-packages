3b40fcd0c28706072a69cb6226bb88cbc9d6a7c3
Julian Easterling <julian@julianscorner.com>
2016-05-24 04:50:06 -0400

    Updated Azure PowerShell package to 1.4.0

--- posh-azure/tools/chocolateyInstall.ps1
+++ posh-azure/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = "posh-azure"
-$url = "https://github.com/Azure/azure-powershell/releases/download/v1.0.1-November2015/azure-powershell.1.0.1.msi"
+$url = "https://github.com/Azure/azure-powershell/releases/download/v1.4.0-May2016/azure-powershell.1.4.0.msi"
 $installerType = "MSI"
 $installerArgs = "/qb"
 
