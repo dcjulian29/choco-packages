333dfb948b74202d39b44b89d245a27caa4ef780
Julian Easterling <julian@julianscorner.com>
2016-06-20 12:05:00 -0400

    Updated SQL Server Management Studio to 2016

--- sqlmanagementstudio/tools/chocolateyInstall.ps1
+++ sqlmanagementstudio/tools/chocolateyInstall.ps1
@@ -1,37 +1,18 @@
 $packageName = "sqlmanagementstudio"
 $installerType = "EXE"
-$installerArgs = "/QUIETSIMPLE /IACCEPTSQLSERVERLICENSETERMS /ACTION=install /FEATURES=Tools"
-$url = "http://download.microsoft.com/download/1/5/6/156992E6-F7C7-4E55-833D-249BD2348138/ENU/x86/SQLManagementStudio_x86_ENU.exe"
-$url64 = "http://download.microsoft.com/download/1/5/6/156992E6-F7C7-4E55-833D-249BD2348138/ENU/x64/SQLManagementStudio_x64_ENU.exe"
+$installerArgs = "/install /passive /norestart"
+$url = "http://download.microsoft.com/download/E/D/3/ED3B06EC-E4B5-40B3-B861-996B710A540C/SSMS-Setup-ENU.exe"
 $downloadPath = "$($env:TEMP)\chocolatey\$packageName"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try
+if (Test-Path $downloadPath)
 {
-    if (-not (Test-Path $downloadPath))
-    {
-        New-Item -Type Directory -Path $downloadPath | Out-Null
-    }
-
-    Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.exe" $url $url64
-    
-    Push-Location $downloadPath
-    Invoke-Expression "$downloadPath\$packageName.exe /extract:""$downloadPath"" /Q"
-    Pop-Location
-    
-    Wait-Process -Name $packageName -ErrorAction Silent
-
-    Invoke-Expression "$downloadPath\$packageName\setup.exe $installerArgs"
-
-    Wait-Process -Name "SETUP" -ErrorAction Silent
-
-    Write-ChocolateySuccess $packageName
+    Remove-Item -Path $downloadPath -Force -Recurse
 }
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
-}
+
+New-Item -Type Directory -Path $downloadPath | Out-Null
+
+Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url
