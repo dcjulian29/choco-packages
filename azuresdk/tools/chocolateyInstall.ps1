808124ebfd4d36676521cb3a7da9699c8f4b467f
Julian Easterling <julian@julianscorner.com>
2016-06-20 12:36:53 -0400

    Azure SDK install kills the PowerShell Session in order to install the Azure PowerShell module. Since I'm using PowerShell to script the installs, I can no longer install it as a package. Visual Studio will prompt to update the SDK when it runs so it will be done that way...

--- azuresdk/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-$packageName = "azuresdk"
-$webpi = "$($env:ChocolateyInstall)\bin\webpicmd.bat"
-
-if ($psISE) {
-    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
-}
-
-& $webpi /Install /AcceptEula /SuppressReboot /Products:VWDOrVs2015AzurePack.2.9
