444cf12d35dc2fd5be6558ab3b88cc7b983852e2
Julian Easterling <julian@julianscorner.com>
2016-06-20 12:00:55 -0400

    Updated SQL Server Development Tools to latest version.

--- sqlserverssdt/tools/chocolateyInstall.ps1
+++ sqlserverssdt/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = "sqlserverssdt"
 $installerType = "EXE"
 $installerArgs = "/PASSIVE /NORESTART"
-$url = "http://download.microsoft.com/download/4/D/3/4D39DA54-DF09-4628-B63D-685BFCE523EA/Dev14/EN/SSDTSetup.exe"
+$url = "http://download.microsoft.com/download/F/3/E/F3ECF816-0C17-4F54-A881-6BAB7ACD058B/Dev14/EN/SSDTSetup.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
