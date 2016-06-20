247776e396a7fa9f09d5e37a890575e3760d3734
Julian Easterling <julian@julianscorner.com>
2016-06-20 11:50:56 -0400

    updated mongoDB to 3.2.7 and switched from the ZIP installer to the MSI installer

--- mongodb/tools/chocolateyInstall.ps1
+++ mongodb/tools/chocolateyInstall.ps1
@@ -1,46 +1,20 @@
 $packageName = "mongodb"
-$url = "http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-3.2.6.zip"
-$appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
-$downloadPath = "$env:TEMP\chocolatey\$packageName"
+$installerType = "MSI"
+$installerArgs = "/passive /norestart"
+$url = "http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-3.2.7-signed.msi"
 $dataDir = "$($env:SYSTEMDRIVE)\data\mongo"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-if (Get-Service | Where-Object { $_.Name -eq "MongoDB" }) {
-    $cmd = "net.exe stop MongoDB;sc.exe delete MongoDB"
-    if (Test-ProcessAdminRights) {
-        Invoke-Expression $cmd
-    } else {
-        Start-ChocolateyProcessAsAdmin $cmd
-    }
-}
-
-if (Test-Path $appDir) {
-    Write-Output "Removing previous version of package..."
-    Remove-Item "$($appDir)" -Recurse -Force
-}
-
-New-Item -Type Directory -Path $appDir | Out-Null
-
-if (-not (Test-Path $downloadPath)) {
-    New-Item -Type Directory -Path $downloadPath | Out-Null
-}
-
-Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url
-Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\"
-
-$extractPath = $(Get-ChildItem -Directory -Path $downloadPath | Select-Object -First 1).Name
-
-Copy-Item -Path "$($downloadPath)\$($extractPath)\bin\*" -Destination "$appDir" -Recurse
-Copy-Item -Path "$($downloadPath)\$($extractPath)\GNU-AGPL-3.0" -Destination "$appDir"
+Install-ChocolateyPackage $packageName $installerType $installerArgs "" $url
 
 if (-not (Test-Path $dataDir)) {
     New-Item -Type Directory -Path $dataDir | Out-Null
 }
 
-$config = "$($appDir)\mongod.cfg"
+$config = "$($env:ProgramFiles)\MongoDB\Server\3.2\bin\mongod.cfg"
 
 $dataDir = $dataDir -replace '\\', '/'
 
@@ -58,7 +32,8 @@ Add-Content -Path $config -Encoding Ascii -Value "   dbPath: ""$dataDir"""
 Add-Content -Path $config -Encoding Ascii -Value "   directoryPerDB: true"
 Add-Content -Path $config -Encoding Ascii -Value "   engine: wiredTiger"
 
-$cmd = "& $appDir\mongod.exe --config ""$appDir\mongod.cfg"" --install"
+$cmd = "& ""$($env:ProgramFiles)\MongoDB\Server\3.2\bin\mongod.exe"" --config ""$($env:ProgramFiles)\MongoDB\Server\3.2\bin\mongod.cfg"" --install"
+
 if (Test-ProcessAdminRights) {
     Invoke-Expression $cmd
 } else {
--- mongodb/tools/chocolateyUninstall.ps1
+++
@@ -1,7 +0,0 @@
-$packageName = "mongodb"
-$appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
-
-if (Test-Path $appDir)
-{
-  Remove-Item "$($appDir)" -Recurse -Force
-}
