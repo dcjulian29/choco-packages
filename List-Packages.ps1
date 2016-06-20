8c51d0045ebf96a7e704752725159bb67809f7e9
Julian Easterling <julian@julianscorner.com>
2016-06-20 13:49:35 -0400

    Added script to List out package names, version, and website

---
+++ List-Packages.ps1
@@ -0,0 +1,14 @@
+﻿
+$packages = Get-ChildItem  | ?{ $_.PSIsContainer } | Select-Object FullName
+
+foreach ($package in $packages) {
+    $file = $package.FullName
+    $nuspec = [xml](Get-Content "$file\Package.nuspec")
+
+    $packageName = $nuspec.package.metadata.id
+    $version = $nuspec.package.metadata.version
+    $website = $nuspec.package.metadata.projectUrl
+
+    Write-Output "$packageName [$version]  $website"
+
+}
