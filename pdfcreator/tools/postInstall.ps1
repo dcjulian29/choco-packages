7749dda52ce2d91e65b38596c14f838b55fae221
Julian Easterling <julian@julianscorner.com>
2016-06-20 13:50:10 -0400

    Removed two packages that I'm no longer using since the features are provided by Windows 10.

--- pdfcreator/tools/postInstall.ps1
+++
@@ -1,6 +0,0 @@
-$key = "{0001B4FD-9EA3-4D90-A79E-FD14BA3AB01D}"
-
-Get-ChildItem HKLM:\SOFTWARE\Classes -Recurse -Include "*$key*" -ErrorAction SilentlyContinue `
-    | Remove-Item -Recurse -Force
-
-Remove-Item "$($env:PUBLIC)\Desktop\PDFCreator.lnk" -Force
