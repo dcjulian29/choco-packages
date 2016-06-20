24fe68648e15c8532608127e3d310a31876b1203
Julian Easterling <julian@julianscorner.com>
2016-06-20 11:56:14 -0400

    Updated VLC to 2.2.4

--- vlc/tools/chocolateyInstall.ps1
+++ vlc/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = "vlc"
 $installerType = "exe"
 $installerArgs = "/L=1033 /S"
-$url = "http://get.videolan.org/vlc/2.2.3/win32/vlc-2.2.3-win32.exe"
+$url = "http://get.videolan.org/vlc/2.2.4/win32/vlc-2.2.4-win32.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
