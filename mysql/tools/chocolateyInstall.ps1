20fc9d5bae3071f8539da5925f98b7e23220ce1d
Julian Easterling <julian@julianscorner.com>
2016-06-20 12:01:52 -0400

    Updated MySQL Server to 5.7.13

--- mysql/tools/chocolateyInstall.ps1
+++ mysql/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "mysql"
-$url = "http://cdn.mysql.com/Downloads/MySQL-5.7/mysql-5.7.11-win32.zip"
-$url64 = "http://cdn.mysql.com/Downloads/MySQL-5.7/mysql-5.7.11-winx64.zip"
+$url = "http://cdn.mysql.com/Downloads/MySQL-5.7/mysql-5.7.13-win32.zip"
+$url64 = "http://cdn.mysql.com/Downloads/MySQL-5.7/mysql-5.7.13-winx64.zip"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
 $downloadPath = "$env:TEMP\chocolatey\$packageName"
 $dataDir = "$($env:SYSTEMDRIVE)\data\mysql"
@@ -10,63 +10,53 @@ if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
 
-try
-{
-    if (Get-Service | Where-Object { $_.Name -eq $sn }) {
-        $cmd = "Stop-Service -ErrorAction 0 -Name $sn;sc.exe delete $sn"
-        if (Test-ProcessAdminRights) {
-            Invoke-Expression $cmd
-        } else {
-            Start-ChocolateyProcessAsAdmin $cmd
-        }
-    }
-
-    if (Test-Path $appDir)
-    {
-        Write-Output "Removing previous version of package..."
-        Remove-Item "$($appDir)" -Recurse -Force
-    }
-
-    New-Item -Type Directory -Path $appDir | Out-Null
-    
-    if (-not (Test-Path $downloadPath))
-    {
-        New-Item -Type Directory -Path $downloadPath | Out-Null
-    }
-
-    Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url $url64
-    Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\"
-
-    $extractPath = $(Get-ChildItem -Directory -Path $downloadPath | Select-Object -First 1).Name
-
-    New-Item -Type Directory -Path $appDir\bin | Out-Null
-    Copy-Item -Path "$($downloadPath)\$($extractPath)\bin\*" -Destination "$appDir\bin" -Recurse
-    New-Item -Type Directory -Path $appDir\bin\share | Out-Null
-    Copy-Item -Path "$($downloadPath)\$($extractPath)\share\*" -Destination "$appDir\bin\share" -Recurse
-
-    if (-not (Test-Path $dataDir))
-    {
-        New-Item -Type Directory -Path $dataDir | Out-Null
-        Copy-Item -Path "$($downloadPath)\$($extractPath)\data\*" -Destination "$dataDir" -Recurse
-    }
-
-    $config = "$appDir\my.ini"
-
-    Set-Content -Path $config -Encoding Ascii -Value "[mysqld]"
-    Add-Content -Path $config -Encoding Ascii -Value "basedir=$(""$appDir\bin"" -replace '\\', '\\')"
-    Add-Content -Path $config -Encoding Ascii -Value "datadir=$($dataDir -replace '\\', '\\')"
-
-    $cmd = "& $appDir\bin\mysqld.exe --install"
+if (Get-Service | Where-Object { $_.Name -eq $sn }) {
+    $cmd = "Stop-Service -ErrorAction 0 -Name $sn;sc.exe delete $sn"
     if (Test-ProcessAdminRights) {
         Invoke-Expression $cmd
     } else {
         Start-ChocolateyProcessAsAdmin $cmd
     }
-
-    Write-ChocolateySuccess $packageName
 }
-catch
+
+if (Test-Path $appDir)
 {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+    Write-Output "Removing previous version of package..."
+    Remove-Item "$($appDir)" -Recurse -Force
+}
+
+New-Item -Type Directory -Path $appDir | Out-Null
+
+if (-not (Test-Path $downloadPath))
+{
+    New-Item -Type Directory -Path $downloadPath | Out-Null
+}
+
+Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url $url64
+Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\"
+
+$extractPath = $(Get-ChildItem -Directory -Path $downloadPath | Select-Object -First 1).Name
+
+New-Item -Type Directory -Path $appDir\bin | Out-Null
+Copy-Item -Path "$($downloadPath)\$($extractPath)\bin\*" -Destination "$appDir\bin" -Recurse
+New-Item -Type Directory -Path $appDir\bin\share | Out-Null
+Copy-Item -Path "$($downloadPath)\$($extractPath)\share\*" -Destination "$appDir\bin\share" -Recurse
+
+if (-not (Test-Path $dataDir))
+{
+    New-Item -Type Directory -Path $dataDir | Out-Null
+    Copy-Item -Path "$($downloadPath)\$($extractPath)\data\*" -Destination "$dataDir" -Recurse
+}
+
+$config = "$appDir\my.ini"
+
+Set-Content -Path $config -Encoding Ascii -Value "[mysqld]"
+Add-Content -Path $config -Encoding Ascii -Value "basedir=$(""$appDir\bin"" -replace '\\', '\\')"
+Add-Content -Path $config -Encoding Ascii -Value "datadir=$($dataDir -replace '\\', '\\')"
+
+$cmd = "& $appDir\bin\mysqld.exe --install"
+if (Test-ProcessAdminRights) {
+    Invoke-Expression $cmd
+} else {
+    Start-ChocolateyProcessAsAdmin $cmd
 }
