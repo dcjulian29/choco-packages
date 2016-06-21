82d09e8e770f62756d3b27383afad0ecb99b1365
Julian Easterling <julian@julianscorner.com>
2016-06-21 14:42:51 -0400

    Updated OpenVPN to 2.3.11

--- openvpn/tools/chocolateyInstall.ps1
+++ openvpn/tools/chocolateyInstall.ps1
@@ -1,23 +1,13 @@
 ﻿$packageName = "openvpn" # arbitrary name for the package, used in messages
 $installerType = "exe" #only one of these two: exe or msi
 $installerArgs = "/S"
-$url = "http://swupdate.openvpn.org/community/releases/openvpn-install-2.3.6-I603-i686.exe"
-$url64 = "http://swupdate.openvpn.org/community/releases/openvpn-install-2.3.6-I603-x86_64.exe"
+$url = "https://swupdate.openvpn.org/community/releases/openvpn-install-2.3.11-I601-i686.exe"
+$url64 = "https://swupdate.openvpn.org/community/releases/openvpn-install-2.3.11-I601-x86_64.exe"
 
-try
-{
-    Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
+Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
 
-    if (Test-ProcessAdminRights) {
-        Remove-Item "$($env:PUBLIC)\Desktop\OpenVPN GUI.lnk" -Force
-    } else {
-        Start-ChocolateyProcessAsAdmin "Remove-Item '$($env:PUBLIC)\Desktop\OpenVPN GUI.lnk' -Force"
-    }
-
-    Write-ChocolateySuccess $packageName
-}
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+if (Test-ProcessAdminRights) {
+    Remove-Item "$($env:PUBLIC)\Desktop\OpenVPN GUI.lnk" -Force
+} else {
+    Start-ChocolateyProcessAsAdmin "Remove-Item '$($env:PUBLIC)\Desktop\OpenVPN GUI.lnk' -Force"
 }
