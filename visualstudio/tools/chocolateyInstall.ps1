702dced240813a5376f893112f02d0ff7cc8a40c
Julian Easterling <julian@julianscorner.com>
2016-06-27 21:20:26 -0400

    Updated Visual Studio 2015 to Update 3

--- visualstudio/tools/chocolateyInstall.ps1
+++ visualstudio/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@ $packageName = "visualstudio"
 $installerType = "EXE"
 $installerArgs = "/PASSIVE /NORESTART"
 $url = "http://download.microsoft.com/download/6/4/7/647EC5B1-68BE-445E-B137-916A0AE51304/vs_enterprise.exe"
-$update = "http://download.microsoft.com/download/c/8/6/c868d99e-f6cb-4b6f-955e-4571614e6fdb/vs2015.2.exe"
+$update = "http://download.microsoft.com/download/4/8/f/48f0645f-51b6-4733-b808-63e640cddaec/vs2015.3.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
