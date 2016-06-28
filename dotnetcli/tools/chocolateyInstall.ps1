a176654c7450d56797787039e9e527deba2229e5
Julian Easterling <julian@julianscorner.com>
2016-06-27 21:32:34 -0400

    Updated dotnet core SDK CLI to lates preview version

--- dotnetcli/tools/chocolateyInstall.ps1
+++ dotnetcli/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName = "dotnetcli"
 $installerType = 'exe'
 $installerArgs = '/passive'
-$url = 'https://download.microsoft.com/download/4/6/1/46116DFF-29F9-4FF8-94BF-F9BE05BE263B/packages/DotNetCore.1.0.0.RC2-SDK.Preview1-x86.exe'
-$url64 = 'https://download.microsoft.com/download/4/6/1/46116DFF-29F9-4FF8-94BF-F9BE05BE263B/packages/DotNetCore.1.0.0.RC2-SDK.Preview1-x64.exe'
+$url = 'https://download.microsoft.com/download/A/3/8/A38489F3-9777-41DD-83F8-2CBDFAB2520C/packages/DotNetCore.1.0.0-SDK.Preview2-x86.exe'
+$url64 = 'https://download.microsoft.com/download/A/3/8/A38489F3-9777-41DD-83F8-2CBDFAB2520C/packages/DotNetCore.1.0.0-SDK.Preview2-x64.exe'
 
 $path = 'HKLM:\SOFTWARE\dotnet\Setup'
 $version = "1.0.0"
