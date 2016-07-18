0e5616065086a4928aa0b53bedb6d2f7601c84fe
Julian Easterling <julian@julianscorner.com>
2016-07-18 17:32:33 -0400

    Updated install of visual studio to also be able to install community and professional editions depending on the argument passes in...

--- visualstudio/tools/chocolateyInstall.ps1
+++ visualstudio/tools/chocolateyInstall.ps1
@@ -1,12 +1,31 @@
 $packageName = "visualstudio"
 $installerType = "EXE"
 $installerArgs = "/PASSIVE /NORESTART"
-$url = "http://download.microsoft.com/download/6/4/7/647EC5B1-68BE-445E-B137-916A0AE51304/vs_enterprise.exe"
-$update = "http://download.microsoft.com/download/4/8/f/48f0645f-51b6-4733-b808-63e640cddaec/vs2015.3.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
 }
+
+$arguments = ($env:chocolateyInstallArguments).ToLower()
+
+switch ($arguments) {
+    "community" {
+        $url = "http://download.microsoft.com/download/D/2/3/D23F4D0F-BA2D-4600-8725-6CCECEA05196/vs_community_ENU.exe"
+        break
+    }
+
+    "professional" {
+        $url = "http://download.microsoft.com/download/D/2/8/D28D3B41-BF4A-409A-AFB5-2C82C216D4E1/vs_professional_ENU.exe"
+        break
+    }
+
+    default {
+        $url = "http://download.microsoft.com/download/C/7/8/C789377D-7D49-4331-8728-6CED518956A0/vs_enterprise_ENU.exe"
+        break
+    }
+}
+
+$update = "http://download.microsoft.com/download/4/8/f/48f0645f-51b6-4733-b808-63e640cddaec/vs2015.3.exe"
 
 if (Test-Path 'HKLM:\SOFTWARE\Wow6432Node' ) {
     $path = 'HKLM:\SOFTWARE\Wow6432Node'
