60178546b77783bd164ac2c0139fa705fe0636d9
Julian Easterling <julian@julianscorner.com>
2016-08-09 08:33:36 -0400

    Updated my PowerShell scripts to the latest version.

--- myscripts-powershell/tools/chocolateyInstall.ps1
+++ myscripts-powershell/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "myscripts-powershell"
 $appDir = "$($env:SYSTEMDRIVE)\tools\powershell"
-$version = "2016.7.27"
+$version = "2016.8.9"
 $repo = "scripts-powershell"
 $url = "https://github.com/dcjulian29/$repo/archive/$version.zip"
 $toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
