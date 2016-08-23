f4b269d93f4521a631b178208fd05312c9933ae5
Julian Easterling <julian@julianscorner.com>
2016-08-23 16:30:17 -0400

    Updated ChefDK to 0.17.17.1

--- chefdk/tools/chocolateyInstall.ps1
+++ chefdk/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = "chefdk"
 $installerType = "MSI"
 $installerArgs = '/passive'
-$url = "https://packages.chef.io/stable/windows/2012r2/chefdk-0.16.28-1-x86.msi"
+$url = "https://packages.chef.io/stable/windows/2012r2/chefdk-0.17.17-1-x86.msi"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
