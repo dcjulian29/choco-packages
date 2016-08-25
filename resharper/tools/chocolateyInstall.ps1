fb9c835abe7aa6b2e83d7717072a3562f8255ef9
Julian Easterling <julian@julianscorner.com>
2016-08-25 09:00:19 -0400

    Updated ReSharper to 2016.2

--- resharper/tools/chocolateyInstall.ps1
+++ resharper/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = "resharper"
 $installerType = "EXE"
 $installerArgs = "/VsVersion=14 /SpecificProductNames=ReSharper;dotCover;teamCityAddin;dotPeek /Silent=True"
-$url = "https://download.jetbrains.com/resharper/JetBrains.ReSharperUltimate.2016.1.exe"
+$url = "https://download.jetbrains.com/resharper/JetBrains.ReSharperUltimate.2016.2.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
--- resharper/tools/chocolateyUninstall.ps1
+++ resharper/tools/chocolateyUninstall.ps1
@@ -1,7 +1,7 @@
 $packageName = "resharper"
 $installerType = "EXE"
 $installerArgs = "/SpecificProductNames=ReSharper;dotCover;teamCityAddin;dotPeek /Silent=True /ReSharper9PlusMsi=True"
-$url = "https://download.jetbrains.com/resharper/JetBrains.ReSharperUltimate.2016.1.exe"
+$url = "https://download.jetbrains.com/resharper/JetBrains.ReSharperUltimate.2016.2.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
@@ -9,5 +9,5 @@ if ($psISE) {
 
 Push-Location $env:TEMP
 Get-WebFile $url
-Uninstall-ChocolateyPackage $packageName $installerType $installerArgs "JetBrains.ReSharperUltimate.2016.1.exe"
+Uninstall-ChocolateyPackage $packageName $installerType $installerArgs "JetBrains.ReSharperUltimate.2016.2.exe"
 Pop-Location
