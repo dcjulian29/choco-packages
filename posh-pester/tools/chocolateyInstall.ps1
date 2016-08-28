e0c18500e5a976855d2c2b7c157a0a1505a8b8bf
Julian Easterling <julian@julianscorner.com>
2016-08-28 15:26:14 -0400

    updated pester to 3.4.3

--- posh-pester/tools/chocolateyInstall.ps1
+++ posh-pester/tools/chocolateyInstall.ps1
@@ -1,20 +1,20 @@
 $packageName = "posh-pester"
-$version = "3.4.2"
+$version = "3.4.3"
 $url = "https://codeload.github.com/pester/Pester/zip/$version"
 $appDir = "$($env:UserProfile)\Documents\WindowsPowerShell\Modules\pester"
-$downloadPath = "$env:TEMP\chocolatey\$packageName"
+$downloadPath = "$env:TEMP\$packageName"
 
-if ($psISE) {
-    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
-}
-
-if (-not (Test-Path $downloadPath))
+if (Test-Path $downloadPath)
 {
-    New-Item -Type Directory -Path $downloadPath | Out-Null
+    Remove-Item -Path $downloadPath -Recurse -Force
 }
 
-Get-ChocolateyWebFile $packageName "$downloadPath\$version.zip" $url
-Get-ChocolateyUnzip "$downloadPath\$version.zip" "$downloadPath\"
+New-Item -Type Directory -Path $downloadPath | Out-Null
+
+(New-Object System.Net.WebClient).DownloadFile($url, "$downloadPath\$version.zip")
+
+[System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
+[System.IO.Compression.ZipFile]::ExtractToDirectory("$downloadPath\$version.zip", $downloadPath)
 
 if (Test-Path $appDir)
 {
