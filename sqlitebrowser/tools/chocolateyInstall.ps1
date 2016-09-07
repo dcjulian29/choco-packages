7f16a816fd9a1c77e590995621cdb65f9e72724c
Julian Easterling <julian@julianscorner.com>
2016-09-07 13:04:55 -0400

    Updated SQLite Browser to 3.9.0

--- sqlitebrowser/tools/chocolateyInstall.ps1
+++ sqlitebrowser/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName = "sqlitebrowser"
 $installerType = "EXE"
 $installerArgs = "/S"
-$url = "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v3.8.0/sqlitebrowser-3.8.0-win32v3.exe"
-$url64 = 'https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v3.8.0/sqlitebrowser-3.8.0-win64v2.exe'
+$url = "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v3.9.0/sqlitebrowser-3.9.0-win32v3.exe"
+$url64 = 'https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v3.9.0/sqlitebrowser-3.9.0-win64v2.exe'
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
