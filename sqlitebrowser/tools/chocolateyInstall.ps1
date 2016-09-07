807ffdab85c29a21692ddaff72b7a3a01d6293aa
Julian Easterling <julian@julianscorner.com>
2016-09-07 13:15:18 -0400

    SQLite Browser URLs were wrong.

--- sqlitebrowser/tools/chocolateyInstall.ps1
+++ sqlitebrowser/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName = "sqlitebrowser"
 $installerType = "EXE"
 $installerArgs = "/S"
-$url = "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v3.9.0/sqlitebrowser-3.9.0-win32v3.exe"
-$url64 = 'https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v3.9.0/sqlitebrowser-3.9.0-win64v2.exe'
+$url = "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v3.9.0/DB.Browser.for.SQLite-3.9.0-win32.exe"
+$url64 = "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v3.9.0/DB.Browser.for.SQLite-3.9.0-win64.exe"
 
 if ($psISE) {
     Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
