c714db36adf702ab424da4db8d1a7a870e0c3d94
Julian Easterling <julian@julianscorner.com>
2016-09-14 12:10:55 -0400

    Updated Wireshark to 2.2.0

--- wireshark/tools/chocolateyInstall.ps1
+++ wireshark/tools/chocolateyInstall.ps1
@@ -1,11 +1,20 @@
 $packageName = "wireshark"
 $installerType = "EXE"
-$installerArgs = "/S"
-$url = "https://2.na.dl.wireshark.org/win32/Wireshark-win32-2.0.5.exe"
-$url64 = "https://2.na.dl.wireshark.org/win64/Wireshark-win64-2.0.5.exe"
+$installerArgs = "/S /quicklaunchicon=no"
+$url = "https://www.wireshark.org/download/win32/all-versions/Wireshark-win32-2.2.0.exe"
+$url64 = "https://www.wireshark.org/download/win64/all-versions/Wireshark-win64-2.2.0.exe"
+$downloadPath = "$env:TEMP\$packageName\"
 
-if ($psISE) {
-    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+if ([System.IntPtr]::Size -ne 4) {
+    $url = $url64
 }
 
-Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
+if (Test-Path $downloadPath) {
+    Remove-Item -Path $downloadPath -Recurse -Force
+}
+
+New-Item -Type Directory -Path $downloadPath | Out-Null
+
+(New-Object System.Net.WebClient).DownloadFile($url, "$downloadPath\$packageName.$installerType")
+
+& "$downloadPath\$packageName.$installerType" $installerArgs
