3081b7e163e5a97a8a6942476fedc921f9675a91
Julian Easterling <julian@julianscorner.com>
2016-09-16 12:42:58 -0400

    docker for windows isn't stable inside a Hyper-V VM even with the virtualization extensions enabled so for now, I'll use docker for windows on my host system and docker-toolbox inside my development VM.

---
+++ docker-toolbox/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = "docker-toolbox"
+$installerType = "EXE"
+$installerArgs = "/SILENT /COMPONENTS=docker,dockermachine,dockercompose"
+
+$url = "https://github.com/docker/toolbox/releases/download/v1.12.1/DockerToolbox-1.12.1.exe"
+
+$downloadPath = "$env:TEMP\$packageName\"
+
+if (Test-Path $downloadPath) {
+    Remove-Item -Path $downloadPath -Recurse -Force
+}
+
+New-Item -Type Directory -Path $downloadPath | Out-Null
+
+(New-Object System.Net.WebClient).DownloadFile($url, "$downloadPath\$packageName.$installerType")
+
+Invoke-Expression "$(Join-Path -Path $downloadPath -ChildPath "$packageName.$installerType") $installerArgs"
--- docker/tools/chocolateyInstall.ps1
+++ docker/tools/chocolateyInstall.ps1
@@ -2,9 +2,14 @@ $packageName = "docker"
 $installerType = "MSI"
 $installerArgs = '/norestart /passive'
 $url = "https://download.docker.com/win/stable/InstallDocker.msi"
+$downloadPath = "$env:TEMP\$packageName\"
 
-if ($psISE) {
-    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+if (Test-Path $downloadPath) {
+    Remove-Item -Path $downloadPath -Recurse -Force
 }
 
-Install-ChocolateyPackage $packageName $installerType $installerArgs $null $url
+New-Item -Type Directory -Path $downloadPath | Out-Null
+
+(New-Object System.Net.WebClient).DownloadFile($url, "$downloadPath\$packageName.$installerType")
+
+& "$downloadPath\$packageName.$installerType" $installerArgs
