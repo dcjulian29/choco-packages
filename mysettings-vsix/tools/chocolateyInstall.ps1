97da9d60cf9385c7fc39dbf27f299e3941faa7be
Julian Easterling <julian@julianscorner.com>
2016-09-20 14:32:21 -0400

    Removed SpecFlow visual studio extension. I don't have any project currently or in the near term that are using it.

--- mysettings-vsix/tools/chocolateyInstall.ps1
+++ mysettings-vsix/tools/chocolateyInstall.ps1
@@ -1,7 +1,3 @@
 $packageName = "mysettings-vsix"
 
-if ($psISE) {
-    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
-}
-
-Write-Output "Visual Studio VSIX Virtual Package."
+# Visual Studio VSIX Virtual Package.
