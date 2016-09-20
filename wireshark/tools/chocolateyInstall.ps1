d4c766f70933dbb33cf6d0a47df73acf36100839
Julian Easterling <julian@julianscorner.com>
2016-09-20 12:59:19 -0400

    Updated wireshark to fix an issue with the download path and to replace the file download with a new function.

--- wireshark/tools/chocolateyInstall.ps1
+++ wireshark/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $installerType = "EXE"
 $installerArgs = "/S /quicklaunchicon=no"
 $url = "https://www.wireshark.org/download/win32/all-versions/Wireshark-win32-2.2.0.exe"
 $url64 = "https://www.wireshark.org/download/win64/all-versions/Wireshark-win64-2.2.0.exe"
-$downloadPath = "$env:TEMP\$packageName\"
+$downloadPath = "$env:TEMP\$packageName"
 
 if ([System.IntPtr]::Size -ne 4) {
     $url = $url64
@@ -15,6 +15,6 @@ if (Test-Path $downloadPath) {
 
 New-Item -Type Directory -Path $downloadPath | Out-Null
 
-(New-Object System.Net.WebClient).DownloadFile($url, "$downloadPath\$packageName.$installerType")
+Download-File $url "$downloadPath\$packageName.$installerType"
 
 & "$downloadPath\$packageName.$installerType" $installerArgs
