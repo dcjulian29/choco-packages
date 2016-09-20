af3204d678ae523d74bbfd2c0d81c2cd2aa706bb
Julian Easterling <julian@julianscorner.com>
2016-09-20 12:57:40 -0400

    Updated nodejs to 6.6.0 and removed last of the chocolatey helper functions in it.

--- nodejs/tools/chocolateyInstall.ps1
+++ nodejs/tools/chocolateyInstall.ps1
@@ -2,14 +2,24 @@ $packageName = "nodejs"
 $installerType = "MSI"
 $installerArgs = "/qb"
 
-$version = "6.5.0"
+$version = "6.6.0"
 $rootUrl = "https://nodejs.org/dist/v$version"
 
 $url = "$rootUrl/node-v$version-x86.msi"
 $url64 = "$rootUrl/node-v$version-x64.msi"
 
-if ($psISE) {
-    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+$downloadPath = "$env:TEMP\$packageName\"
+
+if ([System.IntPtr]::Size -ne 4) {
+    $url = $url64
 }
 
-Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
+if (Test-Path $downloadPath) {
+    Remove-Item -Path $downloadPath -Recurse -Force
+}
+
+New-Item -Type Directory -Path $downloadPath | Out-Null
+
+Download-File $url "$downloadPath\$packageName.$installerType"
+
+& "$downloadPath\$packageName.$installerType" $installerArgs
