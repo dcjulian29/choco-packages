593c0fd908d07d420dd88d1d07e800b5ce69596b
Julian Easterling <julian@julianscorner.com>
2016-09-20 12:56:19 -0400

    Updated my PowerShell package to include the latest changes to my scripts which will replace the need in many of these scripts for the chocolatey helper functions. I also removed the last of the chocolatey helper functions in this package.

--- myscripts-powershell/tools/chocolateyInstall.ps1
+++ myscripts-powershell/tools/chocolateyInstall.ps1
@@ -1,13 +1,15 @@
 $packageName = "myscripts-powershell"
 $appDir = "$($env:SYSTEMDRIVE)\tools\powershell"
-$version = "2016.9.9"
+$version = "2016.9.20"
 $repo = "scripts-powershell"
 $url = "https://github.com/dcjulian29/$repo/archive/$version.zip"
-$toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
 
-if ($psISE) {
-    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
-}
+$file = "$repo-$version"
+
+(New-Object System.Net.WebClient).DownloadFile("$url", "$env:TEMP\$file.zip")
+
+[System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
+[System.IO.Compression.ZipFile]::ExtractToDirectory("$env:TEMP\$file.zip", $env:TEMP)
 
 if (Test-Path $appDir\Modules) {
     cmd /c rmdir "$appdir\Modules"
@@ -24,22 +26,17 @@ if (-not (Test-Path $appDir))
     New-Item -Type Directory -Path $appDir | Out-Null
 }
 
-$file = "$repo-$version"
-
-(New-Object System.Net.WebClient).DownloadFile("$url", "$env:TEMP\$file.zip")
-
-[System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
-[System.IO.Compression.ZipFile]::ExtractToDirectory("$env:TEMP\$file.zip", $env:TEMP)
-
 Copy-Item -Path "$($env:TEMP)\$file\*" -Destination $appdir -Recurse -Force
 
 Remove-Item -Path "$($env:TEMP)\$file" -Recurse -Force
 Remove-Item -Path "$($env:TEMP)\$file.zip" -Force
 
-$cmd = ". $toolDir\postInstall.bat"
+$cmd = ". $PSScriptRoot\postInstall.bat"
 
 if (Test-ProcessAdminRights) {
     Invoke-Expression $cmd
 } else {
-    Start-ChocolateyProcessAsAdmin $cmd
+    Start-Process $PSHOME\powershell.exe `
+        -ArgumentList "-NoProfile", "-ExecutionPolicy Bypass", "-Command `"&{$cmd}`"" `
+        -Wait
 }
