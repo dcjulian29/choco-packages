749ed44b160441c2cbca672fefbc50f6898377ad
Julian Easterling <julian@julianscorner.com>
2016-09-20 12:51:39 -0400

    Updated dotNet Core to 1.0.1 and removed last chocolatey helper functions.

--- dotnetcli/tools/chocolateyInstall.ps1
+++ dotnetcli/tools/chocolateyInstall.ps1
@@ -1,24 +1,20 @@
 $packageName = "dotnetcli"
 $installerType = 'exe'
 $installerArgs = '/passive'
-$url = 'https://download.microsoft.com/download/A/3/8/A38489F3-9777-41DD-83F8-2CBDFAB2520C/packages/DotNetCore.1.0.0-SDK.Preview2-x86.exe'
-$url64 = 'https://download.microsoft.com/download/A/3/8/A38489F3-9777-41DD-83F8-2CBDFAB2520C/packages/DotNetCore.1.0.0-SDK.Preview2-x64.exe'
+$url = 'https://download.microsoft.com/download/0/A/3/0A372822-205D-4A86-BFA7-084D2CBE9EDF/DotNetCore.1.0.1-SDK.1.0.0.Preview2-003131-x86.exe'
+$url64 = 'https://download.microsoft.com/download/0/A/3/0A372822-205D-4A86-BFA7-084D2CBE9EDF/DotNetCore.1.0.1-SDK.1.0.0.Preview2-003131-x64.exe'
+$downloadPath = "$env:TEMP\$packageName\"
 
-$path = 'HKLM:\SOFTWARE\dotnet\Setup'
-$version = "1.0.0"
-
-if ($psISE) {
-    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+if ([System.IntPtr]::Size -ne 4) {
+    $url = $url64
 }
 
-#if (Test-Path $Path) {
-#    $installedversion =  (Get-ItemProperty -Path $Path -Name Version).Version
-#} else {
-#    $installedversion = "0.0.0"
-#}
+if (Test-Path $downloadPath) {
+    Remove-Item -Path $downloadPath -Recurse -Force
+}
 
-#if (-not ($installedversion.StartsWith($version))) {
-    Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
-#} else {
-#    Write-Host "Microsoft .Net CLI is already installed on this machine."
-#}
+New-Item -Type Directory -Path $downloadPath | Out-Null
+
+Download-File $url "$downloadPath\$packageName.$installerType"
+
+& "$downloadPath\$packageName.$installerType" $installerArgs
