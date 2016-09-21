3756a208a55cedc4be1876c4183a69bfbc76b039
Julian Easterling <julian@julianscorner.com>
2016-09-21 08:20:04 -0400

    Removed dependency on chocolatey helper functions.

--- xmind/tools/chocolateyInstall.ps1
+++ xmind/tools/chocolateyInstall.ps1
@@ -2,8 +2,17 @@
 $installerType = "EXE"
 $installerArgs = "/SILENT"
 $url = "http://dl3.xmind.net/xmind-7.5-update1-windows.exe"
+$downloadPath = "$env:TEMP\$packageName"
 
-Install-ChocolateyPackage $packageName $installerType $installerArgs $url
+if (Test-Path $downloadPath) {
+    Remove-Item -Path $downloadPath -Recurse -Force
+}
+
+New-Item -Type Directory -Path $downloadPath | Out-Null
+
+Download-File $url "$downloadPath\$packageName.$installerType"
+
+& "$downloadPath\$packageName.$installerType" $installerArgs
 
 if (Test-Path "$($env:USERPROFILE)\Desktop\XMind 7.lnk") {
     Remove-Item "$($env:USERPROFILE)\Desktop\XMind 7.lnk" -force
