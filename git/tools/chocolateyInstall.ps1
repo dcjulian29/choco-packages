ff5a152dd9a06f798326c34a426010d7cc48561b
Julian Easterling <julian@julianscorner.com>
2016-09-21 08:12:04 -0400

    Updated Git to 2.10.0 and removed last remaining chocolatey helper functions from scripts.

--- git/tools/chocolateyInstall.ps1
+++ git/tools/chocolateyInstall.ps1
@@ -4,9 +4,18 @@ $installerArgs = '/SILENT /COMPONENTS="!ext,!ext\cheetah,!assoc,!assoc_sh"'
 $version = "2.9.3"
 $url = "https://github.com/git-for-windows/git/releases/download/v$version.windows.1/Git-$version-32-bit.exe"
 $url64 = "https://github.com/git-for-windows/git/releases/download/v$version.windows.1/Git-$version-64-bit.exe"
+$downloadPath = "$env:TEMP\$packageName"
 
-if ($psISE) {
-    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+if ([System.IntPtr]::Size -ne 4) {
+    $url = $url64
 }
 
-Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
+if (Test-Path $downloadPath) {
+    Remove-Item -Path $downloadPath -Recurse -Force
+}
+
+New-Item -Type Directory -Path $downloadPath | Out-Null
+
+Download-File $url "$downloadPath\$packageName.$installerType"
+
+& "$downloadPath\$packageName.$installerType" $installerArgs
--- git/tools/chocolateyUninstall.ps1
+++ git/tools/chocolateyUninstall.ps1
@@ -8,8 +8,8 @@ if (Test-Path "${env:ProgramFiles(x86)}\Git") {
 
 $uninstaller = Join-Path $git "unins000.exe"
 
-if (Test-ProcessAdminRights) {
-    Start-ChocolateyProcessAsAdmin "cmd.exe /c `"$uninstaller`" /SILENT"
-} else {
+if (Test-Elevation) {
     Invoke-Expression "cmd.exe /c `"$uninstaller`" /SILENT"
+} else {
+    Invoke-ElevatedCommand cmd.exe /c "$uninstaller" /SILENT
 }
