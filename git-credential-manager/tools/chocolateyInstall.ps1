25621bb83c50dc54f526a22209cd402925220d75
Julian Easterling <julian@julianscorner.com>
2016-09-26 23:30:41 -0400

    Updated Git Credential Manager to 1.6.1

--- git-credential-manager/tools/chocolateyInstall.ps1
+++ git-credential-manager/tools/chocolateyInstall.ps1
@@ -1,6 +1,15 @@
 $packageName = "git-credential-manager"
-$url = "https://github.com/Microsoft/Git-Credential-Manager-for-Windows/releases/download/v1.5.0/GCMW-1.5.0.exe"
-$installerType = 'exe'
-$silentArgs = '/SILENT /NORESTART'
+$url = "https://github.com/Microsoft/Git-Credential-Manager-for-Windows/releases/download/v1.6.1/GCMW-1.6.1.exe"
+$installerType = "exe"
+$installerArgs = "/SILENT /NORESTART"
+$downloadPath = "$env:TEMP\$packageName"
  
-Install-ChocolateyPackage $packageName $installerType $silentArgs $url
+if (Test-Path $downloadPath) {
+    Remove-Item -Path $downloadPath -Recurse -Force
+}
+
+New-Item -Type Directory -Path $downloadPath | Out-Null
+
+Download-File $url "$downloadPath\$packageName.$installerType"
+
+Invoke-ElevatedCommand "$downloadPath\$packageName.$installerType" -ArgumentList $installerArgs -Wait
