aae123912029576892349846ca6648a77a517914
Julian Easterling <julian@julianscorner.com>
2016-10-03 13:23:31 -0400

    updated NodeJS to 6.7.0

--- nodejs/tools/chocolateyInstall.ps1
+++ nodejs/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@ $packageName = "nodejs"
 $installerType = "MSI"
 $installerArgs = "/qb"
 
-$version = "6.6.0"
+$version = "6.7.0"
 $rootUrl = "https://nodejs.org/dist/v$version"
 
 $url = "$rootUrl/node-v$version-x86.msi"
