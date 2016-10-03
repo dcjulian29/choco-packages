fa6c8fb185e481bd9d40c609baa75bda689b2779
Julian Easterling <julian@julianscorner.com>
2016-10-03 13:19:58 -0400

    Updated ChefDK to 0.18.26

--- chefdk/tools/chocolateyInstall.ps1
+++ chefdk/tools/chocolateyInstall.ps1
@@ -1,10 +1,15 @@
 $packageName = "chefdk"
 $installerType = "MSI"
 $installerArgs = '/passive'
-$url = "https://packages.chef.io/stable/windows/2012r2/chefdk-0.17.17-1-x86.msi"
+$url = "https://packages.chef.io/stable/windows/2012r2/chefdk-0.18.26-1-x86.msi"
+$downloadPath = "$env:TEMP\$packageName"
 
-if ($psISE) {
-    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+if (Test-Path $downloadPath) {
+    Remove-Item -Path $downloadPath -Recurse -Force
 }
 
-Install-ChocolateyPackage $packageName $installerType $installerArgs $url
+New-Item -Type Directory -Path $downloadPath | Out-Null
+
+Download-File $url "$downloadPath\$packageName.$installerType"
+
+Invoke-ElevatedCommand "$downloadPath\$packageName.$installerType" -ArgumentList $installerArgs -Wait
