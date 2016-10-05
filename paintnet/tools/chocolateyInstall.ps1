370be32b77009e44c2f73c202da5cbf37d75a8c4
Julian Easterling <julian@julianscorner.com>
2016-10-05 10:14:39 -0400

    Updated Paint.Net to 4.0.12

--- paintnet/tools/chocolateyInstall.ps1
+++ paintnet/tools/chocolateyInstall.ps1
@@ -1,20 +1,17 @@
 $packageName = "paintnet"
 $installerType = "EXE"
 $installerArgs = "/auto DESKTOPSHORTCUT=0"
-$url = "http://www.dotpdn.com/files/paint.net.4.0.10.install.zip"
-$downloadPath = "$env:TEMP\chocolatey\$packageName"
-
-if ($psISE) {
-    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
-}
+$version = "4.0.12"
+$url = "http://www.dotpdn.com/files/paint.net.$version.install.zip"
+$downloadPath = "$env:TEMP\$packageName"
 
 if (Test-Path $downloadPath) {
-    Remove-Item $downloadPath -Recurse -Force
+    Remove-Item -Path $downloadPath -Recurse -Force
 }
 
 New-Item -Type Directory -Path $downloadPath | Out-Null
 
-Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url $url
-Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\"
+Download-File $url "$downloadPath\$packageName.$installerType"
+Unzip-File "$downloadPath\$packageName.$installerType" $downloadPath
 
-Install-ChocolateyPackage $packageName $installerType $installerArgs "$downloadPath\paint.net.4.0.10.install.exe"
+Invoke-ElevatedCommand "$downloadPath\paint.net.$version.install.exe" -ArgumentList $installerArgs -Wait
