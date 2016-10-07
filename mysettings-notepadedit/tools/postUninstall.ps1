063463c8e1507b384642b5b5d4779d2503c038b5
Julian Easterling <julian@julianscorner.com>
2016-10-07 11:00:51 -0400

    Removed these meta packages as I'm not using Chocolatey on my servers anymore.

--- mysettings-notepadedit/tools/postUninstall.ps1
+++
@@ -1,7 +0,0 @@
-if (-not $(Test-Path HKCR:)) {
-    New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT -Name HKCR
-}
-
-if ($(Test-Path -LiteralPath "HKCR:\*\shell\runas")) {
-    Remove-Item -LiteralPath "HKCR:\*\shell\runas" -Recurse
-}
