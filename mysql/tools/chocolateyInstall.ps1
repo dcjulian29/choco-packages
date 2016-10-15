17ccb5030276d659758800b49df8dc10e2f76b73
Julian Easterling <julian@julianscorner.com>
2016-10-15 04:19:07 -0400

    Updated MySQL to 5.7.16

--- mysql/tools/chocolateyInstall.ps1
+++ mysql/tools/chocolateyInstall.ps1
@@ -1,48 +1,46 @@
 $packageName = "mysql"
-$url = "http://cdn.mysql.com/Downloads/MySQL-5.7/mysql-5.7.15-win32.zip"
-$url64 = "http://cdn.mysql.com/Downloads/MySQL-5.7/mysql-5.7.15-winx64.zip"
+$url = "http://cdn.mysql.com/Downloads/MySQL-5.7/mysql-5.7.16-win32.zip"
+$url64 = "http://cdn.mysql.com/Downloads/MySQL-5.7/mysql-5.7.16-winx64.zip"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
-$downloadPath = "$env:TEMP\chocolatey\$packageName"
+$downloadPath = "$env:TEMP\$packageName"
 $dataDir = "$($env:SYSTEMDRIVE)\data\mysql"
-$sn = "MySQL"
 
-if (Get-Service | Where-Object { $_.Name -eq $sn }) {
-    $cmd = "Stop-Service -ErrorAction 0 -Name $sn;sc.exe delete $sn"
-    if (Test-ProcessAdminRights) {
-        Invoke-Expression $cmd
-    } else {
-        Start-Process -$cmd -verb RunAs
-        Start-ChocolateyProcessAsAdmin $cmd
-    }
+if (Get-Service | Where-Object { $_.Name -eq $packageName }) {
+    $cmd = "Stop-Service -ErrorAction 0 -Name $packageName;sc.exe delete $packageName"
+    Invoke-ElevatedExpression $cmd
 }
 
-if (Test-Path $appDir)
-{
+if (Test-Path $appDir) {
     Write-Output "Removing previous version of package..."
     Remove-Item "$($appDir)" -Recurse -Force
 }
 
 New-Item -Type Directory -Path $appDir | Out-Null
 
-if (-not (Test-Path $downloadPath))
-{
-    New-Item -Type Directory -Path $downloadPath | Out-Null
+if (Test-Path $downloadPath) {
+    Remove-Item -Path $downloadPath -Recurse -Force
 }
 
-Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url $url64
-Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$downloadPath\"
+New-Item -Type Directory -Path $downloadPath | Out-Null
+
+if ([System.IntPtr]::Size -ne 4) {
+    $url = $url64
+}
+
+Download-File $url "$downloadPath\$packageName.zip"
+
+Unzip-File "$downloadPath\$packageName.zip" "$downloadPath\"
 
 $extractPath = $(Get-ChildItem -Directory -Path $downloadPath | Select-Object -First 1).Name
 
 New-Item -Type Directory -Path $appDir\bin | Out-Null
 Copy-Item -Path "$($downloadPath)\$($extractPath)\bin\*" -Destination "$appDir\bin" -Recurse
+
 New-Item -Type Directory -Path $appDir\bin\share | Out-Null
 Copy-Item -Path "$($downloadPath)\$($extractPath)\share\*" -Destination "$appDir\bin\share" -Recurse
 
-if (-not (Test-Path $dataDir))
-{
+if (-not (Test-Path $dataDir)) {
     New-Item -Type Directory -Path $dataDir | Out-Null
-    Copy-Item -Path "$($downloadPath)\$($extractPath)\data\*" -Destination "$dataDir" -Recurse
 }
 
 $config = "$appDir\my.ini"
@@ -51,9 +49,4 @@ Set-Content -Path $config -Encoding Ascii -Value "[mysqld]"
 Add-Content -Path $config -Encoding Ascii -Value "basedir=$(""$appDir\bin"" -replace '\\', '\\')"
 Add-Content -Path $config -Encoding Ascii -Value "datadir=$($dataDir -replace '\\', '\\')"
 
-$cmd = "& $appDir\bin\mysqld.exe --install"
-if (Test-ProcessAdminRights) {
-    Invoke-Expression $cmd
-} else {
-    Start-ChocolateyProcessAsAdmin $cmd
-}
+Invoke-ElevatedExpression "& $appDir\bin\mysqld.exe --install"
