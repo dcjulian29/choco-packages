34c3758a612961553bb3266d02a31a26ed04bbc6
Julian Easterling <julian@julianscorner.com>
2016-11-07 03:38:43 -0500

    Updated SearchMyFiles to 2.74

--- searchmyfiles/tools/chocolateyInstall.ps1
+++ searchmyfiles/tools/chocolateyInstall.ps1
@@ -1,35 +1,28 @@
 $packageName = "searchmyfiles"
 $url = "http://www.nirsoft.net/utils/searchmyfiles.zip"
 $url64 = "http://www.nirsoft.net/utils/searchmyfiles-x64.zip"
-$downloadPath = "$env:TEMP\chocolatey\$packageName"
+$downloadPath = "$env:TEMP\$packageName"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
 
-if ($psISE) {
-    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+if ([System.IntPtr]::Size -ne 4) {
+    $url = $url64
 }
 
-try
+if (Test-Path $downloadPath) {
+    Remove-Item -Path $downloadPath -Recurse -Force
+}
+
+New-Item -Type Directory -Path $downloadPath | Out-Null
+
+Download-File $url "$downloadPath\$packageName.zip"
+
+if (Test-Path $appDir)
 {
-    if (Test-Path $appDir)
-    {
-        Write-Output "Removing previous version of package..."
-        Remove-Item "$($appDir)" -Recurse -Force
-    }
-
-    New-Item -Type Directory -Path $appDir | Out-Null
-    
-    if (-not (Test-Path $downloadPath))
-    {
-        New-Item -Type Directory -Path $downloadPath | Out-Null
-    }
-
-    Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url $url64
-    Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$appDir\"
-
-    Write-ChocolateySuccess $packageName
+    Write-Output "Removing previous version of package..."
+    Remove-Item "$($appDir)" -Recurse -Force
 }
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
-}
+
+New-Item -Type Directory -Path $appDir | Out-Null
+
+Unzip-File "$downloadPath\$packageName.zip" "$appDir\"
+
