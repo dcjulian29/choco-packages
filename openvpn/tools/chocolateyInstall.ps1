db61e0dc8c7b4da1004cf25d215eb46f6738cb6c
Julian Easterling <julian@julianscorner.com>
2016-11-14 16:39:43 -0500

    Updated OpenVPN to 2.3.13

--- openvpn/tools/chocolateyInstall.ps1
+++ openvpn/tools/chocolateyInstall.ps1
@@ -1,13 +1,23 @@
 ﻿$packageName = "openvpn" # arbitrary name for the package, used in messages
 $installerType = "exe" #only one of these two: exe or msi
 $installerArgs = "/S"
-$url = "https://swupdate.openvpn.org/community/releases/openvpn-install-2.3.11-I601-i686.exe"
-$url64 = "https://swupdate.openvpn.org/community/releases/openvpn-install-2.3.11-I601-x86_64.exe"
+$url = "https://swupdate.openvpn.org/community/releases/openvpn-install-2.3.13-I601-i686.exe"
+$url64 = "https://swupdate.openvpn.org/community/releases/openvpn-install-2.3.13-I601-x86_64.exe"
+$downloadPath = "$env:TEMP\$packageName"
 
-Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
-
-if (Test-ProcessAdminRights) {
-    Remove-Item "$($env:PUBLIC)\Desktop\OpenVPN GUI.lnk" -Force
-} else {
-    Start-ChocolateyProcessAsAdmin "Remove-Item '$($env:PUBLIC)\Desktop\OpenVPN GUI.lnk' -Force"
+if ([System.IntPtr]::Size -ne 4) {
+    $url = $url64
 }
+
+if (Test-Path $downloadPath) {
+    Remove-Item -Path $downloadPath -Recurse -Force
+}
+
+New-Item -Type Directory -Path $downloadPath | Out-Null
+
+Download-File $url "$downloadPath\$packageName.$installerType"
+
+Invoke-ElevatedCommand "$downloadPath\$packageName.$installerType" -ArgumentList $installerArgs -Wait
+
+Invoke-ElevatedCommand "Remove-Item" -ArgumentList "'$($env:PUBLIC)\Desktop\OpenVPN GUI.lnk' -Force" -Wait
+
