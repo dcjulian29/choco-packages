aa0bcba2522652e973f77236941052aef520ac37
Julian Easterling <julian@julianscorner.com>
2016-11-14 15:59:42 -0500

    Updated GreenShot to 1.2.8.14

--- greenshot/tools/chocolateyInstall.ps1
+++ greenshot/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "greenshot"
 $installerArgs = "/SILENT /NORESTART"
-$url = "https://github.com/greenshot/greenshot/releases/download/Greenshot-RELEASE-1.2.8.12/Greenshot-INSTALLER-1.2.8.12-RELEASE.exe"
+$url = "https://github.com/greenshot/greenshot/releases/download/Greenshot-RELEASE-1.2.8.14/Greenshot-INSTALLER-1.2.8.14-RELEASE.exe"
 $downloadPath = "$env:TEMP\$packageName"
 
 if (-not (Test-Path "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5")) {
