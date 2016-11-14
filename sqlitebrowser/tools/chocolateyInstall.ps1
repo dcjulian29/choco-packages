5bbb51fb6ed1948e2e1647994bd73e45ca17a597
Julian Easterling <julian@julianscorner.com>
2016-11-14 16:29:41 -0500

    Updated SQLite Browser to 3.9.1

--- sqlitebrowser/tools/chocolateyInstall.ps1
+++ sqlitebrowser/tools/chocolateyInstall.ps1
@@ -1,11 +1,20 @@
 $packageName = "sqlitebrowser"
 $installerType = "EXE"
 $installerArgs = "/S"
-$url = "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v3.9.0/DB.Browser.for.SQLite-3.9.0-win32.exe"
-$url64 = "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v3.9.0/DB.Browser.for.SQLite-3.9.0-win64.exe"
+$url = "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v3.9.1/DB.Browser.for.SQLite-3.9.1-win32.exe"
+$url64 = "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v3.9.1/DB.Browser.for.SQLite-3.9.1-win64.exe"
+$downloadPath = "$env:TEMP\$packageName"
 
-if ($psISE) {
-    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+if ([System.IntPtr]::Size -ne 4) {
+    $url = $url64
 }
 
-Install-ChocolateyPackage $packageName $installerType $installerArgs $url $url64
+if (Test-Path $downloadPath) {
+    Remove-Item -Path $downloadPath -Recurse -Force
+}
+
+New-Item -Type Directory -Path $downloadPath | Out-Null
+
+Download-File $url "$downloadPath\$packageName.$installerType"
+
+Invoke-ElevatedCommand "$downloadPath\$packageName.$installerType" -ArgumentList $installerArgs -Wait
