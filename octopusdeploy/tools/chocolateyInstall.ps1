88fe5c7f359aa69a2616564b899da232302d8513
Julian Easterling <julian@julianscorner.com>
2016-11-14 16:28:59 -0500

    Updated OctopusDeploy to 4.0.4

--- octopusdeploy/tools/chocolateyInstall.ps1
+++ octopusdeploy/tools/chocolateyInstall.ps1
@@ -2,12 +2,7 @@ $packageName = "octopusdeploy"
 $downloadPath = "$env:TEMP\$packageName"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
 
-$url = "https://download.octopusdeploy.com/octopus-tools/3.4.2/OctopusTools.3.4.2.zip"
-
-$keep = @(
-  "octo.exe",
-  "octo.exe.config"
-)
+$url = "https://download.octopusdeploy.com/octopus-tools/4.0.4/OctopusTools.4.0.4.zip"
 
 if (Test-Path $downloadPath) {
     Remove-Item $downloadPath -Recurse -Force | Out-Null
@@ -27,10 +22,7 @@ if (Test-Path $appDir)
 
 New-Item -Type Directory -Path $appDir | Out-Null
 
-Get-ChildItem -Path $downloadPath -Include $keep -Recurse | Copy-Item -Destination "$appDir"
+Copy-Item -Path $downloadPath -Destination "$appDir"
 
-if (Test-Elevation) {
-    cmd /c mklink "$env:ChocolateyInstall\bin\octo.exe" "$appDir\octo.exe"
-} else {
-    Invoke-ElevatedCommand "cmd.exe" -ArgumentList "/c mklink '$env:ChocolateyInstall\bin\octo.exe' '$appDir\octo.exe'" -Wait
-}
+Invoke-ElevatedCommand "cmd.exe" `
+    -ArgumentList "/c mklink '$env:ChocolateyInstall\bin\octo.exe' '$appDir\octo.exe'" -Wait
