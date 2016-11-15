2ded7b1fca7005ebd8862ae90dced48c7cee286d
Julian Easterling <julian@julianscorner.com>
2016-11-15 13:45:34 -0500

    GreenShot is no longer using .Net 3.5

--- greenshot/tools/chocolateyInstall.ps1
+++ greenshot/tools/chocolateyInstall.ps1
@@ -3,15 +3,6 @@ $installerArgs = "/SILENT /NORESTART"
 $url = "https://github.com/greenshot/greenshot/releases/download/Greenshot-RELEASE-1.2.8.14/Greenshot-INSTALLER-1.2.8.14-RELEASE.exe"
 $downloadPath = "$env:TEMP\$packageName"
 
-if (-not (Test-Path "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5")) {
-    $dism = "$env:WinDir\system32\dism.exe"
-    $arguments = "/Online /NoRestart /Enable-Feature /FeatureName:NetFx3"
-
-    Write-Output "Installing .Net 3.5 Framework..."
-
-    Invoke-ElevatedCommand $dism -ArgumentList $arguments -Wait
-} 
-
 if (Test-Path $downloadPath) {
     Remove-Item -Path $downloadPath -Recurse -Force
 }
@@ -24,4 +15,4 @@ if (Get-Process $packageName -ErrorAction SilentlyContinue) {
     Stop-Process -ProcessName $packageName -Force -ErrorAction Stop
 }
 
-Invoke-ElevatedCommand "$downloadPath\$packageName.exe" -ArgumentList $installerArgs -Wait
+Invoke-ElevatedCommand "$downloadPath\$packageName.exe" -ArgumentList $installerArgs
