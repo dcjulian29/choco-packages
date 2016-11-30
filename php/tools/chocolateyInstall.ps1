4d0db3cd605a2cc18a1f03393d9e25d463c6632e
Julian Easterling <julian@julianscorner.com>
2016-11-30 14:17:01 -0500

    Updated PHP to 7.0.13

--- php/tools/chocolateyInstall.ps1
+++ php/tools/chocolateyInstall.ps1
@@ -1,17 +1,43 @@
 $packageName = "php"
-$version = "7.0.9"
-$url = "http://windows.php.net/downloads/releases/php-$version-nts-Win32-VC14-x86.zip"
-$url64 = "http://windows.php.net/downloads/releases/php-$version-nts-Win32-VC14-x64.zip" 
+$version = "7.0.13"
 $installArgs = "/install /passive /norestart"
+
+$url = "http://windows.php.net/downloads/releases/php-$version-nts-Win32-VC14-x86.zip"
+if ([System.IntPtr]::Size -ne 4) {
+    $url = "http://windows.php.net/downloads/releases/php-$version-nts-Win32-VC14-x64.zip" 
+}
+
+
 $vcredist = "https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x86.exe"
 $vcredist64 = "https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe"
 
-$downloadPath = "$env:TEMP\chocolatey\$packageName"
+$downloadPath = "$env:TEMP\$packageName"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
 
-if ($psISE) {
-    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+if (Test-Path $downloadPath) {
+    Remove-Item $downloadPath -Recurse -Force | Out-Null
 }
+
+New-Item -Type Directory -Path $downloadPath | Out-Null
+
+if ((Get-WmiObject Win32_Processor).AddressWidth -eq 64) {
+    if (-not (Test-Path "HKLM:SOFTWARE\WOW6432Node\Microsoft\DevDiv\vc\Servicing\14.0")) {
+        Download-File $url "$downloadPath\vcredist.exe"
+        Invoke-ElevatedCommand "$downloadPath\vcredist.exe" -ArgumentList $installArgs -Wait
+    }
+    
+    if (-not (Test-Path "HKLM:SOFTWARE\Microsoft\DevDiv\vc\Servicing\14.0")) {
+    Download-File $url "$downloadPath\vcredist64.exe"
+        Invoke-ElevatedCommand "$downloadPath\vcredist64.exe" -ArgumentList $installArgs -Wait
+    }
+} else {
+    if (-not (Test-Path "HKLM:SOFTWARE\Microsoft\DevDiv\vc\Servicing\14.0")) {
+        Download-File $url "$downloadPath\vcredist.exe"
+        Invoke-ElevatedCommand "$downloadPath\vcredist.exe" -ArgumentList $installArgs -Wait
+    }
+}
+
+Download-File $url "$downloadPath\$packageName.zip"
 
 if (Test-Path $appDir) {
   Remove-Item "$($appDir)" -Recurse -Force
@@ -19,26 +45,6 @@ if (Test-Path $appDir) {
 
 New-Item -Type Directory -Path $appDir | Out-Null
 
-if (-not (Test-Path $downloadPath)) {
-    New-Item -Type Directory -Path $downloadPath | Out-Null
-}
-
-
-if ((Get-WmiObject Win32_Processor).AddressWidth -eq 64) {
-    if (-not (Test-Path "HKLM:SOFTWARE\WOW6432Node\Microsoft\DevDiv\vc\Servicing\14.0")) {
-        Install-ChocolateyPackage "vcredist2015" "EXE" "/install /passive /norestart" $vcredist
-    }
-    
-    if (-not (Test-Path "HKLM:SOFTWARE\Microsoft\DevDiv\vc\Servicing\14.0")) {
-        Install-ChocolateyPackage "vcredist2015" "EXE" "/install /passive /norestart" "" $vcredist64
-    }
-} else {
-    if (-not (Test-Path "HKLM:SOFTWARE\Microsoft\DevDiv\vc\Servicing\14.0")) {
-        Install-ChocolateyPackage "vcredist2015" "EXE" "/install /passive /norestart" $vcredist
-    }
-}
-
-Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.zip" $url $url64
-Get-ChocolateyUnzip "$downloadPath\$packageName.zip" "$appDir\"
+Unzip-File "$downloadPath\$packageName.zip" "$appDir\"
 
 Copy-Item "$appDir\php.ini-production" "$appDir\php.ini"
--- php/tools/chocolateyUninstall.ps1
+++ php/tools/chocolateyUninstall.ps1
@@ -1,17 +1,6 @@
 $packageName = "php"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
 
-try
-{
-    if (Test-Path $appDir)
-    {
-      Remove-Item "$($appDir)" -Recurse -Force
-    }
-    
-    Write-ChocolateySuccess $packageName
-}
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+if (Test-Path $appDir) {
+    Remove-Item "$($appDir)" -Recurse -Force
 }
