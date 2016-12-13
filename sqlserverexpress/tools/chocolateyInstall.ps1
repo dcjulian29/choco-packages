d5cacc79aa5ca068a02c31ce4ce6c126e6e31c6a
Julian Easterling <julian@julianscorner.com>
2016-12-13 09:23:03 -0500

    Updated SQL Server to 2016 SP1 and changed the chocolatey script to download the install files from my server since Microsoft has decided to remove command line options to extract the install files using their new installer.

--- sqlserverexpress/tools/chocolateyInstall.ps1
+++ sqlserverexpress/tools/chocolateyInstall.ps1
@@ -1,50 +1,31 @@
 $packageName = "sqlserverexpress"
-$installerType = "EXE"
-
-$url64 = "https://download.microsoft.com/download/E/1/2/E12B3655-D817-49BA-B934-CEB9DAC0BAF3/SQLEXPRADV_x64_ENU.exe"
-
-$downloadPath = "$($env:TEMP)\chocolatey\$packageName"
+$url = "https://julianscorner.com/downloads/sql2016.1.zip"
+$downloadPath = "$($env:TEMP)\$packageName"
 $dataDir = "$($env:SYSTEMDRIVE)\data"
 
-if ($psISE) {
-    Import-Module -name "$($env:ChocolateyInstall)\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+$sid = New-Object Security.Principal.SecurityIdentifier 'S-1-5-32-544'
+$administrators = $sid.Translate([Security.Principal.NTAccount]).Value
+
+$arg1 = "/QS /ACTION=Install /FEATURES=SQL /TCPENABLED=1 /SKIPRULES=""RebootRequiredCheck"""
+$arg2 = "/INSTANCENAME=MSSQLSERVER /SQLSVCACCOUNT=""NT AUTHORITY\Network Service"""
+$arg3 = "/SQLSYSADMINACCOUNTS=""$($administrators)"" /INSTALLSQLDATADIR=""$($dataDir)"""
+$arg4 = "/AGTSVCACCOUNT=""NT AUTHORITY\Network Service"" /IACCEPTSQLSERVERLICENSETERMS"
+
+$installerArgs = "$arg1 $arg2 $arg3 $arg4"
+
+if (-not (Test-Path $dataDir)) {
+    New-Item -Type Directory -Path $dataDir | Out-Null
 }
 
-try {
-    $sid = New-Object Security.Principal.SecurityIdentifier 'S-1-5-32-544'
-    $administrators = $sid.Translate([Security.Principal.NTAccount]).Value
-
-    $arg1 = "/QS /ACTION=Install /FEATURES=SQL /TCPENABLED=1 /SKIPRULES=""RebootRequiredCheck"""
-    $arg2 = "/INSTANCENAME=MSSQLSERVER /SQLSVCACCOUNT=""NT AUTHORITY\Network Service"""
-    $arg3 = "/SQLSYSADMINACCOUNTS=""$($administrators)"" /INSTALLSQLDATADIR=""$($dataDir)"""
-    $arg4 = "/AGTSVCACCOUNT=""NT AUTHORITY\Network Service"" /IACCEPTSQLSERVERLICENSETERMS"
-
-    $installerArgs = "$arg1 $arg2 $arg3 $arg4"
-
-    if (-not (Test-Path $dataDir))
-    {
-        New-Item -Type Directory -Path $dataDir | Out-Null
-    }
-
-    if (-not (Test-Path $downloadPath))
-    {
-        New-Item -Type Directory -Path $downloadPath | Out-Null
-    }
-
-    Get-ChocolateyWebFile $packageName "$downloadPath\$packageName.exe" $null $url64
-    
-    Push-Location $downloadPath
-    Invoke-Expression "$downloadPath\$packageName.exe /extract:""$downloadPath"" /Q"
-    Pop-Location
-    
-    Wait-Process -Name $packageName -ErrorAction Silent
-
-    Invoke-Expression "$downloadPath\$packageName\setup.exe $installerArgs"
-
-    Wait-Process -Name "SETUP" -ErrorAction Silent
-
-    Write-ChocolateySuccess $packageName
-} catch {
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+if (Test-Path $downloadPath) {
+    Remove-Item -Path $downloadPath -Recurse -Force
 }
+
+New-Item -Type Directory -Path $downloadPath | Out-Null
+
+Download-File $url "$downloadPath\$packageName.zip"
+Unzip-File "$downloadPath\$packageName.zip" "$downloadPath\"
+
+Invoke-Expression "$downloadPath\setup.exe $installerArgs"
+
+Wait-Process -Name "SETUP" -ErrorAction Silent
