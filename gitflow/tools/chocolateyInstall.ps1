fa05e877faf610ed31917d211693539f41c003a2
Julian Easterling <julian@julianscorner.com>
2016-12-13 09:14:34 -0500

    Updated GitFlow to 1.10.1

--- gitflow/tools/chocolateyInstall.ps1
+++ gitflow/tools/chocolateyInstall.ps1
@@ -1,60 +1,77 @@
 $packageName = "gitflow"
-$release = "1.9.1"
-$gitflow = "https://github.com/petervanderdoes/gitflow-avh/archive/$release.zip"
+$gitflow = "https://github.com/petervanderdoes/gitflow-avh/archive/1.10.1.zip"
 $getoptbin = "https://julianscorner.com/downloads/util-linux-ng-2.14.1-bin.zip"
 $getoptdll = "https://julianscorner.com/downloads/util-linux-ng-2.14.1-dep.zip"
-$downloadPath = "$env:TEMP\chocolatey\$packageName"
+$downloadPath = "$env:TEMP\$packageName"
 
-if ($psISE) {
-    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+if (Test-Path $downloadPath) {
+    Remove-Item -Path $downloadPath -Recurse -Force
 }
 
-if (Test-Path "$env:ProgramFiles\Git") {
-    $git = "$env:ProgramFiles\Git"
+New-Item -Type Directory -Path $downloadPath | Out-Null
+
+Download-File $getoptbin "$downloadPath\bin.zip"
+Unzip-File "$downloadPath\bin.zip" "$downloadPath\bin\"
+
+Download-File $getoptdll "$downloadPath\dep.zip"
+Unzip-File "$downloadPath\dep.zip" "$downloadPath\dep\"
+
+Download-File $gitflow "$downloadPath\gitflow.zip"
+Unzip-File "$downloadPath\gitflow.zip" "$downloadPath\"
+
+Invoke-ElevatedScript {
+    if (Test-Path "$env:ProgramFiles\Git") {
+        $git = "$env:ProgramFiles\Git"
+    }
+
+    if (Test-Path "${env:ProgramFiles(x86)}\Git") {
+        $git = "${env:ProgramFiles(x86)}\Git"
+    }
+
+    Get-ChildItem -Path "$git" -Include 'git-flow*','gitflow-*','gitflow*' -Recurse `
+        | Remove-Item -Recurse -Force
+    Get-ChildItem -Path "$git" -Include 'getopt.exe','libintl3.dll','libiconv2.dll' -Recurse `
+        | Remove-Item -Recurse -Force
 }
 
-if (Test-Path "${env:ProgramFiles(x86)}\Git") {
-    $git = "${env:ProgramFiles(x86)}\Git"
+Push-Location $downloadPath
+
+Invoke-ElevatedScript {
+    if (Test-Path "$env:ProgramFiles\Git") {
+        $git = "$env:ProgramFiles\Git"
+    }
+
+    if (Test-Path "${env:ProgramFiles(x86)}\Git") {
+        $git = "${env:ProgramFiles(x86)}\Git"
+    }
+
+    if (Test-Path "$git\usr\bin") {
+        $git = "$git\usr\bin"
+    } else {
+        $git = "$git\bin"
+    }
+
+    Get-ChildItem -Path . -Include 'getopt.exe','libintl3.dll','libiconv2.dll' -Recurse `
+        | Copy-Item -Destination "$git" -Force
 }
 
-if (Test-ProcessAdminRights) {
-    Get-ChildItem -Path "$git" -Include 'git-flow*','gitflow-*','gitflow*' -Recurse | Remove-Item -Recurse -Force
-    Get-ChildItem -Path "$git" -Include 'getopt.exe','libintl3.dll','libiconv2.dll' -Recurse | Remove-Item -Recurse -Force
-} else {
-    Start-ChocolateyProcessAsAdmin "Get-ChildItem -Path '$git' -Include 'git-flow*','gitflow-*','gitflow*' -Recurse | Remove-Item -Recurse -Force"
-    Start-ChocolateyProcessAsAdmin "Get-ChildItem -Path '$git' -Include 'getopt.exe','libintl3.dll','libiconv2.dll' -Recurse | Remove-Item -Recurse -Force"
+Invoke-ElevatedScript {
+    if (Test-Path "$env:ProgramFiles\Git") {
+        $git = "$env:ProgramFiles\Git"
+    }
+
+    if (Test-Path "${env:ProgramFiles(x86)}\Git") {
+        $git = "${env:ProgramFiles(x86)}\Git"
+    }
+
+    if (Test-Path "$git\usr\bin") {
+        $git = "$git\usr\bin"
+    } else {
+        $git = "$git\bin"
+    }
+
+    Get-ChildItem -Path . -Include 'git-flow*','gitflow-*','gitflow*' -Recurse `
+        | Copy-Item -Destination "$git" -Force
 }
 
-if (!(Test-Path $downloadPath)) {
-    New-Item -ItemType directory $downloadPath -Force | Out-Null
-}
-
-# Download and copy getopt.exe & libintl3.dll & libiconv2.dll
-Get-ChocolateyWebFile $packageName "$downloadPath\bin.zip" $getoptbin
-Get-ChocolateyWebFile $packageName "$downloadPath\dep.zip" $getoptdll
-
-Get-ChocolateyUnzip "$downloadPath\bin.zip" "$downloadPath\"
-Get-ChocolateyUnzip "$downloadPath\dep.zip" "$downloadPath\"
-
-if (Test-Path "$git\usr\bin") {
-    $git = "$git\usr\bin"
-} else {
-    $git = "$git\bin"
-}
-
-if (Test-ProcessAdminRights) {
-    Get-ChildItem -Path $downloadPath -Include 'getopt.exe','libintl3.dll','libiconv2.dll' -Recurse | Copy-Item -Destination "$git" -Force
-} else {
-    Start-ChocolateyProcessAsAdmin "Get-ChildItem -Path $downloadPath -Include 'getopt.exe','libintl3.dll','libiconv2.dll' -Recurse | Copy-Item -Destination '$git' -Force"
-}
-
-Get-ChocolateyWebFile $packageName "$downloadPath\gitflow.zip" $gitflow
-Get-ChocolateyUnzip "$downloadPath\gitflow.zip" "$downloadPath\"
-
-$downloadPath = "$downloadPath\$packageName-avh-$release"
-
-if (Test-ProcessAdminRights) {
-    Get-ChildItem -Path $downloadPath -Include 'git-flow*','gitflow-*','gitflow*' -Recurse | Copy-Item -Destination "$git" -Force
-} else {
-    Start-ChocolateyProcessAsAdmin "Get-ChildItem -Path $downloadPath -Include 'git-flow*','gitflow-*','gitflow*' -Recurse | Copy-Item -Destination '$git' -Force"
-}
+Pop-Location
--- gitflow/tools/chocolateyUninstall.ps1
+++ gitflow/tools/chocolateyUninstall.ps1
@@ -1,21 +1,16 @@
 $packageName = "gitflow"
 
-if ($psISE) {
-    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
-}
+Invoke-ElevatedScript {
+    if (Test-Path "$env:ProgramFiles\Git") {
+        $git = "$env:ProgramFiles\Git"
+    }
 
-if (Test-Path "$env:ProgramFiles\Git") {
-    $git = "$env:ProgramFiles\Git"
-}
+    if (Test-Path "${env:ProgramFiles(x86)}\Git") {
+        $git = "${env:ProgramFiles(x86)}\Git"
+    }
 
-if (Test-Path "${env:ProgramFiles(x86)}\Git") {
-    $git = "${env:ProgramFiles(x86)}\Git"
-}
-
-if (Test-ProcessAdminRights) {
-    Get-ChildItem -Path "$git" -Include 'git-flow*','gitflow-*','gitflow*' -Recurse | Remove-Item -Recurse -Force
-    Get-ChildItem -Path "$git" -Include 'getopt.exe','libintl3.dll','libiconv2.dll' -Recurse | Remove-Item -Recurse -Force
-} else {
-    Start-ChocolateyProcessAsAdmin "Get-ChildItem -Path '$git' -Include 'git-flow*','gitflow-*','gitflow*' -Recurse | Remove-Item -Recurse -Force"
-    Start-ChocolateyProcessAsAdmin "Get-ChildItem -Path '$git' -Include 'getopt.exe','libintl3.dll','libiconv2.dll' -Recurse | Remove-Item -Recurse -Force"
+    Get-ChildItem -Path "$git" -Include 'git-flow*','gitflow-*','gitflow*' -Recurse `
+        | Remove-Item -Recurse -Force
+    Get-ChildItem -Path "$git" -Include 'getopt.exe','libintl3.dll','libiconv2.dll' -Recurse `
+        | Remove-Item -Recurse -Force
 }
