517b30388a6b107f642f6cabcc8779a270644b47
Julian Easterling <julian@julianscorner.com>
2016-12-19 23:53:05 -0500

    Updated Resharper to 2016.3.0

--- resharper/tools/chocolateyInstall.ps1
+++ resharper/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = "resharper"
 $installerType = "EXE"
 $installerArgs = "/VsVersion=14 /SpecificProductNames=ReSharper;dotCover;teamCityAddin;dotPeek /Silent=True"
-$url = "https://download.jetbrains.com/resharper/JetBrains.ReSharperUltimate.2016.2.2.exe"
+$url = "https://download.jetbrains.com/resharper/JetBrains.ReSharperUltimate.2016.3.exe"
 $downloadPath = "$env:TEMP\$packageName"
 
 if (Test-Path $downloadPath) {
