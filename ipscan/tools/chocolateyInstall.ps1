5794aafc4e9a5f4d79177af4a4c00021bb09a742
Julian Easterling <julian@julianscorner.com>
2016-12-20 19:00:04 -0500

    Updated ipscan to 3.5.0

--- ipscan/tools/chocolateyInstall.ps1
+++ ipscan/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "ipscan"
-$url = "https://github.com/angryziber/ipscan/releases/download/3.4.2/ipscan-win32-3.4.2.exe"
-$url64 = "https://github.com/angryziber/ipscan/releases/download/3.4.2/ipscan-win64-3.4.2.exe"
+$url = "https://github.com/angryziber/ipscan/releases/download/3.5/ipscan-win32-3.5.exe"
+$url64 = "https://github.com/angryziber/ipscan/releases/download/3.5/ipscan-win64-3.5.exe"
 $downloadPath = "$env:TEMP\$packageName"
 $appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
 
