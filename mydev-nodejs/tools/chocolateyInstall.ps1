6a454d25c498701b05899f8d7b6a64fa65bf69c8
Julian Easterling <julian@julianscorner.com>
2017-03-01 12:58:44 -0500

    Added a hubot generator to be installed.

--- mydev-nodejs/tools/chocolateyInstall.ps1
+++ mydev-nodejs/tools/chocolateyInstall.ps1
@@ -1,9 +1,5 @@
 $packageName = "mydev-nodejs"
 
-if ($psISE) {
-    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
-}
-
 $npm = "$($env:ProgramFiles)\nodejs\npm.cmd"
     
 & $npm install -g bower
@@ -11,8 +7,8 @@ $npm = "$($env:ProgramFiles)\nodejs\npm.cmd"
 & $npm install -g yo 
 & $npm install -g generator-aspnet generator-express
 & $npm install -g generator-hottowel generator-meanjs generator-sails-rest-api
+& $npm install -g generator-hubot
 & $npm install -g csslint
 & $npm install -g jslint jshint
 & $npm install -g typescript
 & $npm install -g pm2
-
