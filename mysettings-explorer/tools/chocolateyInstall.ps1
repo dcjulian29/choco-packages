e19f40a3c9e2589240a8f2e481d2317ce4402344
Julian Easterling <julian@julianscorner.com>
2017-03-01 13:03:18 -0500

    Removed the Chocolatey cmdlets from scripts and the post install script since it isn't used anymore...

--- mysettings-explorer/tools/chocolateyInstall.ps1
+++ mysettings-explorer/tools/chocolateyInstall.ps1
@@ -1,32 +1,12 @@
 $packageName = "mysettings-explorer"
 $toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
 
-if ($psISE) {
-    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+$cmd = "$env:WINDIR\system32\reg.exe import $toolDir\registry.reg"
+
+if (Get-ProcessorBits -eq 64) {
+    $cmd = "$cmd /reg:64"
 }
 
-try
-{
-    $cmd = "$env:WINDIR\system32\reg.exe import $toolDir\registry.reg"
-    
-    if (Get-ProcessorBits -eq 64) {
-        $cmd = "$cmd /reg:64"
-    }
-    
-    cmd /c "$cmd"
-    
-#    if (Test-ProcessAdminRights) {
-#        . $toolDir\postInstall.ps1
-#    } else {
-#        Start-ChocolateyProcessAsAdmin ". $toolDir\postInstall.ps1"
-#    }
+cmd /c "$cmd"
 
-    Write-Output "You need to reboot to complete the action..."
-
-    Write-ChocolateySuccess $packageName
-}
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
-}
+Write-Output "You need to reboot to complete the action..."
