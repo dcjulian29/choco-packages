ec6bf3b2432eedd9907554b48a038c119d6983d4
Julian Easterling <julian@julianscorner.com>
2017-03-19 11:42:14 -0400

    Removed sysinternals package

--- sysinternals/tools/chocolateyUninstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$packageName = 'sysinternals'
-$appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
-$toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
-
-if (Test-Path $appDir) {
-    Remove-Item "$($appDir)" -Recurse -Force
-}
-
-Invoke-ElevatedExpression -Command ". $toolDir\postUninstall.ps1"
