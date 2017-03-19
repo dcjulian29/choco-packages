21e3ee8ede4d55fed21493d1e816fa4f0d0c0889
Julian Easterling <julian@julianscorner.com>
2017-03-19 05:14:23 -0400

    Updated package names in the mydev-tools package to match the community feed names where applicable...

--- mydev-tools/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-$packageName = "devvm-tools"
-
-if ($psISE) {
-    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
-}
-
-Write-Output "Installed package: $packageName"
