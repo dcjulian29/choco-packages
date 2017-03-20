9eeb95438edde24dfd59b7a01b77a4c3db61bb1d
Julian Easterling <julian@julianscorner.com>
2017-03-19 22:58:16 -0400

    Updated XMLQuire tool to use tools directory instead of the app directory

--- xmlquire/tools/chocolateyUninstall.ps1
+++ xmlquire/tools/chocolateyUninstall.ps1
@@ -1,17 +1,6 @@
 $packageName = "xmlquire"
-$appDir = "$($env:SYSTEMDRIVE)\tools\apps\$($packageName)"
+$appDir = "$($env:SYSTEMDRIVE)\tools\$($packageName)"
 
-try
-{
-    if (Test-Path $appDir)
-    {
-      Remove-Item "$($appDir)" -Recurse -Force
-    }
-
-    Write-ChocolateySuccess $packageName
-}
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
+if (Test-Path $appDir) {
+    Remove-Item "$($appDir)" -Recurse -Force
 }
