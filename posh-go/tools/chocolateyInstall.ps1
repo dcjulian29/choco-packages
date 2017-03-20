25b940354ca622fb0259c3927bd9ffad5351476f
Julian Easterling <julian@julianscorner.com>
2017-03-20 16:20:03 -0400

    Removed the explicit need for Chocolatey in the Posh-GO module.

--- posh-go/tools/chocolateyInstall.ps1
+++ posh-go/tools/chocolateyInstall.ps1
@@ -1,36 +1,24 @@
 $packageName = "posh-go"
 $url = "https://github.com/cameronharp/Go-Shell/archive/master.zip"
 $appDir = "$($env:UserProfile)\Documents\WindowsPowerShell\Modules\go"
-$downloadPath = "$env:TEMP\chocolatey\$packageName"
+$downloadPath = "$env:LOCALAPPDATA\Temp\$packageName"
 
-if ($psISE) {
-    Import-Module -name "$env:ChocolateyInstall\chocolateyinstall\helpers\chocolateyInstaller.psm1"
+if (Test-Path $downloadPath) {
+    Remove-Item $downloadPath -Recurse -Force | Out-Null
 }
 
-try
-{
-    if (Test-Path $appDir)
-    {
-      Write-Output "Removing previous version of package..."
-      Remove-Item "$($appDir)" -Recurse -Force
-    }
+New-Item -Type Directory -Path $downloadPath | Out-Null
 
-    if (-not (Test-Path $downloadPath))
-    {
-        New-Item -Type Directory -Path $downloadPath | Out-Null
-    }
+Download-File $url "$downloadPath\$packageName.zip"
 
-    Get-ChocolateyWebFile $packageName "$downloadPath\master.zip" $url
-    Get-ChocolateyUnzip "$downloadPath\master.zip" "$downloadPath\"
+Unzip-File "$downloadPath\$packageName.zip" "$downloadPath\"
 
-    New-Item -Type Directory -Path $appDir | Out-Null
-
-    Copy-Item -Path "$downloadPath\Go-Shell-master\*" -Destination "$appDir"
-
-    Write-ChocolateySuccess $packageName
+if (Test-Path $appDir) {
+  Write-Output "Removing previous version of package..."
+  Remove-Item "$($appDir)" -Recurse -Force
 }
-catch
-{
-    Write-ChocolateyFailure $packageName $($_.Exception.Message)
-    throw
-}
+
+New-Item -Type Directory -Path $appDir | Out-Null
+
+Copy-Item -Path "$downloadPath\Go-Shell-master\*" -Destination "$appDir"
+
