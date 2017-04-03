2f83cfc278e24849d374209f5f822ca1481088f6
Julian Easterling <julian@julianscorner.com>
2017-04-03 16:33:25 -0400

    I don't like the way (and it doesn't consistently install) Resharper packages install, so I'm going to keep my current package and manually update it myself...

---
+++ resharper/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = "resharper"
+$installerType = "EXE"
+$installerArgs = "/SpecificProductNames=ReSharper;dotCover;teamCityAddin;dotPeek /Silent=True"
+$url = "https://download.jetbrains.com/resharper/JetBrains.ReSharperUltimate.2016.3.2.exe"
+$downloadPath = "$env:TEMP\$packageName"
+
+if (Test-Path $downloadPath) {
+    Remove-Item -Path $downloadPath -Recurse -Force
+}
+
+New-Item -Type Directory -Path $downloadPath | Out-Null
+
+Download-File $url "$downloadPath\$packageName.$installerType"
+
+Invoke-ElevatedCommand "$downloadPath\$packageName.$installerType" -ArgumentList $installerArgs -Wait
