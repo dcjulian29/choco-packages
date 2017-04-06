1bc88ef9ed82aaf3ff0ab1ea093ca4874cd90b1b
Julian Easterling <julian@julianscorner.com>
2017-04-05 22:50:52 -0400

    Remove AEScrypt package

--- aescrypt/tools/postUninstall.ps1
+++
@@ -1,9 +0,0 @@
-$links = @(
-    "aescrypt.exe"
-)
-
-foreach ($link in $links) {
-    if (Test-Path "${env:ChocolateyInstall}\bin\$link") {
-        (Get-Item "${env:ChocolateyInstall}\bin\$link").Delete()
-    }
-}
