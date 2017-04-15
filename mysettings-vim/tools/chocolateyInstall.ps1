f813c5baa159fd9a90d17fcbfa9ad6035fb29064
Julian Easterling <julian@julianscorner.com>
2017-04-15 09:25:50 -0400

    Updated my git settings to include my VIM settings.

---
+++ mysettings-vim/tools/chocolateyInstall.ps1
@@ -0,0 +1,47 @@
+$packageName = "mysettings-vim"
+
+if (Test-Path "${env:USERPROFILE}\_vimrc") {
+    Remove-Item "${env:USERPROFILE}\_vimrc" -Force
+}
+
+Set-Content -Path "${env:USERPROFILE}\_vimrc" -Value @"
+set tabstop=4       " number of visual spaces per TAB
+set softtabstop=4   " number of spaces in tab when editing
+set expandtab       " tabs are spaces
+set number      " show line numbers
+set showcmd     " show command in bottom bar
+set wildmenu        " visual autocomplete for command menu
+set lazyredraw      " redraw only when we need to.
+set showmatch       " highlight matching [{()}]
+set hlsearch        " highlight matches
+set foldenable          " enable folding
+set foldlevelstart=10   " open most folds by default
+set foldnestmax=10      " 10 nested fold max
+set foldmethod=indent   " fold based on indent level
+set ignorecase      " case-insensitive search
+set ruler       " display cursor position in bottom bar
+set backspace=indent,eol,start  " make backspace work as expected on Windows.
+syntax enable
+color slate
+if has("gui_running")
+    set lines=40 columns=120
+    set guifont=Consolas\ 16
+endif
+autocmd Filetype gitcommit setlocal spell textwidth=72
+hi def link gitcommitSummary Normal
+hi def link gitcommitBlank Normal 
+"@
+
+if (Test-Path "$env:ProgramFiles\Git") {
+    $git = "$env:ProgramFiles\Git\bin\git.exe"
+    if (Test-Path $git) {
+        & $git config --global core.editor "'C:\Program Files\vim\vim80\vim.exe'"
+    }
+}
+
+if (Test-Path "${env:ProgramFiles(x86)}\Git") {
+    $git = "${env:ProgramFiles(x86)}\Git\bin\git.exe"
+    if (Test-Path $git) {
+        & $git config --global core.editor "'C:\Program Files (x86)\vim\vim80\vim.exe'"
+    }
+}
