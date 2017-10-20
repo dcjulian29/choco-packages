1019c334f544c00e0084850189d0e9d1d5806d03
Julian Easterling <julian@julianscorner.com>
2017-10-20 08:26:46 -0400

    Updated SmartGit to 17.1.0

--- smartgit/tools/chocolateyInstall.ps1
+++ smartgit/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = "smartgit"
 $downloadPath = "$env:LOCALAPPDATA\Temp\$packageName"
 $installerArgs = '/sp- /silent /norestart'
-$version = '17_0_4'
+$version = '17_1_0'
 
 $url = `
     'https://www.syntevo.com/static/smart/download/smartgit/smartgit-win32-setup-jre-{0}.zip' `
