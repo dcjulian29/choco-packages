8111ce23cadae38b91077c3c428c369bf150728a
Julian Easterling <julian@julianscorner.com>
2018-03-06 13:34:33 -0500

    Removed minikube as I'll probablly only install that on my laptop... Also added some checks for the features that get installed during the course of the script.

--- mycomputers-workstation-personal/tools/chocolateyInstall.ps1
+++ mycomputers-workstation-personal/tools/chocolateyInstall.ps1
@@ -1,7 +1,14 @@
 $packageName = "mycomputers-workstation-personal"
 
-Enable-PSRemoting -Force
+if (-not ([bool](Invoke-Command -ComputerName $env:COMPUTERNAME `
+    -ScriptBlock {"IPConfig"} -ErrorAction SilentlyContinue))) {
+    Enable-PSRemoting -Force
+}
 
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart
+if (-not ([bool](Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V))) {
+    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart
+}
 
-Enable-WindowsOptionalFeature -Online -FeatureName containers -All
+if (-not ([bool](Get-WindowsOptionalFeature -Online -FeatureName Containers))) {
+    Enable-WindowsOptionalFeature -Online -FeatureName Containers -All
+}
