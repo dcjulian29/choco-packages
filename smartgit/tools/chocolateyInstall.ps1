97e9b2923f82cba8e79248157a393c651208c428
Julian Easterling <julian@julianscorner.com>
2018-04-14 16:48:25 -0400

    Updated SmartGit to 18.1.0

--- smartgit/tools/chocolateyInstall.ps1
+++ smartgit/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 $packageName = "smartgit"
 $downloadPath = "$env:LOCALAPPDATA\Temp\$packageName"
 $installerArgs = '/sp- /silent /norestart'
-$version = '17_1_6'
+$version = '18_1_0'
 
 $url = `
-    'https://www.syntevo.com/static/smart/download/smartgit/smartgit-win32-setup-jre-{0}.zip' `
+    'https://www.syntevo.com/downloads/smartgit/smartgit-win-{0}.zip' `
     -f $version
 
 if (Test-Path $downloadPath) {
@@ -16,7 +16,7 @@ New-Item -Type Directory -Path $downloadPath | Out-Null
 Download-File $url "$downloadPath\$packageName.zip"
 Unzip-File "$downloadPath\$packageName.zip" "$downloadPath\"
 
-$installFileLocation = [IO.Path]::Combine($downloadPath, "setup-{0}-jre.exe" -f $version)
+$installFileLocation = [IO.Path]::Combine($downloadPath, "smartgit-{0}-setup.exe" -f $version)
 
 Invoke-ElevatedCommand $installFileLocation -ArgumentList $installerArgs -Wait
 
