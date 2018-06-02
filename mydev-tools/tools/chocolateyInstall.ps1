d26eeb5385ffc9eab1cdc61cab0ae4de7de84725
Julian Easterling <julian@julianscorner.com>
2018-06-02 06:27:02 -0400

    Added SearchMyFiles manually to my DevVM package since the community package is not being updated.

--- mydev-tools/tools/chocolateyInstall.ps1
+++ mydev-tools/tools/chocolateyInstall.ps1
@@ -32,3 +32,15 @@ gem install --local "$downloadPath\rubygems.gem"
 update_rubygems
 
 gem install jekyll
+
+Write-Output " Installing SearchMyFiles manually since the community package hasn't been updated..."
+
+$url = 'http://www.nirsoft.net/utils/searchmyfiles-x64.zip'
+$url64 = 'http://www.nirsoft.net/utils/searchmyfiles-x64.zip'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "searchmyfiles.exe"
+
+Install-ChocolateyZipPackage -PackageName "searchmyfiles" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" 
