840d757b0080d75d17f15ba7aeed9df67883540c
Julian Easterling <julian@julianscorner.com>
2018-07-25 22:43:01 -0400

    The way that I was detecting Hyper-V wasn't working so I change the method.

--- mycomputers-workstation-personal/tools/chocolateyInstall.ps1
+++ mycomputers-workstation-personal/tools/chocolateyInstall.ps1
@@ -1,14 +1,20 @@
 $packageName = "mycomputers-workstation-personal"
 
-if (-not ([bool](Invoke-Command -ComputerName $env:COMPUTERNAME `
-    -ScriptBlock {"IPConfig"} -ErrorAction SilentlyContinue))) {
+$psremote = ([bool](Invoke-Command -ComputerName $env:COMPUTERNAME `
+    -ScriptBlock {IPConfig} -ErrorAction SilentlyContinue))
+    
+$hyperv = (Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V).State -eq "Enabled"
+
+$containers = (Get-WindowsOptionalFeature -Online -FeatureName Containers).State -eq "Enabled"
+    
+    if (-not $psremote) {
     Enable-PSRemoting -Force
 }
 
-if (-not ([bool](Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V))) {
+if (-not $hyperv) {
     Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart
 }
 
-if (-not ([bool](Get-WindowsOptionalFeature -Online -FeatureName Containers))) {
+if (-not $containers) {
     Enable-WindowsOptionalFeature -Online -FeatureName Containers -All
 }
