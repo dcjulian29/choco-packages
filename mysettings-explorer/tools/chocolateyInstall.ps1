c8cc9938ecf96b25400c546d1b2a7809859463b5
Julian Easterling <julian@julianscorner.com>
2018-08-14 09:42:22 -0400

    Power settings need to be set via powercfg.exe instead of registry.

--- mysettings-explorer/tools/chocolateyInstall.ps1
+++ mysettings-explorer/tools/chocolateyInstall.ps1
@@ -9,4 +9,8 @@ if (Get-ProcessorBits -eq 64) {
 
 cmd /c "$cmd"
 
+powercfg.exe /change monitor-timeout-ac 0
+powercfg.exe /change standby-timeout-ac 0
+powercfg.exe /change hibernate-timeout-ac 0
+
 Write-Output "You need to reboot to complete the action..."
