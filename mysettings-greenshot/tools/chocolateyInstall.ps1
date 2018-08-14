9507eddbb8c4ca41299910d1bacc1e6bc8fe5bc7
Julian Easterling <julian@julianscorner.com>
2018-08-14 09:46:05 -0400

    Instead of just installing Greenshot and configuring manually, created a package to do it for me.

---
+++ mysettings-greenshot/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+$packageName = "mysettings-greenshot"
+
+if (Get-Process -Name Greenshot -ErrorAction SilentlyContinue) {
+    Stop-Process -Name Greenshot -Force
+}
+
+$ini = Get-Content -Path "${env:APPDATA}\Greenshot\Greenshot.ini"
+
+$ini = $ini.Replace("OutputFileFilenamePattern=`${capturetime:d""yyyy-MM-dd HH_mm_ss""}-`${title}", `
+    "OutputFileFilenamePattern=`${capturetime:d""yyyy-MM-dd HH_mm_ss""}")
+
+$ini = $ini.Replace("OutputFilePath=${env:USERPROFILE}\Desktop", "OutputFilePath=${env:USERPROFILE}\Pictures")
+
+Set-Content -Path "${env:APPDATA}\Greenshot\Greenshot.ini" -Value $ini
+
+$key = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
+$exists = (Get-ItemProperty $key "Greenshot") -ne $null
+ 
+if ($exists) {
+    Remove-ItemProperty -Path $key -Name Greenshot
+} 
