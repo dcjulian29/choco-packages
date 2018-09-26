8eb4f63276d5c07298b8c6f8d17179de8174050c
Julian Easterling <julian@julianscorner.com>
2018-09-26 18:57:23 -0400

    Removed non-SQL Server related packages.

--- mydev-database/tools/chocolateyInstall.ps1
+++ mydev-database/tools/chocolateyInstall.ps1
@@ -1,10 +1,6 @@
 $packageName = "mydev-database"
 
-Invoke-ElevatedCommand "sc.exe" -ArgumentList "config MongoDb start= demand" -Wait
 Invoke-ElevatedCommand "sc.exe" -ArgumentList "config MSSQLSERVER start= demand" -Wait
-
-$service = Get-Service | Where-Object { $_.Name -like "postgresql*" }
-Invoke-ElevatedCommand "sc.exe" -ArgumentList "config $($service.Name) start= demand" -Wait
 
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 
