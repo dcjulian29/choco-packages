1cdf4f83e1bbbaa68950aaeb372a861087995e1b
Julian Easterling <julian@julianscorner.com>
2018-10-15 21:00:03 -0400

    Updated some of the visual studio code extensions I'm using.

--- mysettings-vscode/tools/chocolateyInstall.ps1
+++ mysettings-vscode/tools/chocolateyInstall.ps1
@@ -24,10 +24,25 @@ Start-Process -FilePath $code -ArgumentList "--install-extension EditorConfig.Ed
 Start-Process -FilePath $code -ArgumentList "--install-extension eg2.tslint" -NoNewWindow -Wait
 Start-Process -FilePath $code -ArgumentList "--install-extension haaaad.ansible" -NoNewWindow -Wait
 Start-Process -FilePath $code -ArgumentList "--install-extension kisstkondoros.vscode-codemetrics" -NoNewWindow -Wait
-Start-Process -FilePath $code -ArgumentList "--install-extension ms-vscode.PowerShell" -NoNewWindow -Wait
+Start-Process -FilePath $code -ArgumentList "--install-extension powershell.vscode-powershell" -NoNewWindow -Wait
 Start-Process -FilePath $code -ArgumentList "--install-extension thomas-baumgaertner.vcl" -NoNewWindow -Wait
 Start-Process -FilePath $code -ArgumentList "--install-extension eamodio.gitlens" -NoNewWindow -Wait
 Start-Process -FilePath $code -ArgumentList "--install-extension ms-kubernetes-tools.vscode-kubernetes-tools" -NoNewWindow -Wait
 Start-Process -FilePath $code -ArgumentList "--install-extension mkaufman.htmlhint" -NoNewWindow -Wait
 Start-Process -FilePath $code -ArgumentList "--install-extension jebbs.plantuml" -NoNewWindow -Wait
 Start-Process -FilePath $code -ArgumentList "--install-extension redhat.vscode-yaml" -NoNewWindow -Wait
+Start-Process -FilePath $code -ArgumentList "--install-extension DavidAnson.vscode-markdownlint" -NoNewWindow -Wait
+Start-Process -FilePath $code -ArgumentList "--install-extension ms-vscode.go" -NoNewWindow -Wait
+Start-Process -FilePath $code -ArgumentList "--install-extension sonarsource.sonarlint-vscode" -NoNewWindow -Wait
+Start-Process -FilePath $code -ArgumentList "--install-extension idleberg.vscode-nsis" -NoNewWindow -Wait
+Start-Process -FilePath $code -ArgumentList "--install-extension marcostazi.vs-code-vagrantfile" -NoNewWindow -Wait
+Start-Process -FilePath $code -ArgumentList "--install-extension dbaeumer.vscode-eslint" -NoNewWindow -Wait
+Start-Process -FilePath $code -ArgumentList "--install-extension shardulm94.trailing-spaces" -NoNewWindow -Wait
+Start-Process -FilePath $code -ArgumentList "--install-extension mauve.terraform" -NoNewWindow -Wait
+Start-Process -FilePath $code -ArgumentList "--install-extension mkaufman.htmlhint" -NoNewWindow -Wait
+Start-Process -FilePath $code -ArgumentList "--install-extension slevesque.vscode-autohotkey" -NoNewWindow -Wait
+Start-Process -FilePath $code -ArgumentList "--install-extension robertohuertasm.vscode-icons" -NoNewWindow -Wait
+Start-Process -FilePath $code -ArgumentList "--install-extension dbaeumer.jshint" -NoNewWindow -Wait
+Start-Process -FilePath $code -ArgumentList "--install-extension shinnn.stylelint" -NoNewWindow -Wait
+
+
