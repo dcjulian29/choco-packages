84869ec2b03b70d403bc939649d00a457d4f11ed
Julian Easterling <julian@julianscorner.com>
2018-10-24 20:40:02 -0400

    Updated some syntax errors

--- mydev-scm/tools/chocolateyInstall.ps1
+++ mydev-scm/tools/chocolateyInstall.ps1
@@ -1,12 +1,10 @@
-$packageName = "mydev-scm"
-
 if (Test-Path $env:SYSTEMDRIVE\etc\Sourcetree\accounts.json) {
-    if (-not Test-Path $env:LOCALAPPDATA\Atlassian\SourceTree) {
+    if (-not (Test-Path $env:LOCALAPPDATA\Atlassian\SourceTree)) {
         New-Item $env:LOCALAPPDATA\Atlassian -ItemType Directory | Out-Null
         New-Item $env:LOCALAPPDATA\Atlassian\SourceTree -ItemType Directory | Out-Null
     }
-    
-    if (-not Test-Path $env:LOCALAPPDATA\Atlassian\SourceTree\accounts.json) {
+
+    if (-not (Test-Path "$env:LOCALAPPDATA\Atlassian\SourceTree\accounts.json")) {
         Copy-Item $env:SYSTEMDRIVE\etc\Sourcetree\accounts.json `
             $env:LOCALAPPDATA\Atlassian\SourceTree\accounts.json | Out-Null
 
