fff882c40aa9a604fb500920484a36b109389ccd
Julian Easterling <julian@julianscorner.com>
2018-10-26 14:48:12 -0400

    Moved a bunch of packages from main workstation package to personal workstation package.

--- mycomputers-workstation-personal/tools/chocolateyInstall.ps1
+++ mycomputers-workstation-personal/tools/chocolateyInstall.ps1
@@ -1,12 +1,10 @@
-$packageName = "mycomputers-workstation-personal"
-
 $psremote = ([bool](Invoke-Command -ComputerName $env:COMPUTERNAME `
     -ScriptBlock {IPConfig} -ErrorAction SilentlyContinue))
-    
+
 $hyperv = (Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V).State -eq "Enabled"
 
 $containers = (Get-WindowsOptionalFeature -Online -FeatureName Containers).State -eq "Enabled"
-    
+
 if (-not $psremote) {
     Enable-PSRemoting -Force
 }
@@ -18,3 +16,11 @@ if (-not $hyperv) {
 if (-not $containers) {
     Enable-WindowsOptionalFeature -Online -FeatureName Containers -All
 }
+
+$url = "http://www.nirsoft.net/utils/searchmyfiles.zip"
+$url64 = "http://www.nirsoft.net/utils/searchmyfiles-x64.zip"
+$installFile = Join-Path $PSScriptRoot "searchmyfiles.exe"
+
+Install-ChocolateyZipPackage -PackageName "searchmyfiles" -Url $url -url64 $url64 -UnzipLocation $PSScriptRoot
+
+Set-Content -Path "$installFile.gui" -Value $null
