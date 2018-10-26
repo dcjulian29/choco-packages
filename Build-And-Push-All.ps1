388e280a4217fbe790f35106b26b72a58092ca0d
Julian Easterling <julian@julianscorner.com>
2018-10-26 14:45:56 -0400

    fixed a style issue

--- Build-And-Push-All.ps1
+++ Build-And-Push-All.ps1
@@ -4,8 +4,8 @@ Push-Location choco-packages
 
 Load-NuGetProfile choco
 
-Get-ChildItem -Directory | foreach { 
-    Push-Location $_.Name 
+Get-ChildItem -Directory | ForEach-Object {
+    Push-Location $_.Name
     Remove-Item *.nupkg
     Make-ChocolateyPackage
     nuget-publish *.nupkg
