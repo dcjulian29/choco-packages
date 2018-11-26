91d88bcf1def08f6dd0f46b944ed903e77154660
Julian Easterling <julian@julianscorner.com>
2018-11-26 07:37:44 -0500

    Sorted Registry settings and updated monitor turn off timing.

--- mysettings-explorer/tools/chocolateyInstall.ps1
+++ mysettings-explorer/tools/chocolateyInstall.ps1
@@ -9,8 +9,12 @@ if (Get-ProcessorBits -eq 64) {
 
 cmd /c "$cmd"
 
-powercfg.exe /change monitor-timeout-ac 0
+powercfg.exe /change monitor-timeout-ac 10
 powercfg.exe /change standby-timeout-ac 0
 powercfg.exe /change hibernate-timeout-ac 0
+
+powercfg.exe /change monitor-timeout-dc 5
+powercfg.exe /change standby-timeout-dc 10
+powercfg.exe /change hibernate-timeout-dc 30
 
 Write-Output "You need to reboot to complete the action..."
