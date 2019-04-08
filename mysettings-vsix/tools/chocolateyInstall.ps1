3092d33a20537c97f50832bac73f56510fc7a291
Julian Easterling <julian@julianscorner.com>
2019-04-07 21:48:28 -0400

    Updated VSIX to 2019 VSIX packages

--- mysettings-vsix/tools/chocolateyInstall.ps1
+++ mysettings-vsix/tools/chocolateyInstall.ps1
@@ -1,5 +1,7 @@
-$packageName = "mysettings-vsix"
-$downloadPath = "$env:TEMP\$packageName"
+$downloadPath = "$env:TEMP\vsix"
+$errorInstalling = $false
+$vsix = Find-VSIX
+
 
 if (Test-Path $downloadPath) {
     Remove-Item -Path $downloadPath -Recurse -Force
@@ -7,95 +9,113 @@ if (Test-Path $downloadPath) {
 
 New-Item -Type Directory -Path $downloadPath | Out-Null
 
-$vsix = "C:\Program Files"
-
-if (Test-Path "C:\Program Files (x86)") {
-    $vsix = "C:\Program Files (x86)"
-}
-
-$vsix = "$vsix\Microsoft Visual Studio\2017\Professional\Common7\IDE\VSIXInstaller.exe"
-
 Function Install-VSIX {
     param (
         [string]$Name,
         [string]$Url
     )
 
+    $originalErrorAction = $ErrorActionPreference
+
+    $ErrorActionPreference = 'Stop'
+
+    $invalidChars = [IO.Path]::GetInvalidFileNameChars() -join ''
+    $re = "[{0} ]" -f [RegEx]::Escape($invalidChars)
+
+    $baseName = ($Name -replace $re)
+
+    $logFile = "$downloadPath\{0}_{1:yyyyMMddHHmmss}.log" -f $baseName, (Get-Date)
+    $vsixFile = "$downloadPath\$baseName.vsix"
+
     " "
+    "=========================================================="
     " "
     "Installing $Name Extension from"
     "   $Url"
     " "
-    
-    $invalidChars = [IO.Path]::GetInvalidFileNameChars() -join ''
-    $re = "[{0}]" -f [RegEx]::Escape($invalidChars)
-    $Name = ($Name -replace $re)
+    "    VSIX File: $vsixFile"
+    "    Log File: $logFile"
+    " "
 
-    if (Test-Path "$downloadPath\$Name.vsix") {
-        Remove-Item "$downloadPath\$Name.vsix" -Force
-    }
-
-    Download-File "$Url" "$downloadPath\$Name.vsix"
-
-    $process = New-Object System.Diagnostics.ProcessStartInfo
-    $process.FileName = $vsix
-    $process.Arguments = "/quiet /logFile:""$downloadPath\$Name.log"" ""$downloadPath\$Name.vsix"""
-
-    $run = [System.Diagnostics.Process]::Start($process)
-    $run.WaitForExit()
-
-    $exitCode = $run.ExitCode
-
-    if ($exitCode -eq 1001) {
-        Write-Information "The $Name Extension is already installed."
-    } else {
-        if ($exitCode -gt 0) {
-            Write-Warning "An error occurred Installing $Name Extension..."
-            Write-Warning "Review the log file: $logFile"
-            throw
+    try {
+        if (Test-Path $vsixFile) {
+            Remove-Item $vsixFile -Force
         }
+
+        Download-File $Url $vsixFile
+
+        $arguments = @(
+            "/quiet"
+            "/logFile:$logFile"
+            "$vsixFile"
+        )
+
+        $run = Start-Process -FilePath $vsix -ArgumentList $arguments -PassThru -Wait -NoNewWindow -Verbose
+
+        $exitCode = [Int32]$run.ExitCode
+
+        if ($exitCode -eq 1001) {
+            Write-Host "INFORMATION: The $Name Extension is already installed." -ForegroundColor Magenta
+        } else {
+            if ($exitCode -gt 0) {
+                " "
+                Write-Host "An error occurred installing $Name Extension..." -ForegroundColor Magenta
+                Write-Host "Exit Code: *$exitCode*" -ForegroundColor Magenta
+                Write-Host "Review the log file: $logFile" -ForegroundColor Magenta
+                $errorInstalling = $true
+            }
+        }
+    } catch {
+        $errorMessage = $_.Exception.Message
+        Write-Host " "
+        Write-Host "An error occurred during installation of the $Name Extension..." -ForegroundColor Magenta
+        Write-Host "Error: $errorMessage" -ForegroundColor Magenta
+        Write-Host "Review the log file: $logFile" -ForegroundColor Magenta
+        $errorInstalling = $true
     }
+
+    $ErrorActionPreference = $originalErrorAction
 }
 
-#### Web Essentials 2017
+#### Core Web Essentials
 Install-VSIX "Bundler & Minifier" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/bundlerminifier/2.8.396/1535134367605/Bundler___Minifier_v2.8.396.vsix"
-Install-VSIX "CSS Tools" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/csstools/1.1.23/1534434064085/CSS_Tools_v1.1.23.vsix"
+Install-VSIX "CSS Tools" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/csstools2019/1.1.2/1553187100196/CSS_Tools_2019_v1.1.2.vsix"
 Install-VSIX "Editor Enhancements" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/editorenhancements/1.0.30/1535134387263/Editor_Enhancements_v1.0.30.vsix"
-Install-VSIX "File Icons" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/fileicons/2.7.222/1536178942711/File_Icons_v2.7.222.vsix"
-Install-VSIX "HTML Tools" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/htmltools/1.0.3/1482143974862/236842/2/HTML%20Tools%20v1.0.3.vsix"
-Install-VSIX "Project File Tools" "https://ms-madsk.gallerycdn.vsassets.io/extensions/ms-madsk/projectfiletools/1.3.1/1537564617139/ProjectFileTools.vsix"
-Install-VSIX "Syntax Highlighting Pack" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/syntaxhighlightingpack/2.8.134/1536785834762/Syntax_Highlighting_Pack_v2.8.134.vsix"
-Install-VSIX "Web Compiler" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/webcompiler/1.11.326/1482141920258/164873/42/Web%20Compiler%20v1.11.326.vsix"
-Install-VSIX "Trailing Whitespace Visualizer" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/trailingwhitespacevisualizer/2.5.90/1535142616954/Trailing_Whitespace_Visualizer_v2.5.90.vsix"
-Install-VSIX ".ignore" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/ignore/1.2.77/1535142107247/.ignore_v1.2.77.vsix"
-Install-VSIX "EditorConfig Language Service" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/editorconfig/1.17.246/1535731435315/EditorConfig_Language_Service_v1.17.246.vsix"
-Install-VSIX "Web Accessibility Checker" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/webaccessibilitychecker/1.5.58/1535134672243/Web_Accessibility_Checker_v1.5.58.vsix"
-Install-VSIX "Package Security Alerts" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/packagesecurityalerts/1.0.22/1536787178851/Package_Security_Alerts_v1.0.22.vsix"
+Install-VSIX "File Icons" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/fileicons/2.7.228/1551463426680/File_Icons_v2.7.228.vsix"
 Install-VSIX "Markdown Editor" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/markdowneditor/1.12.236/1535134643679/Markdown_Editor_v1.12.236.vsix"
-Install-VSIX "Vue.js Pack" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/vuejspack-18329/1.1.8/1490717404120/250225/5/Vue.js%20Pack%202017%20v1.1.8.vsix"
-Install-VSIX "CSS Sorter" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/csssorter/0.8.7/1538079473632/CSS_Sorter_v0.8.7.vsix"
+Install-VSIX "Package Security Alerts" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/packagesecurityalerts/1.0.22/1536787178851/Package_Security_Alerts_v1.0.22.vsix"
+Install-VSIX "Syntax Highlighting Pack" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/syntaxhighlightingpack/2.8.134/1536785834762/Syntax_Highlighting_Pack_v2.8.134.vsix"
+Install-VSIX "Web Accessibility Checker" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/webaccessibilitychecker/1.5.58/1535134672243/Web_Accessibility_Checker_v1.5.58.vsix"
+Install-VSIX "Web Compiler" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/webcompiler/1.12.394/1538582164333/Web_Compiler_v1.12.394.vsix"
 
-#### Productivity Power Tools 2017
-Install-VSIX "Custom Document Well" "https://visualstudioplatformteam.gallerycdn.vsassets.io/extensions/visualstudioplatformteam/customdocumentwell/15.0.4/1517829822936/242835/6/CustomDocWell.vsix"
-Install-VSIX "Editor Guidelines" "https://visualstudioproductteam.gallerycdn.vsassets.io/extensions/visualstudioproductteam/editorguidelines/15.0.4/1514397356063/242845/6/EditorGuidelines.vsix"
-Install-VSIX "Fix Mixed Tabs" "https://visualstudioproductteam.gallerycdn.vsassets.io/extensions/visualstudioproductteam/fixmixedtabs/15.0.3/1492133419585/242851/5/FixMixedTabs.vsix"
-Install-VSIX "Shrink Empty Lines" "https://visualstudioproductteam.gallerycdn.vsassets.io/extensions/visualstudioproductteam/syntacticlinecompression/15.0.5/1492133133449/242914/8/ShrinkEmptylines.vsix"
-Install-VSIX "Solution Error Visualizer" "https://visualstudioproductteam.gallerycdn.vsassets.io/extensions/visualstudioproductteam/solutionerrorvisualizer/15.0.4/1492133129068/242909/7/SolutionErrorFilter.vsix"
+#### Other Mads Kristensen Extentions
+Install-VSIX ".ignore" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/ignore/1.2.77/1535142107247/.ignore_v1.2.77.vsix"
+Install-VSIX "Trailing Whitespace Visualizer" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/trailingwhitespacevisualizer/2.5.93/1544213674964/Trailing_Whitespace_Visualizer_v2.5.93.vsix"
+Install-VSIX "EditorConfig Language Service" "https://madskristensen.gallerycdn.vsassets.io/extensions/madskristensen/editorconfig/1.17.258/1552490541253/EditorConfig_Language_Service_v1.17.258.vsix"
+
+#### Productivity Power Tools
+Install-VSIX "Fix Mixed Tabs" "https://visualstudioplatformteam.gallerycdn.vsassets.io/extensions/visualstudioplatformteam/fixmixedtabs/15.0.6/1553638217769/FixMixedTabs.vsix"
+Install-VSIX "Shrink Empty Lines" "https://visualstudioplatformteam.gallerycdn.vsassets.io/extensions/visualstudioplatformteam/syntacticlinecompression/15.0.7/1543238149040/SyntacticLineCompression.vsix"
+Install-VSIX "Solution Error Visualizer" "https://visualstudioplatformteam.gallerycdn.vsassets.io/extensions/visualstudioplatformteam/solutionerrorvisualizer/15.0.4/1543238152087/SolutionErrorFilter.vsix"
 
 #### Others
-Install-VSIX "VSColorOutput" "https://mikeward-annarbor.gallerycdn.vsassets.io/extensions/mikeward-annarbor/vscoloroutput/2.6.3/1543972507000/VSColorOutput.vsix" 
-Install-VSIX "Region Expander" "https://davidperfors.gallerycdn.vsassets.io/extensions/davidperfors/regionexpander/0.4/1521454529241/RegionExpander.vsix"
-Install-VSIX "Git Diff Margin" "https://laurentkempe.gallerycdn.vsassets.io/extensions/laurentkempe/gitdiffmargin/3.9.3.49/1551515359174/GitDiffMargin.vsix"
-Install-VSIX "CodeMaid" "https://stevecadwallader.gallerycdn.vsassets.io/extensions/stevecadwallader/codemaid/10.5.119/1528571016929/CodeMaid_v10.5.119.vsix"
-Install-VSIX "SQLite / SQL Server Compact Toolbox" "https://erikej.gallerycdn.vsassets.io/extensions/erikej/sqlservercompactsqlitetoolbox/4.7.534/1518354313794/SqlCeVsToolbox.4.7.534.vsix"
-Install-VSIX "StyleCop" "https://chrisdahlberg.gallerycdn.vsassets.io/extensions/chrisdahlberg/stylecop/5.0.6419.0/1501345807969/231103/4/StyleCop.vsix"
-Install-VSIX "SlowCheetah" "https://vscps.gallerycdn.vsassets.io/extensions/vscps/slowcheetah-xmltransforms/3.1.69.25612/1531957598091/Microsoft.VisualStudio.SlowCheetah.vsix"
-Install-VSIX "GhostDoc" "https://sergeb.gallerycdn.vsassets.io/extensions/sergeb/ghostdoc/5.9.18070/1520626062408/GhostDoc.v5.9.18070.VS2017.Extension.vsix"
-Install-VSIX "ReAttach" "https://erlandr.gallerycdn.vsassets.io/extensions/erlandr/reattach/2.2/1490598305903/86492/14/ReAttach.vsix"
-Install-VSIX "Disable No Source Available Tab" "https://mayerwin.gallerycdn.vsassets.io/extensions/mayerwin/disablenosourceavailabletab/2.0/1488372019971/39083/13/DisableNoSourceAvailableTab%20(2.0%20vs%202017%20support).vsix"
-Install-VSIX "PerfWatson Monitor" "https://paulharrington.gallerycdn.vsassets.io/extensions/paulharrington/perfwatsonmonitor-9621/11.17.414.0/1492179304031/75983/9/PerfWatsonMonitor.vsix"
-Install-VSIX "Roslyn Security Guard" "https://philippearteau.gallerycdn.vsassets.io/extensions/philippearteau/roslynsecurityguard/2.3.0/1519069597068/232680/3/RoslynSecurityGuard.Vsix.vsix"
-Install-VSIX "Puma Scan" "https://pumasecurity.gallerycdn.vsassets.io/extensions/pumasecurity/pumascan/2.0.0.1/1533875976755/Puma.Security.Rules.Vsix.2.0.0.1.vsix"
-Install-VSIX "SonarLint" "https://sonarsource.gallerycdn.vsassets.io/extensions/sonarsource/sonarlintforvisualstudio2017/4.4.0.3745/1536052203864/SonarLint.VSIX-4.4.0.3745-2017.vsix"Install-VSIX "Whack Whack Terminal" "https://danielgriffen.gallerycdn.vsassets.io/extensions/danielgriffen/whackwhackterminal/1.1.3.25906/1530136152443/WhackWhackTerminal.vsix"
-Install-VSIX "Visual Studio IntelliCode" "https://visualstudioexptteam.gallerycdn.vsassets.io/extensions/visualstudioexptteam/vsintellicode/1.3.8.60621/1536614597513/Microsoft.VisualStudio.IntelliCode.vsix"
-Install-VSIX "Visual Studio Spell Checker" "https://ewoodruff.gallerycdn.vsassets.io/extensions/ewoodruff/visualstudiospellcheckervs2017andlater/2018.8.25.0/1535239788431/VisualStudio.SpellChecker.VS2017AndLater.vsix"
+Install-VSIX "VSColorOutput" "https://mikeward-annarbor.gallerycdn.vsassets.io/extensions/mikeward-annarbor/vscoloroutput/2.6.4/1548094340477/VSColorOutput.vsix"
+Install-VSIX "Region Expander" "https://davidperfors.gallerycdn.vsassets.io/extensions/davidperfors/regionexpander/0.5/1543951484486/RegionExpander.vsix"
+Install-VSIX "Git Diff Margin" "https://laurentkempe.gallerycdn.vsassets.io/extensions/laurentkempe/gitdiffmargin/3.9.4.73/1552425425998/GitDiffMargin.vsix"
+Install-VSIX "CodeMaid" "https://stevecadwallader.gallerycdn.vsassets.io/extensions/stevecadwallader/codemaid/11.0.183/1553341607616/CodeMaid_v11.0.183.vsix"
+Install-VSIX "SQLite / SQL Server Compact Toolbox" "https://erikej.gallerycdn.vsassets.io/extensions/erikej/sqlservercompactsqlitetoolbox/4.7.634/1549801715905/SqlCeVsToolbox.4.7.634.vsix"
+#Install-VSIX "StyleCop" "https://chrisdahlberg.gallerycdn.vsassets.io/extensions/chrisdahlberg/stylecop/5.0.6419.0/1501345807969/231103/4/StyleCop.vsix"
+Install-VSIX "SlowCheetah" "https://vscps.gallerycdn.vsassets.io/extensions/vscps/slowcheetah-xmltransforms/3.2.18.4433/1547757608353/Microsoft.VisualStudio.SlowCheetah.vsix"
+#Install-VSIX "GhostDoc" "https://sergeb.gallerycdn.vsassets.io/extensions/sergeb/ghostdoc/5.9.18070/1520626062408/GhostDoc.v5.9.18070.VS2017.Extension.vsix"
+#Install-VSIX "ReAttach" "https://erlandr.gallerycdn.vsassets.io/extensions/erlandr/reattach/2.2/1490598305903/86492/14/ReAttach.vsix"
+Install-VSIX "Disable No Source Available Tab" "https://mayerwin.gallerycdn.vsassets.io/extensions/mayerwin/disablenosourceavailabletab/3.2/1552895908582/DisableNoSourceAvailableTab__3.2_.vsix"
+Install-VSIX "PerfWatson Monitor" "https://paulharrington.gallerycdn.vsassets.io/extensions/paulharrington/perfwatsonmonitor-9621/12.0.0/1553664190479/PerfWatsonMonitor.vsix"
+#Install-VSIX "Roslyn Security Guard" "https://philippearteau.gallerycdn.vsassets.io/extensions/philippearteau/roslynsecurityguard/2.3.0/1519069597068/232680/3/RoslynSecurityGuard.Vsix.vsix"
+Install-VSIX "Puma Scan" "https://pumasecurity.gallerycdn.vsassets.io/extensions/pumasecurity/pumascan/2.1.0.0/1553877086038/Puma.Security.Rules.Vsix.2.1.0.0.vsix"
+Install-VSIX "SonarLint" "https://sonarsource.gallerycdn.vsassets.io/extensions/sonarsource/sonarlintforvisualstudio2019/4.9.0.4316/1554460936213/SonarLint.VSIX-4.9.0.4316-2019.vsix"
+Install-VSIX "Visual Studio Spell Checker" "https://ewoodruff.gallerycdn.vsassets.io/extensions/ewoodruff/visualstudiospellcheckervs2017andlater/2018.10.27.0/1540682853066/VisualStudio.SpellChecker.VS2017AndLater.vsix"
+
+if ($errorInstalling) {
+    Write-Error "An Error occurred installing one of the VSIX extentions. Review the log file..."
+    throw
+}
