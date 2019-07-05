b4d1b072717ad3b2bc4450f49da026c9b692bde0
Julian Easterling <julian@julianscorner.com>
2019-07-05 09:45:38 -0400

    Updated my console settings to use ColorTool for the color scheme in consoles

--- mysettings-console/tools/chocolateyInstall.ps1
+++ mysettings-console/tools/chocolateyInstall.ps1
@@ -1,4 +1,21 @@
-$packageName = "mysettings-console"
+$url = "https://codeload.github.com/mbadolato/iTerm2-Color-Schemes/zip/master"
+
+if (Test-Path $env:TEMP\iTerm2-Color-Schemes.zip) {
+    Remove-Item $env:TEMP\iTerm2-Color-Schemes.zip -Force
+}
+
+Download-File $url $env:TEMP\iTerm2-Color-Schemes.zip
+
+if (Test-Path $env:TEMP\iTerm2-Color-Schemes-master) {
+    Remove-Item $env:TEMP\iTerm2-Color-Schemes-master -Recurse -Force
+}
+
+Unzip-File $env:TEMP\iTerm2-Color-Schemes.zip $env:TEMP\
+
+Copy-Item -Path $env:TEMP\iTerm2-Color-Schemes-master\schemes\* `
+    -Destination $env:ChocolateyInstall\lib\colortool\content\schemes\ `
+    -Recurse -Force
+
 $cmd = "$env:WINDIR\system32\reg.exe import $PSScriptRoot\registry.reg"
 
 if ([System.IntPtr]::Size -ne 4) {
@@ -6,3 +23,5 @@ if ([System.IntPtr]::Size -ne 4) {
 }
 
 cmd /c "$cmd"
+
+ColorTool.exe -b Andromeda
