3e9d7ba75c13e801b81f4cb503a55f1b8a01f9a0
Julian Easterling <julian@julianscorner.com>
2019-08-21 20:31:44 -0400

    removed bing desktop package since I can do with PowerShell the only feature I ever really wanted from that package.

--- bingdesktop/tools/chocolateyInstall.ps1
+++
@@ -1,6 +0,0 @@
-$packageName = "bingdesktop"
-$installerType = "EXE"
-$installerArgs = "/Q"
-$url = "http://download.microsoft.com/download/5/0/5/505FFC2C-8E37-41A0-8746-F9F3CF9FD8F9/BingDesktopSetup.exe"
-
-Install-ChocolateyPackage $packageName $installerType $installerArgs $url
