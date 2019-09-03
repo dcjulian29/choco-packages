7aa2fdf585a7e1377b9bd38eed8377621465f4eb
Julian Easterling <julian@julianscorner.com>
2019-09-03 10:36:39 -0400

    Added a line to output which package the current operation is doing.

--- Build-And-Push-All.ps1
+++ Build-And-Push-All.ps1
@@ -5,6 +5,7 @@ Push-Location choco-packages
 Load-NuGetProfile choco
 
 Get-ChildItem -Directory | ForEach-Object {
+    Write-Output $("------------ {0}" -f $_.Name)
     Push-Location $_.Name
     Remove-Item *.nupkg
     Make-ChocolateyPackage
