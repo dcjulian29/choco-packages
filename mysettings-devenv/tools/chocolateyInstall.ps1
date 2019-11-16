6c4e81651c4f360d28231bcb5ce13fdd1adff064
Julian Easterling <julian@julianscorner.com>
2019-11-16 10:05:22 -0500

    Updated My Development Environment settings and moved winmerge filter into script.

--- mysettings-devenv/tools/chocolateyInstall.ps1
+++ mysettings-devenv/tools/chocolateyInstall.ps1
@@ -1,4 +1,3 @@
-$packageName = "mysettings-devenv"
 $icon = "https://www.iconfinder.com/icons/37070/download/ico"
 
 if (-not (Test-Path $env:SYSTEMDRIVE\code))
@@ -16,7 +15,7 @@ Mode=
 Vid=
 FolderType=Generic
 "@
- 
+
 attrib.exe +S +H $env:SYSTEMDRIVE\code\desktop.ini
 attrib.exe +S $env:SYSTEMDRIVE\code
 
@@ -27,10 +26,41 @@ gd -Key "code" -SelectedPath "${env:SYSTEMDRIVE}\code" -add
 gd -Key "projects" -delete
 gd -Key "projects" -SelectedPath "${env:SYSTEMDRIVE}\code" -add
 
-if (Test-Path $env:SYSTEMDRIVE\etc\SoftwareDevelopment.flt)
-{
+if (Test-Path $env:SYSTEMDRIVE\etc\SoftwareDevelopment.flt) {
     New-Item $env:USERPROFILE\Documents\WinMerge -ItemType Directory | Out-Null
     New-Item $env:USERPROFILE\Documents\WinMerge\Filters -ItemType Directory | Out-Null
-    Copy-Item $env:SYSTEMDRIVE\etc\SoftwareDevelopment.flt `
-        $env:USERPROFILE\Documents\WinMerge\Filters\SoftwareDevelopment.flt | Out-Null
 }
+
+Set-Content $env:USERPROFILE\Documents\WinMerge\Filters\SoftwareDevelopment.flt @'
+name: Software Development
+desc: This filter lets through only files software developers care about
+def: include
+
+## Filters for filenames begin with f:
+## Filters for directories begin with d:
+## (Inline comments begin with " ##" and extend to the end of the line)
+
+f: \.gitignore$
+f: \.hgignore$
+f: \.svnignore$
+f: \.(vs[sp])?scc$
+f: \.user$
+f: \.bak$
+f: StyleCop\.Cache$
+
+d: \\\.vs$
+d: \\\.svn$
+d: \\_svn$
+d: \\\.git$
+d: \\\.hg$
+d: \\Debug$
+d: \\Release$
+d: \\bin$
+d: \\obj$
+d: \\.build$
+d: \\build$
+d: \\tools$
+d: \\packages$
+'@
+
+[System.Environment]::SetEnvironmentVariable('CAKE_SETTINGS_SKIPPACKAGEVERSIONCHECK', 'true',[System.EnvironmentVariableTarget]::User)
