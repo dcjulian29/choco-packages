69fbd10c7a8fd577c6845f3648df213a902591c3
Julian Easterling <julian@julianscorner.com>
2019-11-30 17:38:17 -0500

    Updated Visual Studio package to copy Code Snippets if they are present.

---
+++ mydev-visualstudio/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+$etc = "$env:SystemDrive\etc\visualstudio\Code Snippets"
+$snippets = "$env:USERPROFILE\Documents\Visual Studio 2019\Code Snippets"
+
+if (-not (Test-Path $snippets)) {
+    New-Item -Path $snippets -ItemType Directory | Out-Null
+}
+
+if (Test-Path $etc) {
+    Copy-Item -Path $etc -Destination $snippets -Recurse -Force
+}
