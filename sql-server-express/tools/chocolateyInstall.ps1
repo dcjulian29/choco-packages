a1470a9214da84b0a203bd7090817d25856e6033
Julian Easterling <julian@julianscorner.com>
2019-12-01 03:09:21 -0500

    Updated to SQL Server Express 2019 and to using INI file to configure the setup

--- sql-server-express/tools/chocolateyInstall.ps1
+++ sql-server-express/tools/chocolateyInstall.ps1
@@ -1,16 +1,6 @@
 $packageName = "sql-server-express"
-$url = "https://download.microsoft.com/download/E/F/2/EF23C21D-7860-4F05-88CE-39AA114B014B/SQLEXPR_x64_ENU.exe"
+$url = "https://download.microsoft.com/download/2/1/6/216eb471-e637-4517-97a6-b247d8051759/SQL2019-SSEI-Expr.exe"
 $downloadPath = "$env:TEMP\$packageName"
-
-$sid = New-Object Security.Principal.SecurityIdentifier 'S-1-5-32-544'
-$administrators = $sid.Translate([Security.Principal.NTAccount]).Value
-
-$arg1 = "/QS /ACTION=Install /FEATURES=SQL /TCPENABLED=1 /SKIPRULES=""RebootRequiredCheck"""
-$arg2 = "/INSTANCENAME=MSSQLSERVER /SQLSVCACCOUNT=""NT AUTHORITY\Network Service"""
-$arg3 = "/SQLSYSADMINACCOUNTS=""$($administrators)"""
-$arg4 = "/AGTSVCACCOUNT=""NT AUTHORITY\Network Service"" /IACCEPTSQLSERVERLICENSETERMS"
-
-$installerArgs = "$arg1 $arg2 $arg3 $arg4"
 
 if (Test-Path $downloadPath) {
     Remove-Item -Path $downloadPath -Recurse -Force
@@ -20,8 +10,6 @@ New-Item -Type Directory -Path $downloadPath | Out-Null
 
 Download-File $url "$downloadPath\$packageName.exe"
 
-Start-Process "$downloadPath\$packageName.exe" "/Q /x:`"$downloadPath\sql`"" -Wait
-
-Start-Process "$downloadPath\sql\setup.exe" "$installerArgs" -Wait
+Start-Process "$downloadPath\$packageName.exe" "/ConfigurationFile=""$env:SystemDrive\etc\sql\setupconfig.ini"" /IACCEPTSQLSERVERLICENSETERMS /MEDIAPATH=""$env:TEMP\SQL2019"" /Q" -Wait
 
 Install-Module -Name SqlServer -AllowClobber
