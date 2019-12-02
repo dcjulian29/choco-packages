7e548573231074b9dfd8e84424113c0847874eaf
Julian Easterling <julian@julianscorner.com>
2019-12-01 19:14:28 -0500

    Updated the Winmerge Filters to just copy to the proper directory instead of keeping it in source control.

--- mysettings-devenv/tools/chocolateyInstall.ps1
+++ mysettings-devenv/tools/chocolateyInstall.ps1
@@ -26,41 +26,9 @@ gd -Key "code" -SelectedPath "${env:SYSTEMDRIVE}\code" -add
 gd -Key "projects" -delete
 gd -Key "projects" -SelectedPath "${env:SYSTEMDRIVE}\code" -add
 
-if (Test-Path $env:SYSTEMDRIVE\etc\SoftwareDevelopment.flt) {
-    New-Item $env:USERPROFILE\Documents\WinMerge -ItemType Directory | Out-Null
-    New-Item $env:USERPROFILE\Documents\WinMerge\Filters -ItemType Directory | Out-Null
+if (Test-Path "$env:SYSTEMDRIVE\etc\WinMerge") {
+    Copy-Item -Path "$env:SYSTEMDRIVE\etc\WinMerge" `
+        -Destination "$env:USERPROFILE\Documents" -Recurse -Force
 }
-
-Set-Content $env:USERPROFILE\Documents\WinMerge\Filters\SoftwareDevelopment.flt @'
-name: Software Development
-desc: This filter lets through only files software developers care about
-def: include
-
-## Filters for filenames begin with f:
-## Filters for directories begin with d:
-## (Inline comments begin with " ##" and extend to the end of the line)
-
-f: \.gitignore$
-f: \.hgignore$
-f: \.svnignore$
-f: \.(vs[sp])?scc$
-f: \.user$
-f: \.bak$
-f: StyleCop\.Cache$
-
-d: \\\.vs$
-d: \\\.svn$
-d: \\_svn$
-d: \\\.git$
-d: \\\.hg$
-d: \\Debug$
-d: \\Release$
-d: \\bin$
-d: \\obj$
-d: \\.build$
-d: \\build$
-d: \\tools$
-d: \\packages$
-'@
 
 [System.Environment]::SetEnvironmentVariable('CAKE_SETTINGS_SKIPPACKAGEVERSIONCHECK', 'true',[System.EnvironmentVariableTarget]::User)
