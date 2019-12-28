9546895bf36050ea4ec67b3235e814cda91b047c
Julian Easterling <julian@julianscorner.com>
2019-12-27 22:46:35 -0500

    Added a script that will build all packages locally

---
+++ Build-All.ps1
@@ -0,0 +1,11 @@
+﻿gd code
+
+Push-Location choco-packages
+
+Get-ChildItem -Directory | ForEach-Object {
+    Write-Output $("------------ {0}" -f $_.Name)
+    Push-Location $_.Name
+    Remove-Item *.nupkg
+    Make-ChocolateyPackage
+    Pop-Location
+}
