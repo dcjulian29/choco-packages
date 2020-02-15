f804d9597a082fe05b86be7523bfe478036c92d0
Julian Easterling <julian@julianscorner.com>
2020-02-15 18:32:30 -0500

    Updated my developement package for databases to include MongoDB

--- mydev-database/tools/chocolateyInstall.ps1
+++ mydev-database/tools/chocolateyInstall.ps1
@@ -2,6 +2,8 @@ $packageName = "mydev-database"
 
 Invoke-ElevatedCommand "sc.exe" -ArgumentList "config MSSQLSERVER start= demand" -Wait
 
+Invoke-ElevatedCommand "sc.exe" -ArgumentList "config MongoDB start= demand" -Wait
+
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 
 Install-Module SqlServer -Force -AllowClobber
