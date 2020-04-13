8c7b9cacf0205e6fa6fef71d7c6e2d47e06c72ed
Julian Easterling <julian@julianscorner.com>
2020-04-12 21:21:40 -0400

    Updated git config package so I can run it on linux systems too

---
+++ mysettings-git/tools/apply.sh
@@ -0,0 +1,23 @@
+INPUT=config.csv
+OLDIFS=$IFS
+IFS=','
+
+while read key value
+do
+    [[ $key != \"* ]] && continue
+
+    [[ $key =~ ^.Key ]] && continue
+    [[ $key =~ .core.editor. ]] && continue
+    [[ $key =~ .*winmerge.* ]] && continue
+    [[ $value =~ .*winmerge.* ]] && continue
+
+    key=${key:1}
+    key=${key:0:-1}
+
+    value=${value:1}
+    value=${value:0:-2}
+
+    echo "git config --global --replace-all $key $value"
+done < $INPUT
+
+IFS=$OLDIFS
