d7c07d74e91ebfb19eebce5657800e1c33f7d28a
Julian Easterling <julian@julianscorner.com>
2020-04-28 19:13:00 -0400

    Updated "my" laptop, workstation, devvm packages to depend on other package to download Ubuntu WSL

--- mydev-tools-personal/tools/chocolateyInstall.ps1
+++ mydev-tools-personal/tools/chocolateyInstall.ps1
@@ -1,5 +1,3 @@
-$Script:UbuntuVersion = 1804
-
 $downloadPath = "$env:TEMP\mydev-tools-personal"
 
 if (Test-Path $downloadPath) {
@@ -8,7 +6,7 @@ if (Test-Path $downloadPath) {
 
 New-Item -Type Directory -Path $downloadPath | Out-Null
 
-Download-File "https://rubygems.org/gems/rubygems-update-2.6.11.gem" "$downloadPath\rubygems.gem"
+Download-File "https://rubygems.org/downloads/rubygems-update-3.1.2.gem" "$downloadPath\rubygems.gem"
 
 gem install --local "$downloadPath\rubygems.gem"
 
@@ -16,50 +14,32 @@ update_rubygems
 
 gem install jekyll
 
-Write-Output "Downloading and installing Ubuntu $($Script:UbuntuVersion)..."
+if (-not (Test-Path $env:SYSTEMDRIVE\Ubuntu\rootfs)) {
+    Write-Output "Ubuntu Linux has been installed...  Starting final install."
 
-Push-Location -Path $env:TEMP
+    $ubuntu = Get-ChildItem -Path $env:SYSTEMDRIVE\Ubuntu `
+        | Where-Object { $_.Name -match "^ubuntu\d+\.exe$" } `
+        | Sort-Object Name -Descending `
+        | Select-Object -First 1
 
-Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-$Script:UbuntuVersion -OutFile Ubuntu.appx -UseBasicParsing
+    Start-Process -FilePath $ubuntu -ArgumentList "install --root" -NoNewWindow -Wait
 
-Rename-Item Ubuntu.appx Ubuntu.zip
+    Start-Process -FilePath $ubuntu `
+        -ArgumentList "run adduser $($env:USERNAME) --gecos ""First,Last,RoomNumber,WorkPhone,HomePhone"" `
+        --disabled-password" -NoNewWindow -Wait
 
-if (Test-Path $env:SYSTEMDRIVE\Ubuntu) {
-    Remove-Item -Path $env:SYSTEMDRIVE\Ubuntu -Recurse -Force | Out-Null
+    Start-Process -FilePath $ubuntu `
+        -ArgumentList "run usermod -aG sudo $($env:USERNAME)" -NoNewWindow -Wait
+
+    Start-Process -FilePath $ubuntu `
+        -ArgumentList "run echo '$($env:USERNAME) ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo" `
+        -NoNewWindow -Wait
+
+    Start-Process -FilePath $ubuntu `
+        -ArgumentList "config --default-user $($env:USERNAME)" -NoNewWindow -Wait
+
+    Start-Process -FilePath $ubuntu `
+        -ArgumentList "run curl -sSL http://dl.julianscorner.com/l/init.sh | bash" -NoNewWindow -Wait
+} else {
+    Write-Output "Ubuutu is already installed and configured. Not overwriting the installed version..."
 }
-
-New-Item -Type Directory -Path $env:SYSTEMDRIVE\Ubuntu | Out-Null
-
-Expand-Archive Ubuntu.zip $env:SYSTEMDRIVE\Ubuntu
-
-Set-Content $env:SYSTEMDRIVE\Ubuntu\desktop.ini @"
-[.ShellClassInfo]
-IconResource=$env:SYSTEMDRIVE\Ubuntu\ubuntu$Script:UbuntuVersion.exe,0
-"@
-
-attrib +S +H $env:SYSTEMDRIVE\Ubuntu\desktop.ini
-attrib +S $env:SYSTEMDRIVE\Ubuntu
-
-Write-Output "Ubuntu Linux has been installed...  Starting final install."
-
-Pop-Location
-
-Remove-Item -Path $env:TEMP\ubuntu.zip -Force | Out-Null
-
-Start-Process -FilePath $env:SYSTEMDRIVE\Ubuntu\ubuntu$Script:UbuntuVersion.exe `
-    -ArgumentList "install --root" -NoNewWindow -Wait
-
-Start-Process -FilePath $env:SYSTEMDRIVE\Ubuntu\ubuntu$Script:UbuntuVersion.exe `
-    -ArgumentList "run adduser $($env:USERNAME) --gecos ""First,Last,RoomNumber,WorkPhone,HomePhone"" --disabled-password" -NoNewWindow -Wait
-
-Start-Process -FilePath $env:SYSTEMDRIVE\Ubuntu\ubuntu$Script:UbuntuVersion.exe `
-    -ArgumentList "run usermod -aG sudo $($env:USERNAME)" -NoNewWindow -Wait
-
-Start-Process -FilePath $env:SYSTEMDRIVE\Ubuntu\ubuntu$Script:UbuntuVersion.exe `
-    -ArgumentList "run echo '$($env:USERNAME) ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo" -NoNewWindow -Wait
-
-Start-Process -FilePath $env:SYSTEMDRIVE\Ubuntu\ubuntu$Script:UbuntuVersion.exe `
-    -ArgumentList "config --default-user $($env:USERNAME)" -NoNewWindow -Wait
-
-Start-Process -FilePath $env:SYSTEMDRIVE\Ubuntu\ubuntu$Script:UbuntuVersion.exe `
-    -ArgumentList "run curl -sSL http://dl.julianscorner.com/l/init.sh | bash" -NoNewWindow -Wait
