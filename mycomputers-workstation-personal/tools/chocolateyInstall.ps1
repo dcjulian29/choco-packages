37d81729e405aab6992495a31bb15bba9ae34414
Julian Easterling <julian@julianscorner.com>
2020-04-28 19:10:35 -0400

    Ubuntu WSL will get downloaded via separate package

--- mycomputers-workstation-personal/tools/chocolateyInstall.ps1
+++ mycomputers-workstation-personal/tools/chocolateyInstall.ps1
@@ -24,33 +24,3 @@ if (-not $wsl) {
 
     Write-Warning "You must reboot before using the Linux Subsystem..."
 }
-
-if (-not (Test-Path $env:SYSTEMDRIVE\Ubuntu)) {
-    Write-Output "Downloading and installing Ubuntu 18.04 ..."
-
-    if (Test-Path $env:TEMP\ubuntu.appx) {
-        Remove-Item -Path $env:TEMP\ubuntu.appx -Force | Out-Null
-    }
-    
-    if (Test-Path $env:TEMP\ubuntu.zip) {
-        Remove-Item -Path $env:TEMP\ubuntu.zip -Force | Out-Null
-    }
-
-    Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile $env:TEMP\Ubuntu.appx -UseBasicParsing
-
-    Rename-Item $env:TEMP\Ubuntu.appx $env:TEMP\Ubuntu.zip
-
-    New-Item -Type Directory -Path $env:SYSTEMDRIVE\Ubuntu | Out-Null
-
-    Expand-Archive $env:TEMP\Ubuntu.zip $env:SYSTEMDRIVE\Ubuntu
-
-    Set-Content $env:SYSTEMDRIVE\Ubuntu\desktop.ini @"
-    [.ShellClassInfo]
-    IconResource=$env:SYSTEMDRIVE\Ubuntu\ubuntu1804.exe,0
-"@
-
-    attrib +S +H $env:SYSTEMDRIVE\Ubuntu\desktop.ini
-    attrib +S $env:SYSTEMDRIVE\Ubuntu
-
-    Remove-Item -Path $env:TEMP\ubuntu.zip -Force | Out-Null
-}
