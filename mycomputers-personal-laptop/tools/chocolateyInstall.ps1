c5f4089400d38ea6496adf87ca0dfc37d5a5799b
Julian Easterling <julian@julianscorner.com>
2020-04-28 19:24:10 -0400

    Updated packages to depending on mysettings-ubuntu-wsl to remove duplicate code

--- mycomputers-personal-laptop/tools/chocolateyInstall.ps1
+++ mycomputers-personal-laptop/tools/chocolateyInstall.ps1
@@ -2,36 +2,6 @@ New-LabVMSwitch
 Set-VMHost -VirtualMachinePath 'C:\Virtual Machines'
 Set-VMHost -VirtualHardDiskPath 'C:\Virtual Machines\Discs'
 
-if (-not (Test-Path $env:SYSTEMDRIVE\Ubuntu\rootfs)) {
-    Write-Output "Ubuntu Linux has been installed...  Starting final install."
-
-    $ubuntu = Get-ChildItem -Path $env:SYSTEMDRIVE\Ubuntu `
-        | Where-Object { $_.Name -match "^ubuntu\d+\.exe$" } `
-        | Sort-Object Name -Descending `
-        | Select-Object -First 1
-
-    Start-Process -FilePath $ubuntu -ArgumentList "install --root" -NoNewWindow -Wait
-
-    Start-Process -FilePath $ubuntu `
-        -ArgumentList "run adduser $($env:USERNAME) --gecos ""First,Last,RoomNumber,WorkPhone,HomePhone"" `
-        --disabled-password" -NoNewWindow -Wait
-
-    Start-Process -FilePath $ubuntu `
-        -ArgumentList "run usermod -aG sudo $($env:USERNAME)" -NoNewWindow -Wait
-
-    Start-Process -FilePath $ubuntu `
-        -ArgumentList "run echo '$($env:USERNAME) ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo" `
-        -NoNewWindow -Wait
-
-    Start-Process -FilePath $ubuntu `
-        -ArgumentList "config --default-user $($env:USERNAME)" -NoNewWindow -Wait
-
-    Start-Process -FilePath $ubuntu `
-        -ArgumentList "run curl -sSL http://dl.julianscorner.com/l/init.sh | bash" -NoNewWindow -Wait
-} else {
-    Write-Output "Ubuntu is already installed and configured. Not overwriting the installed version..."
-}
-
 if (Get-Command "minikube.exe") {
     if (-not ($(Get-VM minikube))) {
         Write-Output "Initialize MiniKube and configure its VM ..."
--- mydev-tools-personal/tools/chocolateyInstall.ps1
+++ mydev-tools-personal/tools/chocolateyInstall.ps1
@@ -13,33 +13,3 @@ gem install --local "$downloadPath\rubygems.gem"
 update_rubygems
 
 gem install jekyll
-
-if (-not (Test-Path $env:SYSTEMDRIVE\Ubuntu\rootfs)) {
-    Write-Output "Ubuntu Linux has been installed...  Starting final install."
-
-    $ubuntu = Get-ChildItem -Path $env:SYSTEMDRIVE\Ubuntu `
-        | Where-Object { $_.Name -match "^ubuntu\d+\.exe$" } `
-        | Sort-Object Name -Descending `
-        | Select-Object -First 1
-
-    Start-Process -FilePath $ubuntu -ArgumentList "install --root" -NoNewWindow -Wait
-
-    Start-Process -FilePath $ubuntu `
-        -ArgumentList "run adduser $($env:USERNAME) --gecos ""First,Last,RoomNumber,WorkPhone,HomePhone"" `
-        --disabled-password" -NoNewWindow -Wait
-
-    Start-Process -FilePath $ubuntu `
-        -ArgumentList "run usermod -aG sudo $($env:USERNAME)" -NoNewWindow -Wait
-
-    Start-Process -FilePath $ubuntu `
-        -ArgumentList "run echo '$($env:USERNAME) ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo" `
-        -NoNewWindow -Wait
-
-    Start-Process -FilePath $ubuntu `
-        -ArgumentList "config --default-user $($env:USERNAME)" -NoNewWindow -Wait
-
-    Start-Process -FilePath $ubuntu `
-        -ArgumentList "run curl -sSL http://dl.julianscorner.com/l/init.sh | bash" -NoNewWindow -Wait
-} else {
-    Write-Output "Ubuutu is already installed and configured. Not overwriting the installed version..."
-}
