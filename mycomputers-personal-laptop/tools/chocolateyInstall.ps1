11a601f1f12d59b903babafd0d40df859bba5bf7
Julian Easterling <julian@julianscorner.com>
2020-04-28 19:33:13 -0400

    Moved the final configuration of minikube to separate package

--- mycomputers-personal-laptop/tools/chocolateyInstall.ps1
+++ mycomputers-personal-laptop/tools/chocolateyInstall.ps1
@@ -1,16 +1,3 @@
 New-LabVMSwitch
 Set-VMHost -VirtualMachinePath 'C:\Virtual Machines'
 Set-VMHost -VirtualHardDiskPath 'C:\Virtual Machines\Discs'
-
-if (Get-Command "minikube.exe") {
-    if (-not ($(Get-VM minikube))) {
-        Write-Output "Initialize MiniKube and configure its VM ..."
-
-        & minikube.exe config set vm-driver hyperv
-        & minikube.exe config set hyperv-virtual-switch "Default Switch"
-        & minikube.exe start
-        & minikube.exe ssh 'sudo poweroff'
-
-        Set-VMMemory -VMName "minikube" -DynamicMemoryEnabled $false
-    }
-}
