481d36f10b9c7d06644754f713630abe34fe8daf
Julian Easterling <julian@julianscorner.com>
2020-05-07 10:33:32 -0400

    Fixed the destination where the gitextensions settings file gets copied

--- mysettings-gitextensions/tools/chocolateyInstall.ps1
+++ mysettings-gitextensions/tools/chocolateyInstall.ps1
@@ -6,5 +6,5 @@ if (-not (Test-Path "$env:AppData\GitExtensions\GitExtensions")) {
     New-Item -Path "$env:AppData\GitExtensions\GitExtensions" -ItemType Directory | Out-Null
 }
 
-Copy-Item -Path "$PSScriptRoot\..\content" `
+Copy-Item -Path "$PSScriptRoot\..\content\*" `
     -Destination "$env:AppData\GitExtensions\GitExtensions" -Recurse -Force
