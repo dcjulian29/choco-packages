ea9b680e7a2729760753bc0cef909a7fc2cf64a8
Julian Easterling <julian@julianscorner.com>
2020-06-05 17:17:01 -0400

    Removed Chrome add-in from Choco installs

--- mycomputers-family/tools/chocolateyInstall.ps1
+++ mycomputers-family/tools/chocolateyInstall.ps1
@@ -1,5 +1,3 @@
-$packageName = "mycomputers-family"
-
 if (Test-Path $env:SYSTEMDRIVE\home)
 {
     Remove-Item -Path $env:SYSTEMDRIVE\home
@@ -8,9 +6,4 @@ if (Test-Path $env:SYSTEMDRIVE\home)
 if (Test-Path $env:SYSTEMDRIVE\tools)
 {
     Remove-Item -Path $env:SYSTEMDRIVE\tools
-}
-
-if (-not ([bool](Invoke-Command -ComputerName $env:COMPUTERNAME `
-    -ScriptBlock {"IPConfig"} -ErrorAction SilentlyContinue))) {
-    Enable-PSRemoting -Force
 }
