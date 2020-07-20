1d484fe650c6789aeb9a17f65478bd3ee96a2211
Julian Easterling <julian@julianscorner.com>
2020-07-20 12:47:39 -0400

    Updated Ruby Gem update and added a few steps to install Jekyll

--- mydev-tools-personal/tools/chocolateyInstall.ps1
+++ mydev-tools-personal/tools/chocolateyInstall.ps1
@@ -6,10 +6,12 @@ if (Test-Path $downloadPath) {
 
 New-Item -Type Directory -Path $downloadPath | Out-Null
 
-Download-File "https://rubygems.org/downloads/rubygems-update-3.1.2.gem" "$downloadPath\rubygems.gem"
+Download-File "https://rubygems.org/downloads/rubygems-update-3.1.4.gem" "$downloadPath\rubygems.gem"
 
 gem install --local "$downloadPath\rubygems.gem"
 
 update_rubygems
 
+ridk install 3
+gem install bundler
 gem install jekyll
