a538e948eab59ada36a961d546bed5c7da80cb83
Julian Easterling <julian@julianscorner.com>
2020-12-17 22:48:00 -0500

    Added a package that will install the WSL 2 Kernel update

---
+++ mysettings-wsl2/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+$wsl = (Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux).State -eq "Enabled"
+
+if (-not $wsl) {
+    throw "WSL feature is not enabled"
+}
+
+if ([System.Environment]::OSVersion.Version.Build -ge 19041) {
+    $vmp = (Get-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform).State -eq "Enabled"
+
+    if (-not $vmp) {
+        throw "Virtual Machine Platform feature is not enabled"
+    }
+
+    Write-Output "Downloading WSL 2 Kernel Update..."
+    Invoke-WebRequest -Uri "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi" `
+        -OutFile "$env:TEMP\wsl_update_x64.msi"
+
+    $logWslFile = Get-LogFileName -Date $(Get-Date) -Suffix "$env:COMPUTERNAME-wsl2kernel"
+    Start-Process "$env:TEMP\wsl_update_x64.msi" "/passive /norestart /log $logWslFile" -Wait
+
+    & wsl.exe --set-default-version 2
+}
