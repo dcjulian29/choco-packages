030ad03962f879e682e1bad80f9d9db7e42ec09c
Julian Easterling <julian@julianscorner.com>
2021-02-16 21:41:24 -0500

    Switched to using docker containers for database servers

--- mydev-database/tools/chocolateyInstall.ps1
+++ mydev-database/tools/chocolateyInstall.ps1
@@ -1,9 +1,3 @@
-$packageName = "mydev-database"
-
-Invoke-ElevatedCommand "sc.exe" -ArgumentList "config MSSQLSERVER start= demand" -Wait
-
-Invoke-ElevatedCommand "sc.exe" -ArgumentList "config MongoDB start= demand" -Wait
-
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 
 Install-Module SqlServer -Force -AllowClobber
