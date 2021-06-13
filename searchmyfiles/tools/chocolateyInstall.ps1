e95d92d0346e22bb0e0469794dc481ea60ef4136
Julian Easterling <julian@julianscorner.com>
2021-06-12 23:53:10 -0400

    SHA256 need to be uppercase for choco

--- searchmyfiles/tools/chocolateyInstall.ps1
+++ searchmyfiles/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 $url = 'http://www.nirsoft.net/utils/searchmyfiles.zip'
-$cksum = '32078f2aa5e5f1f5d7784021f82b470a6eb1ef1bc0c27cf36e7bd70e100856ab'
+$cksum = '32078F2AA5E5F1F5D7784021F82B470A6EB1EF1BC0C27CF36E7BD70E100856AB'
 
 if ([Environment]::Is64BitOperatingSystem) {
     $url = 'http://www.nirsoft.net/utils/searchmyfiles-x64.zip'
-    $cksum = 'a5f8a906e72ea3675ec4b5128e589b5a1de0d50123933496b3b212e1e858e24f'
+    $cksum = 'A5F8A906E72EA3675EC4B5128E589B5A1DE0D50123933496B3B212E1E858E24F'
 }
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
