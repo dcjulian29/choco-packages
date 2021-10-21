9cd192601b16dd7cae0dd6c43e32af4bf2c2ebd2
Julian Easterling <julian@julianscorner.com>
2021-10-20 22:09:24 -0400

    Updated Executor to change default browser to Edge

--- executor-julian/tools/executor-make-master.cmd
+++ executor-julian/tools/executor-make-master.cmd
@@ -23,6 +23,10 @@ for /f "usebackq delims=" %%O in (`dir /b /ad %SYSTEMDRIVE%\home`) do (
     )
 )
 
+if exist "%SYSTEMDRIVE%\home\enclara\etc\executor\executor.ini" (
+    powershell.exe -NoProfile -ExecutionPolicy Bypass -NoLogo -Command "(gc '%SYSTEMDRIVE%\home\enclara\etc\executor\executor.ini') -replace '^browser=$', 'browser=C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe' | Out-File -encoding ASCII '%SYSTEMDRIVE%\home\enclara\etc\executor\executor.ini'"
+)
+
 :EOF
 
 echo.
