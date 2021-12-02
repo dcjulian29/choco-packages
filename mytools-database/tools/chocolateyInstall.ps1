723d21196b86872724626160de60d1a680ab8952
Julian Easterling <julian@julianscorner.com>
2021-12-01 22:22:00 -0500

    Install NuGet provider

--- mytools-database/tools/chocolateyInstall.ps1
+++ mytools-database/tools/chocolateyInstall.ps1
@@ -1,3 +1,5 @@
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
 Install-Module SqlServer -Force -AllowClobber
