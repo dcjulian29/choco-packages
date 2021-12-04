6199c74baeba2ca279564f39cbe569dfbb3091f5
Julian Easterling <julian@julianscorner.com>
2021-12-04 17:59:54 -0500

    Ensure PSGet is configured properly

--- mytools-database/tools/chocolateyInstall.ps1
+++ mytools-database/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-
+Import-Module PackageManagement -RequiredVersion 1.0.0.1
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 
 Install-Module SqlServer -Force -AllowClobber
