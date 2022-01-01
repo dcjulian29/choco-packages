c68849f0c264d905820fe461a815a6160cc14ab6
Julian Easterling <julian@julianscorner.com>
2022-01-01 11:27:53 -0500

    Added a Nerd Font and switch my console windows to that font.

--- mysettings-console/tools/chocolateyInstall.ps1
+++ mysettings-console/tools/chocolateyInstall.ps1
@@ -27,3 +27,13 @@ if ([System.IntPtr]::Size -ne 4) {
 cmd /c "$cmd"
 
 cmd /c "ColorTool.exe -b purplepeter.itermcolors"
+
+# Install My "Nerd" Font
+$font = "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/CascadiaCode/Regular/complete/Caskaydia%20Cove%20Regular%20Nerd%20Font%20Complete%20Mono%20Windows%20Compatible.otf"
+
+Invoke-WebRequest -Uri $font `
+    -OutFile "$env:TEMP\Caskaydia Cove Regular Nerd Font Complete Mono Windows Compatible.otf"
+
+Install-Font -Path "$env:TEMP\Caskaydia Cove Regular Nerd Font Complete Mono Windows Compatible.otf"
+
+Remove-Item -Path "$env:TEMP\Caskaydia Cove Regular Nerd Font Complete Mono Windows Compatible.otf" -Force
