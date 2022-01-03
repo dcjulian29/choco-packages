337e201c23ffdcffa6af50a6533891860f0e8955
Julian Easterling <julian@julianscorner.com>
2022-01-02 22:46:49 -0500

    Switching to a new feature that allows terminal to be used instead of conhost

--- mysettings-console/tools/chocolateyInstall.ps1
+++ mysettings-console/tools/chocolateyInstall.ps1
@@ -15,9 +15,6 @@ Copy-Item -Path $env:TEMP\iTerm2-Color-Schemes-master\schemes\* `
     -Destination $env:ChocolateyInstall\lib\colortool\content\schemes\ `
     -Recurse -Force
 
-# Sometimes color settings get set based on subkeys in HKCU:\Console, remove them
-Remove-Item -Path HKCU:\Console\* -Recurse -Force
-
 $cmd = "$env:WINDIR\system32\reg.exe import $PSScriptRoot\registry.reg"
 
 if ([System.IntPtr]::Size -ne 4) {
@@ -25,8 +22,6 @@ if ([System.IntPtr]::Size -ne 4) {
 }
 
 cmd /c "$cmd"
-
-cmd /c "ColorTool.exe -b purplepeter.itermcolors"
 
 # Install My "Nerd" Font
 $font = "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/CascadiaCode/Regular/complete/Caskaydia%20Cove%20Regular%20Nerd%20Font%20Complete%20Mono%20Windows%20Compatible.otf"
