714556306bde293e8fffa0d830ea7a5986260e2d
Julian Easterling <julian@julianscorner.com>
2022-01-02 22:48:26 -0500

    Don't download the colortool themes

--- mysettings-console/tools/chocolateyInstall.ps1
+++ mysettings-console/tools/chocolateyInstall.ps1
@@ -1,20 +1,3 @@
-$url = "https://codeload.github.com/mbadolato/iTerm2-Color-Schemes/zip/master"
-
-if (-not (Test-Path $env:TEMP\iTerm2-Color-Schemes.zip)) {
-    (New-Object System.Net.WebClient).DownloadFile("$url", "${env:TEMP}\iTerm2-Color-Schemes.zip")
-}
-
-if (Test-Path $env:TEMP\iTerm2-Color-Schemes-master) {
-    Remove-Item $env:TEMP\iTerm2-Color-Schemes-master -Recurse -Force
-}
-
-[System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
-[System.IO.Compression.ZipFile]::ExtractToDirectory("${env:TEMP}\iTerm2-Color-Schemes.zip", $env:TEMP)
-
-Copy-Item -Path $env:TEMP\iTerm2-Color-Schemes-master\schemes\* `
-    -Destination $env:ChocolateyInstall\lib\colortool\content\schemes\ `
-    -Recurse -Force
-
 $cmd = "$env:WINDIR\system32\reg.exe import $PSScriptRoot\registry.reg"
 
 if ([System.IntPtr]::Size -ne 4) {
