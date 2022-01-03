3098eb6cd12ec75e4654b9b4308efc36b911ce93
Julian Easterling <julian@julianscorner.com>
2022-01-03 03:10:50 -0500

    Updated the way the nerd fonts are downloaded

--- mysettings-console/tools/chocolateyInstall.ps1
+++ mysettings-console/tools/chocolateyInstall.ps1
@@ -7,11 +7,13 @@ if ([System.IntPtr]::Size -ne 4) {
 cmd /c "$cmd"
 
 # Install My "Nerd" Font
-$font = "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/CascadiaCode/Regular/complete/Caskaydia%20Cove%20Regular%20Nerd%20Font%20Complete%20Mono%20Windows%20Compatible.otf"
+@("Bold", "ExtraLight", "Light", "Regular", "SemiBold", "SemiLight") | ForEach-Object {
+  $font = "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/CascadiaCode/$_/complete/Caskaydia%20Cove%20$_%20Nerd%20Font%20Complete%20Mono%20Windows%20Compatible.otf"
 
-Invoke-WebRequest -Uri $font `
-    -OutFile "$env:TEMP\Caskaydia Cove Regular Nerd Font Complete Mono Windows Compatible.otf"
+  Invoke-WebRequest -Uri $font `
+    -OutFile "$env:TEMP\Caskaydia Cove $_ Nerd Font Complete Windows Compatible.otf"
 
-Install-Font -Path "$env:TEMP\Caskaydia Cove Regular Nerd Font Complete Mono Windows Compatible.otf"
+  Install-Font -Path "$env:TEMP\Caskaydia Cove $_ Nerd Font Complete Windows Compatible.otf"
 
-Remove-Item -Path "$env:TEMP\Caskaydia Cove Regular Nerd Font Complete Mono Windows Compatible.otf" -Force
+  Remove-Item -Path "$env:TEMP\Caskaydia Cove $_ Nerd Font Complete Windows Compatible.otf" -Force
+}
