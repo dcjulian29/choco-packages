79183efbb6a573a7415777935ebeec1e8e42b1f9
Julian Easterling <julian@julianscorner.com>
2022-01-04 13:32:14 -0500

    Added AngryIP Scanner as a portable app that requires AdoptOpenJDK instead of Oracle

---
+++ angryip/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$version = "${env:ChocolateyPackageVersion}"
+$url = "https://github.com/angryip/ipscan/releases/download/$version/ipscan-win64-$version.exe"
+$app = "${env:ChocolateyInstall}\bin\ipscan.exe"
+
+if (Test-Path $app) {
+    Write-Output "Removing previous version of package..."
+    Remove-Item -Path $app -Force
+}
+
+Invoke-WebRequest -Uri $url -OutFile $app -UseBasicParsing
