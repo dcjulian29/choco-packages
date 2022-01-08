ca1ba3c5ee8be2e6d5db88f6104ab4c3dd96434a
Julian Easterling <julian@julianscorner.com>
2022-01-08 10:37:31 -0500

    Added an addition Powershell module to database dev package

--- mytools-database/tools/chocolateyInstall.ps1
+++ mytools-database/tools/chocolateyInstall.ps1
@@ -3,3 +3,4 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 
 Install-Module SqlServer -Force -AllowClobber
+Install-Module -Name dbatools
