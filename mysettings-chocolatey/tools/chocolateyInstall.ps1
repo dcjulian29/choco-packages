bf2c92f74df1ca33ae1a64e6c22049267136dae4
Julian Easterling <julian@julianscorner.com>
2022-01-07 23:41:20 -0500

    Needed to change the way the directory is listed

--- mysettings-chocolatey/tools/chocolateyInstall.ps1
+++ mysettings-chocolatey/tools/chocolateyInstall.ps1
@@ -8,5 +8,5 @@ choco feature enable  -n showNonElevatedWarnings
 choco feature enable  -n showDownloadProgress
 choco feature enable  -n stopOnFirstPackageFailure
 
-choco config set --name 'cacheLocation' --value '${env:TEMP}'
+choco config set --name 'cacheLocation' --value '%TEMP%'
 choco config set --name 'commandExecutionTimeoutSeconds' --value '3600'
