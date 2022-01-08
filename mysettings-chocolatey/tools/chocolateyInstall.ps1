1597a1278f67d138626c42d9a77fdddb72da89c7
Julian Easterling <julian@julianscorner.com>
2022-01-07 23:13:09 -0500

    Updated my chocolatey settings

--- mysettings-chocolatey/tools/chocolateyInstall.ps1
+++ mysettings-chocolatey/tools/chocolateyInstall.ps1
@@ -1,10 +1,12 @@
 icacls "${env:ALLUSERSPROFILE}\chocolatey" --% /grant Everyone:(OI)(CI)F /T
 
-choco feature enable -n allowEmptyChecksums
-choco feature enable -n logWithoutColor
-
+choco feature disable -n allowEmptyChecksums
 choco feature disable -n exitOnRebootDetected
-choco feature disable -n showNonElevatedWarnings
-choco feature disable -n showDownloadProgress
+choco feature enable  -n logWithoutColor
+choco feature disable -n powershellhost
+choco feature enable  -n showNonElevatedWarnings
+choco feature enable  -n showDownloadProgress
+choco feature enable  -n stopOnFirstPackageFailure
 
+choco config set --name 'cacheLocation' --value '${env:TEMP}'
 choco config set --name 'commandExecutionTimeoutSeconds' --value '3600'
