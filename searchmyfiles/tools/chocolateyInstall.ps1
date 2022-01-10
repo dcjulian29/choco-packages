cbffbccc152f4972de545fa6d81d8715c536c7c3
Julian Easterling <julian@julianscorner.com>
2022-01-10 18:19:43 -0500

    Updated searchmyfiles package

--- searchmyfiles/tools/chocolateyInstall.ps1
+++ searchmyfiles/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 $url = 'http://www.nirsoft.net/utils/searchmyfiles.zip'
-$cksum = '8aea45db108be6201c7999a550ec882c6f6837c197fef40172bc7261610383c6'
+$cksum = '3ba8f93dc0430e8eb4c6a75eeb66ce167b588dcd4dfddcd81bf3c547abaf5344'
 
 if ([Environment]::Is64BitOperatingSystem) {
     $url = 'http://www.nirsoft.net/utils/searchmyfiles-x64.zip'
-    $cksum = '80bd0878000409248172a94c653c538cebc627db50ad369434c11240b7700ee6'
+    $cksum = '6e00a6b765da99a5b48e79b5d4677bbdd863c723897a96cb005ff13a53b7ce86'
 }
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
