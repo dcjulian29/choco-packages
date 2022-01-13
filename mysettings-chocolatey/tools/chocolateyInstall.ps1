8766a422d28072220e3514360069f467a0ff8530
Julian Easterling <julian@julianscorner.com>
2022-01-13 11:03:47 -0500

    Ugh, running some of my scripts "inside" chocolatey is really trying my nerves. Maybe time for me to finally switch to winget properly.

--- mysettings-chocolatey/tools/chocolateyInstall.ps1
+++ mysettings-chocolatey/tools/chocolateyInstall.ps1
@@ -1,11 +1,9 @@
-icacls "${env:ALLUSERSPROFILE}\chocolatey" --% /grant Everyone:(OI)(CI)F /T
+#icacls "${env:ALLUSERSPROFILE}\chocolatey" --% /grant Everyone:(OI)(CI)F /T
 
 choco feature disable -n allowEmptyChecksums
 choco feature disable -n exitOnRebootDetected
 choco feature enable  -n logWithoutColor
-choco feature disable -n powershellhost
 choco feature enable  -n showNonElevatedWarnings
-choco feature enable  -n showDownloadProgress
 choco feature enable  -n stopOnFirstPackageFailure
 
 choco config set --name 'cacheLocation' --value '%TEMP%'
