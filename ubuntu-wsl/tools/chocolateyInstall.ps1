c894957585cad47c08885bbcc7d28cf4ed96a062
Julian Easterling <julian@julianscorner.com>
2022-03-03 13:26:41 -0500

    The download link is pointed to a bundle which contains both ARM and x64... If I get that version, I need to extract the x64 package out of that instead of what it used to be, just the x64 package.

--- ubuntu-wsl/tools/chocolateyInstall.ps1
+++ ubuntu-wsl/tools/chocolateyInstall.ps1
@@ -1,31 +1,50 @@
 if (-not (Test-Path $env:SYSTEMDRIVE\Ubuntu)) {
-    Write-Output "Downloading and installing Ubuntu 20.04 ..."
+  Write-Output "Downloading and installing Ubuntu 20.04 ..."
 
-    if (Test-Path $env:TEMP\ubuntu.appx) {
-        Remove-Item -Path $env:TEMP\ubuntu.appx -Force | Out-Null
+  New-Item -Type Directory -Path $env:SYSTEMDRIVE\Ubuntu | Out-Null
+
+  if (Test-Path $env:TEMP\ubuntu.zip) {
+    Remove-Item -Path $env:TEMP\ubuntu.zip -Force | Out-Null
+  }
+
+  if (Test-Path $env:TEMP\ubuntu) {
+    Remove-Item -Path $env:TEMP\ubuntu -Recurse -Force | Out-Null
+  }
+
+  Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 `
+    -OutFile $env:TEMP\Ubuntu.zip -UseBasicParsing
+
+  Expand-Archive $env:TEMP\Ubuntu.zip $env:TEMP\Ubuntu\
+
+  # Recently, the download link is an APPX bundle instead of the APPX file.
+  if (Test-Path "${env:TEMP}\Ubuntu\ubuntu2004.exe") {
+    Move-Item $env:TEMP\Ubuntu\* $env:SYSTEMDRIVE\Ubuntu
+  } else {
+    $package = (Get-ChildItem -Path $env:TEMP\Ubuntu\ `
+        | Where-Object { $_.Name -match "^Ubuntu.+_x64\.appx$" } `
+        | Sort-Object Name -Descending `
+        | Select-Object -First 1).FullName
+
+    Rename-Item $package $env:TEMP\Ubuntu\Ubuntu.zip
+
+    Expand-Archive $env:TEMP\Ubuntu\Ubuntu.zip $env:SYSTEMDRIVE\Ubuntu
+
+    if (-not (Test-Path "${env:TEMP}\Ubuntu\ubuntu2004.exe")) {
+      New-Item -ItemType SymbolicLink -Path "${env:SYSTEMDRIVE}\Ubuntu\ubuntu2004.exe" `
+        -Value "${env:SYSTEMDRIVE}\Ubuntu\ubuntu.exe" | Out-Null
     }
+  }
 
-    if (Test-Path $env:TEMP\ubuntu.zip) {
-        Remove-Item -Path $env:TEMP\ubuntu.zip -Force | Out-Null
-    }
-
-    Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile $env:TEMP\Ubuntu.appx -UseBasicParsing
-
-    Rename-Item $env:TEMP\Ubuntu.appx $env:TEMP\Ubuntu.zip
-
-    New-Item -Type Directory -Path $env:SYSTEMDRIVE\Ubuntu | Out-Null
-
-    Expand-Archive $env:TEMP\Ubuntu.zip $env:SYSTEMDRIVE\Ubuntu
-
-    Set-Content $env:SYSTEMDRIVE\Ubuntu\desktop.ini @"
-    [.ShellClassInfo]
-    IconResource=$env:SYSTEMDRIVE\Ubuntu\ubuntu2004.exe,0
+  Set-Content $env:SYSTEMDRIVE\Ubuntu\desktop.ini @"
+[.ShellClassInfo]
+IconResource=$env:SYSTEMDRIVE\Ubuntu\ubuntu2004.exe,0
 "@
 
-    attrib +S +H $env:SYSTEMDRIVE\Ubuntu\desktop.ini
-    attrib +S $env:SYSTEMDRIVE\Ubuntu
+  attrib +S +H $env:SYSTEMDRIVE\Ubuntu\desktop.ini
+  attrib +S $env:SYSTEMDRIVE\Ubuntu
 
-    Remove-Item -Path $env:TEMP\ubuntu.zip -Force | Out-Null
+  Remove-Item -Path $env:TEMP\ubuntu.zip -Force | Out-Null
+  Remove-Item -Path $env:TEMP\ubuntu -Recurse -Force | Out-Null
 } else {
-    Write-Output "A version of Ubuntu is already installed. Not overwriting the installed version..."
+  Write-Output "A version of Ubuntu is already installed. Not overwriting the installed version..."
 }
