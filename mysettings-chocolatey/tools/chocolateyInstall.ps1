90ce32b4ccb27f834e70fc2d697d9c1c3b32e0fa
Julian Easterling <julian@julianscorner.com>
2022-04-20 11:41:27 -0400

    Don't set to empty value, unset it. makes perfect sense...

--- mysettings-chocolatey/tools/chocolateyInstall.ps1
+++ mysettings-chocolatey/tools/chocolateyInstall.ps1
@@ -4,10 +4,10 @@ choco feature disable -n logWithoutColor
 choco feature disable -n logEnvironmentValues
 choco feature enable  -n showNonElevatedWarnings
 choco feature disable -n showDownloadProgress
-choco feature enable  -n useRememberedArgumentsForUpgrades 
-choco feature enable  -n logValidationResultsOnWarnings 
+choco feature enable  -n useRememberedArgumentsForUpgrades
+choco feature enable  -n logValidationResultsOnWarnings
 
-choco feature enable  -n autoUninstaller 
+choco feature enable  -n autoUninstaller
 choco feature disable -n failOnAutoUninstaller
 
 choco feature disable -n usePackageExitCodes
@@ -28,22 +28,21 @@ choco feature disable -n allowEmptyChecksumsSecure
 choco feature disable -n allowGlobalConfirmation
 
 choco feature disable -n powershellHost
-choco feature enable  -n scriptsCheckLastExitCode
 choco feature enable  -n removePackageInformationOnUninstall
 
 ##############################################################################
 
-choco config set --name 'cacheLocation' --value '%TEMP%'
-choco config set --name 'upgradeAllExceptions' --value ''
-choco config set --name 'defaultTemplateName' --value ''
+choco config set   --name "cacheLocation" --value "%TEMP%"
+choco config unset --name "upgradeAllExceptions"
+choco config set   --name "defaultTemplateName"
 
-choco config set --name 'proxy' --value ''
-choco config set --name 'proxyUser' --value ''
-choco config set --name 'proxyPassword' --value ''
-choco config set --name 'proxyBypassList' --value ''
-choco config set --name 'proxyBypassOnLocal' --value 'true'
+choco config unset --name "proxy"
+choco config unset --name "proxyUser"
+choco config unset --name "proxyPassword"
+choco config unset --name "proxyBypassList"
+choco config set   --name "proxyBypassOnLocal" --value "true"
 
-choco config set --name 'commandExecutionTimeoutSeconds' --value '3600'
-choco config set --name 'webRequestTimeoutSeconds ' --value '60'
+choco config set   --name "commandExecutionTimeoutSeconds" --value "3600"
+choco config set   --name "webRequestTimeoutSeconds " --value "60"
 
-choco config set --name 'containsLegacyPackageInstalls' --value 'false'
+choco config set   --name "containsLegacyPackageInstalls" --value "false"
