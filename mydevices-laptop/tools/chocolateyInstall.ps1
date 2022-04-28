c9024043331aaec750241ef49b6a1783608dabc0
Julian Easterling <julian@julianscorner.com>
2022-04-28 11:45:57 -0400

    Extracted Hyper-V post configuration to its own package and updated device specific packages

--- mydevices-laptop/tools/chocolateyInstall.ps1
+++
@@ -1,3 +0,0 @@
-New-LabVMSwitch
-Set-VMHost -VirtualMachinePath 'C:\Virtual Machines'
-Set-VMHost -VirtualHardDiskPath 'C:\Virtual Machines\Discs'
---
+++ mysettings-hyperv/tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+New-LabVMSwitch
+
+Set-VMHost -VirtualMachinePath 'C:\Virtual Machines\Hyper-V'
+Set-VMHost -VirtualHardDiskPath 'C:\Virtual Machines\Hyper-V\Discs'
