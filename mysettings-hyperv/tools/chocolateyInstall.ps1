71a4b61b0273d5dc5eba7b745f16fbd7c55ccf93
Julian Easterling <julian@julianscorner.com>
2022-04-30 23:15:20 -0400

    Added my Favorite Folder shortcuts for Hyper-V

--- mysettings-hyperv/tools/chocolateyInstall.ps1
+++ mysettings-hyperv/tools/chocolateyInstall.ps1
@@ -1,4 +1,13 @@
 New-LabVMSwitch
 
-Set-VMHost -VirtualMachinePath 'C:\Virtual Machines\Hyper-V'
-Set-VMHost -VirtualHardDiskPath 'C:\Virtual Machines\Hyper-V\Discs'
+if (-not (Test-Path "$env:SystemDrive\Virtual Machines\Hyper-V")) {
+  New-Item -Path "$env:SystemDrive\Virtual Machines\Hyper-V" -ItemType Directory | Out-Null
+}
+
+Set-VMHost -VirtualMachinePath "$env:SystemDrive\Virtual Machines\Hyper-V"
+Set-VMHost -VirtualHardDiskPath "$env:SystemDrive\Virtual Machines\Hyper-V\Discs"
+
+Import-Module "${env:USERPROFILE}\Documents\WindowsPowerShell\Modules\go\go.psm1"
+
+gd -Key "hyperv" -delete
+gd -Key "hyperv" -SelectedPath "$env:SystemDrive\Virtual Machines\Hyper-V" -add
