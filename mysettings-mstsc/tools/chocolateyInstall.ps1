4b68921ddc65804dee12b76e7fff750832c37523
Julian Easterling <julian@julianscorner.com>
2022-06-24 12:41:29 -0400

    Another attempt at DPI scaling with MSTSC

--- mysettings-mstsc/tools/chocolateyInstall.ps1
+++ mysettings-mstsc/tools/chocolateyInstall.ps1
@@ -1,13 +1,17 @@
-$cmd = "$env:WINDIR\system32\reg.exe import '$PSScriptRoot\registry.reg'"
+Push-Location $PSScriptRoot
 
-if (Get-ProcessorBits -eq 64) {
+$cmd = "$env:WINDIR\system32\reg.exe import registry.reg"
+
+if ([Environment]::Is64BitOperatingSystem) {
     $cmd = "$cmd /reg:64"
 }
 
 cmd /c "$cmd"
 
-Copy-Item -Path "$PSScriptRoot\mstsc.exe.manifest" `
+Copy-Item -Path "mstsc.exe.manifest" `
     -Destination "$env:SystemRoot\System32\mstsc.exe.manifest" `
     -Force
 
 Write-Output "You need to reboot to complete the action..."
+
+Pop-Location
