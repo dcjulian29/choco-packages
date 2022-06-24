c88eaf4f727d99dedda24d58c6159ba75b6b70af
Julian Easterling <julian@julianscorner.com>
2022-06-24 12:45:28 -0400

    For now, I'll comment out this as Docker Desktop isn't playing nice...

--- mytools-containers/tools/chocolateyInstall.ps1
+++ mytools-containers/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-Get-Process | Where-Object { $_.ProcessName -eq "Docker Desktop" } | Stop-Process -Force
+# Get-Process | Where-Object { $_.ProcessName -eq "Docker Desktop" } | Stop-Process -Force
 
-# I accept the Docker Desktop Agreement and License
-& '$($env:ProgramFiles)\Docker\Docker\Docker Desktop.exe' -AcceptLicense
+# # I accept the Docker Desktop Agreement and License
+# & '$($env:ProgramFiles)\Docker\Docker\Docker Desktop.exe' -AcceptLicense
