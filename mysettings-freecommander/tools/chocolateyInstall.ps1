fd01ba4f901e1134fdb4b57971c0d78e5fc9124e
Julian Easterling <julian@julianscorner.com>
2022-08-09 21:01:52 -0400

    Updated package to include override directory for FreeCommander configuration specific to one machine.

--- mysettings-freecommander/tools/chocolateyInstall.ps1
+++ mysettings-freecommander/tools/chocolateyInstall.ps1
@@ -1,10 +1,17 @@
 if (-not (Test-Path "$env:LocalAppData\FreeCommanderXE\Settings")) {
-    if (-not (Test-Path "$env:LocalAppData\FreeCommanderXE")) {
-        New-Item -Path "$env:LocalAppData\FreeCommanderXE" -ItemType Directory | Out-Null
-    }
+  if (-not (Test-Path "$env:LocalAppData\FreeCommanderXE")) {
+    New-Item -Path "$env:LocalAppData\FreeCommanderXE" -ItemType Directory | Out-Null
+  }
 
-    New-Item -Path "$env:LocalAppData\FreeCommanderXE\Settings" -ItemType Directory | Out-Null
+  New-Item -Path "$env:LocalAppData\FreeCommanderXE\Settings" -ItemType Directory | Out-Null
 }
 
 Copy-Item -Path "$PSScriptRoot\..\content\*" `
-    -Destination "$env:LocalAppData\FreeCommanderXE\Settings" -Recurse -Force
+  -Destination "$env:LocalAppData\FreeCommanderXE\Settings" -Recurse -Force
+
+$files = Get-ChildItem -Path "$env:SystemDrive\etc" -Filter "FreeCommander.*"
+
+if ($files.Count -gt 0) {
+  Copy-Item -Path "$env:SystemDrive\etc\FreeCommander.*" `
+    -Destination "$env:LocalAppData\FreeCommanderXE\Settings" -Force
+}
