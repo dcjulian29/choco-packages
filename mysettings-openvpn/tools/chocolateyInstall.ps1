d984aad587e1bc5405f4e405bc99fc6be36bd3a4
Julian Easterling <julian@julianscorner.com>
2022-08-31 16:18:14 -0400

    Switched to WireGuard and a completely different deployment method.

--- mysettings-openvpn/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-$packageName = "mysettings-openvpn"
-
-if (Test-Path $env:SYSTEMDRIVE\etc\vpn) {
-    Copy-Item -Path $env:SYSTEMDRIVE\etc\vpn\*.ovpn `
-        -Destination $env:USERPROFILE\OpenVPN\config\ `
-        -Verbose -Force
-}
