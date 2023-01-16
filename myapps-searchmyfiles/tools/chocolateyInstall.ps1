f6261aa7eaffa802a75fff73e6874da16d0931ac
Julian Easterling <julian@julianscorner.com>
2023-01-15 23:07:50 -0500

    Updated SearchMyFiles to 3.23

--- myapps-searchmyfiles/tools/chocolateyInstall.ps1
+++ myapps-searchmyfiles/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ if (Test-Path "../../searchmyfiles-julian") {
 }
 
 $url = 'http://www.nirsoft.net/utils/searchmyfiles.zip'
-$cksum = '77aa5b1d89b0fc7173a71d09acf3aaf9892c972da0a34fd17aed8ae6b979c3a1'
+$cksum = '2450b31656beb3599e96f652e9e9b787dc3eff1fa91342840368d0e468b87be0'
 
 if ([Environment]::Is64BitOperatingSystem) {
     $url = 'http://www.nirsoft.net/utils/searchmyfiles-x64.zip'
-    $cksum = 'c2dfccf2f68ab90d7aef81d8272ab79231abab8300e8f1159d4011f840d76c8e'
+    $cksum = 'ebe9220d7fe319d61552ab30eae9dee0d18e264b4a5ae172884c34a66c69ad57'
 }
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
