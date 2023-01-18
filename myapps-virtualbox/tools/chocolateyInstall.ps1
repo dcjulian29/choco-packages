58ad37eb3639578ff6093a5e90497bcfe282e7bf
Julian Easterling <julian@julianscorner.com>
2023-01-17 20:33:15 -0500

    Updated Virtualbox to 7.0.6

--- myapps-virtualbox/tools/chocolateyInstall.ps1
+++ myapps-virtualbox/tools/chocolateyInstall.ps1
@@ -19,11 +19,28 @@ Push-Location $env:TEMP
 
 Invoke-WebRequest -Uri $url -OutFile "virtualbox.exe"
 
+# For some stupid reason, Oracle causes the first attempt to fail randomly...
+if (Test-Path -Path "./virtualbox.exe") {
+  # Download package have been over 75MB for a long time so if not, re-download.
+  if ((Get-Item ".\virtualbox.exe").Length - lt 78643200)) {
+    Remove-Item -Path "./virtualbox.exe" -Force
+    Invoke-WebRequest -Uri $url -OutFile "virtualbox.exe"
+  }
+}
+
 if (-not (Test-Path -Path "./virtualbox.exe")) {
   throw "Error downloading virtualbox!"
 }
 
+if ((Get-Item ".\virtualbox.exe").Length - lt 78643200)) {
+  throw "Downloaded package is not big enough!"
+}
+
 Invoke-Expression "./virtualbox.exe -extract -silent"
+
+if (-not (Test-Path -Path "${env:TEMP}\VirtualBox-$version-r$build.msi")) {
+  throw "Error extracting virtualbox installer from package!"
+}
 
 Invoke-Expression "msiexec.exe $($props -join ' ')"
 
