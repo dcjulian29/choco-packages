cf308f6f7cdf4f67374fa09b3b6d22f5adb6e8aa
Julian Easterling <julian@julianscorner.com>
2023-02-07 13:44:48 -0500

    Updated my WSL settings to take in latest way to install

--- mysettings-wsl2/tools/chocolateyInstall.ps1
+++ mysettings-wsl2/tools/chocolateyInstall.ps1
@@ -1,22 +1,26 @@
 $wsl = (Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux).State -eq "Enabled"
 
 if (-not $wsl) {
-    throw "WSL feature is not enabled"
+  throw "WSL feature is not enabled"
 }
 
 if ([System.Environment]::OSVersion.Version.Build -ge 19041) {
-    $vmp = (Get-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform).State -eq "Enabled"
+  $vmp = (Get-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform).State -eq "Enabled"
 
-    if (-not $vmp) {
-        throw "Virtual Machine Platform feature is not enabled"
-    }
+  if (-not $vmp) {
+    throw "Virtual Machine Platform feature is not enabled"
+  }
 
+  if ([System.Environment]::OSVersion.Version.Build -ge 22621) {
+    & wsl.exe --install
+  } else {
     Write-Output "Downloading WSL 2 Kernel Update..."
     Invoke-WebRequest -Uri "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi" `
         -OutFile "$env:TEMP\wsl_update_x64.msi"
 
     $logWslFile = Get-LogFileName -Date $(Get-Date) -Suffix "$env:COMPUTERNAME-wsl2kernel"
     Start-Process "$env:TEMP\wsl_update_x64.msi" "/passive /norestart /log $logWslFile" -Wait
+  }
 
-    & wsl.exe --set-default-version 2
+  & wsl.exe --set-default-version 2
 }
