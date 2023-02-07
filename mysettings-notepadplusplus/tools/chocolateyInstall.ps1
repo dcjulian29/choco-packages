a0fbb19c74be357cca3fd243fda2c571f16034dd
Julian Easterling <julian@julianscorner.com>
2023-02-07 14:40:05 -0500

    No need to update the style configuration

--- mysettings-notepadplusplus/tools/chocolateyInstall.ps1
+++ mysettings-notepadplusplus/tools/chocolateyInstall.ps1
@@ -1,26 +1,13 @@
-$packageName = "mysettings-notepadplusplus"
-$toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Path)"
+$appdata = "${env:APPDATA}\Notepad++"
 
-$appdata = "$($env:APPDATA)\Notepad++"
-
-if (-not (Test-Path $appdata))
-{
-    New-Item -Type Directory -Path $appdata | Out-Null
+if (-not (Test-Path $appdata)) {
+  New-Item -Type Directory -Path $appdata | Out-Null
 }
 
-Copy-Item -Path "$($toolDir)\config.xml" -Destination "$($appdata)\"
+Copy-Item -Path "$PSScriptRoot\config.xml" -Destination "$appdata\config.xml" -Force
 
-if (-not (Test-Path "$($appdata)\themes"))
-{
+if (-not (Test-Path "$appdata\themes")) {
     New-Item -Type Directory -Path "$appdata\themes" | Out-Null
 }
 
-Copy-Item -Path "$($toolDir)\MyObsidian.xml" -Destination "$($appdata)\themes\"
-
-$config = [xml](Get-Content "$($appdata)\config.xml")
-
-$settings = $config.NotepadPlus.GUIConfigs
-$theme = $settings.GUIConfig | where { $_.name -eq 'stylerTheme' }
-$theme.path = "$($appdata)\themes\MyObsidian.xml"
-
-$config.Save("$($appdata)\config.xml")
+Copy-Item -Path "$PSScriptRoot\MyObsidian.xml" -Destination "$appdata\themes\MyObsidian.xml" -Force
