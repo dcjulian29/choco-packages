1ef9bbf0e13ca0e76c95192e4d24c3944d5a5e67
Julian Easterling <julian@julianscorner.com>
2023-03-25 19:14:51 -0400

    irratating...

--- build.ps1
+++ build.ps1
@@ -22,7 +22,7 @@ if (Get-Command -Name "nuget") {
   }
 }
 
-$packDir = Join-Path -Path $env:TEMP -ChildPath "packages"
+$packDir = Join-Path -Path $baseDir -ChildPath ".packages"
 
 if (-not (Test-Path $packDir)) {
   New-Item -Path $packDir -ItemType Directory | Out-Null
