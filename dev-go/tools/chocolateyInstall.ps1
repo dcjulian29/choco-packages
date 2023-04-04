ebf808a26a429d12eafb5608f17609ba3e77103a
Julian Easterling <julian@julianscorner.com>
2023-04-03 22:03:37 -0400

    Added a tool to run GH actions locally

---
+++ dev-go/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+Write-Output "Installed Go Version:" 
+go version
+
+Write-Output "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`n"
+
+# Go Tool installs:
+go install github.com/spf13/cobra-cli@latest
+go install golang.org/x/lint/golint@latest
