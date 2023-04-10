8f93d9dee7f7cdef7c9aad9f37e2dbcdd2ca917e
Julian Easterling <julian@julianscorner.com>
2023-04-10 11:44:20 -0400

    Added GoReleaser tool for Go Development

--- dev-go/tools/chocolateyInstall.ps1
+++ dev-go/tools/chocolateyInstall.ps1
@@ -4,5 +4,6 @@ go version
 Write-Output "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`n"
 
 # Go Tool installs:
+go install github.com/goreleaser/goreleaser@latest
 go install github.com/spf13/cobra-cli@latest
 go install golang.org/x/lint/golint@latest
