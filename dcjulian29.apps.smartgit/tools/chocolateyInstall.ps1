c7f7d7b1bc4b92d8142d2723aa9d8477cd9ddab7
Julian Easterling <julian@julianscorner.com>
2023-04-30 11:59:53 -0400

    Really hate companies who prevent downloading a file to install their software but instead will only show the HTML to download it.

--- dcjulian29.apps.smartgit/tools/chocolateyInstall.ps1
+++ dcjulian29.apps.smartgit/tools/chocolateyInstall.ps1
@@ -2,12 +2,13 @@ $ErrorActionPreference = 'Stop';
 
 $version = $env:chocolateyPackageVersion -replace '\.', '_'
 $packageName = $env:chocolateyPackageName
+$url = "https://julianscorner.com/dl/smartgit-win-$version.zip"
 
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $PSScriptRoot
   fileType       = "exe"
-  url64bit       = "https://www.syntevo.com/downloads/smartgit/smartgit-win-$version.zip"
+  url64bit       = $url
   checksum64     = "748e293d243566a02fc43f75dcea88739709cd67fcc3b7adcd97b18e2a7d1455"
   checksumType64 = "sha256"
 }
