d7d2a1739feb0acf8b3b5f523f4770c230222e3f
Julian Easterling <julian@julianscorner.com>
2023-04-29 21:46:36 -0400

    The community package is very outdated and doesn't seem to work at all so I'll do it my way

---
+++ dcjulian29.apps.smartgit/tools/chocolateyInstall.ps1
@@ -0,0 +1,25 @@
+$ErrorActionPreference = 'Stop';
+
+$version = $env:chocolateyPackageVersion.Replace('.', '_')
+$packageName = $env:chocolateyPackageName
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $PSScriptRoot
+  fileType       = 'exe'
+  url64bit       = 'https://www.syntevo.com/downloads/smartgit/smartgit-win-$version.zip'
+  checksum64     = '748e293d243566a02fc43f75dcea88739709cd67fcc3b7adcd97b18e2a7d1455'
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$installArgs = @{
+  packageName    = $packageName
+  file           = $(Join-Path -Path $PSScriptRoot -ChildPath "smartgit-$version-setup.exe")
+
+  silentArgs     = '/SILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyInstallPackage @installArgs
