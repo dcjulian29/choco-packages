2454c693db45cee8a26c13d84a17d5e52e6ef145
Julian Easterling <julian@julianscorner.com>
2023-06-06 17:41:59 -0400

    Updated SHA1 for devpod and removed keepass package

--- dcjulian29.apps.devpod/tools/chocolateyInstall.ps1
+++ dcjulian29.apps.devpod/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   unzipLocation  = $PSScriptRoot
   fileType       = "msi"
   url64bit       = "https://github.com/loft-sh/devpod/releases/download/v$version/DevPod_windows_x64_en-US.msi"
-  checksum64     = "748e293d243566a02fc43f75dcea88739709cd67fcc3b7adcd97b18e2a7d1455"
+  checksum64     = "DC814AB050F2270AC10D614715EC4F1E7BFB7A703583A82C412D2EAF089872C4"
   checksumType64 = "sha256"
 }
 
