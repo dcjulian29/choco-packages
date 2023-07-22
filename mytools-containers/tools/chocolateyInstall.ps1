0704fbfa50d25235ca4e9f83b77694ad85157e2a
Julian Easterling <julian@julianscorner.com>
2023-07-22 12:36:04 -0400

    Removed need for mytools-containers package

--- mytools-containers/tools/chocolateyInstall.ps1
+++
@@ -1,4 +0,0 @@
-# Get-Process | Where-Object { $_.ProcessName -eq "Docker Desktop" } | Stop-Process -Force
-
-# # I accept the Docker Desktop Agreement and License
-# & '$($env:ProgramFiles)\Docker\Docker\Docker Desktop.exe' -AcceptLicense
