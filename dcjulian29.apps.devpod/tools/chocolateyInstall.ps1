cecc12efee22e27b40d49bc55059359b01eabc8f
Julian Easterling <julian@julianscorner.com>
2023-07-27 10:52:22 -0400

    Updated DevPod to 0.3.3

--- dcjulian29.apps.devpod/tools/chocolateyInstall.ps1
+++ dcjulian29.apps.devpod/tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType       = "msi"
   silentArgs     = "/PASSIVE /NORESTART"
   url64bit       = "https://github.com/loft-sh/devpod/releases/download/v$version/DevPod_windows_x64_en-US.msi"
-  checksum64     = "59610E40D43CBE12B59D5E9DA0A8A19B1AC0480775897F6340BC2DF6E107C095"
+  checksum64     = "0459f4d8c5f079f78c4b60bc16379d3bd6826029b56139d6714a5aa2bbdb5e8d"
   checksumType64 = "sha256"
   validExitCodes = @(0)
 }
