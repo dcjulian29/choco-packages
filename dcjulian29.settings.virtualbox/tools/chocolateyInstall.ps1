e85025af96d609704ad4ce230dd2a3303f1074e1
Julian Easterling <julian@julianscorner.com>
2023-09-11 16:53:20 -0400

    Changed virtualbox VM storage location and removed extension install

--- dcjulian29.settings.virtualbox/tools/chocolateyInstall.ps1
+++ dcjulian29.settings.virtualbox/tools/chocolateyInstall.ps1
@@ -5,37 +5,11 @@ if (-not (Test-Path $vbox)) {
   throw "VBoxManage.exe not found!"
 }
 
-$version = (& "$vbox" --version).Split('r')[0]
-$build = (& "$vbox" --version).Split('r')[1]
-
-Write-Output "Downloading extension pack $version-$build..."
-$url = "https://download.virtualbox.org/virtualbox/$version/" `
-  + "Oracle_VM_VirtualBox_Extension_Pack-$version-$build.vbox-extpack"
-$file = "$env:TEMP\Oracle_VM_VirtualBox_Extension_Pack-$version.vbox-extpack"
-
-Remove-Item -Path $file -Force -ErrorAction SilentlyContinue
-Download-File -Url $url -Destination $file
-
-$sha = "33d7284dc4a0ece381196fda3cfe2ed0e1e8e7ed7f27b9a9ebc4ee22e24bd23c"
-
-& "$vbox" extpack install --accept-license=$sha --replace $file
-
-if ($LastExitCode -ne 0) {
-  throw "Extension pack installation failed with exit code '$LastExitCode'"
-}
-
-& "$vbox" extpack cleanup
-
-Remove-Item -Path $file -Force
-
 Write-Output "Changing default VM location..."
+$machine_folder = "${env:USERPROFILE}\.virtualbox"
 
-if (-not (Test-Path "$env:SystemDrive\Virtual Machines\VirtualBox")) {
-  New-Item -Path "$env:SystemDrive\Virtual Machines\VirtualBox" -ItemType Directory | Out-Null
+if (-not (Test-Path -Path $machine_folder)) {
+  New-Item -Path $machine_folder -ItemType Directory | Out-Null
 }
 
-& "$vbox" setproperty machinefolder "$env:SystemDrive\Virtual Machines\VirtualBox"
-
-if (-not $(Get-FavoriteFolder -Key virtualbox)) {
-  Add-FavoriteFolder -Key "virtualbox" -Path "$env:SystemDrive\Virtual Machines\VirtualBox"
-}
+& "$vbox" setproperty machinefolder $machine_folder
