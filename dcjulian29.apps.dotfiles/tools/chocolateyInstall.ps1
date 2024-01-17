448d1a06b6d1d3bdd8277fa41a653663f7774a23
Julian Easterling <julian@julianscorner.com>
2024-01-16 21:10:42 -0500

    Another attempt to get Chocolatey to behave as expected

--- dcjulian29.apps.dotfiles/tools/chocolateyInstall.ps1
+++ dcjulian29.apps.dotfiles/tools/chocolateyInstall.ps1
@@ -1,13 +1,17 @@
 $ErrorActionPreference = 'Stop'
 
 $version = $env:chocolateyPackageVersion
+$packageName = $env:chocolateyPackageName
 $url = "https://github.com/rhysd/dotfiles/releases/download/v$version/dotfiles_windows_amd64.exe.zip"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-ChocolateyZipPackage @{
-  packageName    = $env:chocolateyPackageName
-  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
   fileType       = 'exe'
   url64bit       = $url
   checksum64     = '5ff598bcb2eafbb2146b8d420d8e4fb08be892fb09fdab2007614390025a8f00'
   checksumType64 = 'sha256'
 }
+
+Install-ChocolateyZipPackage @packageArgs
