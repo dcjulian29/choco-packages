f7f749910878a360e26c624d67e2239daaf12759
Julian Easterling <julian@julianscorner.com>
2024-01-16 21:29:04 -0500

    Need to pass the hashtable as varible instead of directlry as chocolatey picks up a "1" for the unziplocation otherwise.

--- dcjulian29.apps.dive/tools/chocolateyInstall.ps1
+++ dcjulian29.apps.dive/tools/chocolateyInstall.ps1
@@ -1,13 +1,17 @@
 $ErrorActionPreference = 'Stop'
 
 $version = $env:chocolateyPackageVersion
+$packageName = $env:chocolateyPackageName
 $url = "https://github.com/wagoodman/dive/releases/download/v$version/dive_$($version)_windows_amd64.zip"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-ChocolateyZipPackage @{
-  packageName    = $env:chocolateyPackageName
-  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
   fileType       = 'exe'
   url64bit       = $url
   checksum64     = '6f8680c8d906cbd7ef4df3ec7cea90eece95bec45083b8ba6ea1d8a50d645667'
   checksumType64 = 'sha256'
 }
+
+Install-ChocolateyZipPackage @packageArgs
