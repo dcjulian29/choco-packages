6d1907fdd6d0b201ea457a74c74400a909eb3673
Julian Easterling <julian@julianscorner.com>
2024-01-16 19:15:54 -0500

    Updated the Unzip location

--- dcjulian29.apps.dive/tools/chocolateyInstall.ps1
+++ dcjulian29.apps.dive/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
-$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop'
 
 $version = $env:chocolateyPackageVersion
 $url = "https://github.com/wagoodman/dive/releases/download/v$version/dive_$($version)_windows_amd64.zip"
 
 Install-ChocolateyZipPackage @{
   packageName    = $env:chocolateyPackageName
-  unzipLocation  = $PSScriptRoot
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'exe'
   url64bit       = $url
   checksum64     = '6f8680c8d906cbd7ef4df3ec7cea90eece95bec45083b8ba6ea1d8a50d645667'
--- dcjulian29.apps.dotfiles/tools/chocolateyInstall.ps1
+++ dcjulian29.apps.dotfiles/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
-$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop'
 
 $version = $env:chocolateyPackageVersion
 $url = "https://github.com/rhysd/dotfiles/releases/download/v$version/dotfiles_windows_amd64.exe.zip"
 
 Install-ChocolateyZipPackage @{
   packageName    = $env:chocolateyPackageName
-  unzipLocation  = $PSScriptRoot
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'exe'
   url64bit       = $url
   checksum64     = '5ff598bcb2eafbb2146b8d420d8e4fb08be892fb09fdab2007614390025a8f00'
