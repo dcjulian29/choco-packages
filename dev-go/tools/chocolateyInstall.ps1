a6b8f358ff7e0aed4c9fbf9136d7d232e9357d3f
Julian Easterling <julian@julianscorner.com>
2024-04-13 10:13:03 -0400

    No longer using a separate VM for development

--- dev-go/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-Write-Output "Installed Go Version:" 
-go version
-
-Write-Output "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`n"
-
-# Go Tool installs:
-go install github.com/goreleaser/goreleaser@latest
-go install github.com/spf13/cobra-cli@latest
-go install golang.org/x/lint/golint@latest
