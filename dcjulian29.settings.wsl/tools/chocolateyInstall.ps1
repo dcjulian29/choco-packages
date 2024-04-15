87641272a94e1daa7f2494b82bcccc5b19dda3a1
Julian Easterling <julian@julianscorner.com>
2024-04-15 11:59:51 -0400

    Combined the two WSL packages that installs Ubuntu 22.04

---
+++ dcjulian29.settings.wsl/tools/chocolateyInstall.ps1
@@ -0,0 +1,57 @@
+$wsl = (Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux).State -eq "Enabled"
+
+if (-not $wsl) {
+  throw "WSL feature is not enabled"
+}
+
+if ([System.Environment]::OSVersion.Version.Build -ge 19041) {
+  $vmp = (Get-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform).State -eq "Enabled"
+
+  if (-not $vmp) {
+    throw "Virtual Machine Platform feature is not enabled"
+  }
+
+  & wsl.exe --update
+  & wsl.exe --set-default-version 2
+}
+
+$wsl = Invoke-Expression -Command "wsl.exe --list"
+
+if ($wsl.Contains("Ubuntu-22.04")) {
+  Write-Warning "Found previously registered Ubuntu 22.04 instance..."
+  Write-Output "Not overwriting the installed version..."
+} else {
+  Write-Output "Downloading and installing Ubuntu 22.04...`n"
+
+  if (Test-Path "${env:TEMP}\Ubuntu.tar.gz") {
+    Remove-Item -Path "${env:TEMP}\Ubuntu.tar.gz" -Force | Out-Null
+  }
+
+  $baseUrl = "https://cloud-images.ubuntu.com/wsl/jammy/current"
+  $url = "$baseUrl/ubuntu-jammy-wsl-amd64-wsl.rootfs.tar.gz"
+
+  Invoke-WebRequest $url -OutFile $env:TEMP\Ubuntu.tar.gz
+
+  $InstallFolder = "$($env:USERPROFILE)\AppData\Local\Packages\Ubuntu"
+
+  Invoke-Expression -Command `
+    "wsl.exe --import Ubuntu-22.04 `"$InstallFolder`" `"${env:TEMP}\Ubuntu.tar.gz`""
+
+  $exe = "wsl.exe -d Ubuntu-22.04"
+
+  Invoke-Expression -Command `
+    "$exe /usr/sbin/adduser $($env:USERNAME) --gecos `"First,Last,RoomNumber,WorkPhone,HomePhone`" --disabled-password"
+
+  Invoke-Expression -Command "$exe /usr/sbin/usermod -aG sudo $($env:USERNAME)"
+
+  Invoke-Expression -Command `
+    "$exe /bin/bash -c `"echo -e '$($env:USERNAME) ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/$($env:USERNAME)`""
+
+  Get-ItemProperty Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Lxss\*\ DistributionName `
+  | Where-Object -Property DistributionName -eq "Ubuntu-22.04" `
+  | Set-ItemProperty -Name DefaultUid -Value "1000"
+
+  & wsl.exe -d "Ubuntu-22.04" /bin/bash -c "curl -sSL https://julianscorner.com/dl/l/init-wsl.sh | bash"
+
+  & wsl.exe --setdefault "Ubuntu-22.04"
+}
