373e1bd2dbc5e3b5b8ad7f52150a729a76325b0f
Julian Easterling <julian@julianscorner.com>
2024-04-17 22:09:35 -0400

    Need to extract a specific folder inside zip

--- dcjulian29.apps.iperf/tools/chocolateyInstall.ps1
+++ dcjulian29.apps.iperf/tools/chocolateyInstall.ps1
@@ -7,6 +7,7 @@ $installArgs = @{
     checksum64     = "3c3db693c1bdcc902ca9198fc716339373658233b3392ffe3d467f7695762cd1"
     checksum64Type = "sha256"
     UnzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+    specificFolder = "iperf-${env:ChocolateyPackageVersion}-*"
 }
 
 Install-ChocolateyZipPackage @installArgs
