db947cd59cd2f45d59f7e35b7b711e752a632e68
Julian Easterling <julian@julianscorner.com>
2024-04-21 22:54:30 -0400

    Updated family computer meta package

--- dcjulian29.devices.family/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-if (Test-Path "${env:SYSTEMDRIVE}\home") {
-    Remove-Item -Path $env:SYSTEMDRIVE\home -Recurse -Force
-}
-
-if (Test-Path "${env:SYSTEMDRIVE}\tools") {
-    Remove-Item -Path $env:SYSTEMDRIVE\tools -Recurse -Force
-}
