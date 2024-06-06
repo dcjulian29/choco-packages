9c5438c12e0ac36c9cd9f446dead16ee0a8861b1
Julian Easterling <julian@julianscorner.com>
2024-06-06 13:10:12 -0400

    Updated Wireshark 4.2.5

--- dcjulian29.apps.wireshark/tools/chocolateyInstall.ps1
+++ dcjulian29.apps.wireshark/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
   PackageName    = $env:chocolateyPackageName
   FileType       = "EXE"
   SilentArgs     = "/S /EXTRACOMPONENTS=sshdump,udpdump,etwdump,wifidump /DESKTOPICON=no /QUICKLAUNCHICON=no"
-  url            = "https://2.na.dl.wireshark.org/win64/Wireshark-4.2.4-x64.exe"
-  checksum       = "b621718ffe64748590ea9568fbbed0f3d86b0939906dc9f7fe064e20ce385492"
+  url            = "https://2.na.dl.wireshark.org/win64/Wireshark-4.2.5-x64.exe"
+  checksum       = "3d921ee584d0984f694f60a771a6581a6f32a9de995a5cd4bca1931185a4e618"
   checksumType   = "sha256"
   ValidExitCodes = @(0, 3010)
 }
