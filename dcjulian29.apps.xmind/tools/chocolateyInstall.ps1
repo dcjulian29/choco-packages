7d729d6ef5a907bcb592c6e8fd339e2222abd979
Julian Easterling <julian@julianscorner.com>
2024-06-13 18:22:18 -0400

    Updated xmind to 24.04.10311

--- dcjulian29.apps.xmind/tools/chocolateyInstall.ps1
+++ dcjulian29.apps.xmind/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   fileType       = "exe"
   silentArgs     = "/allUsers /S"
   url64bit       = "https://www.xmind.app/zen/download/win64"
-  checksum64     = "586a8a952e2649f62cbb493d5c32d2e50f3625d8996e493ecd6c4afbb3b22b05"
+  checksum64     = "8d5efad51a0f466c63ba6901062cbe7958726b33c9c99a41e76f0c0d185b1162"
   checksumType64 = "sha256"
   validExitCodes = @(0)
 }
