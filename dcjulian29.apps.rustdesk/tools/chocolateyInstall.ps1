7db18bf610e875fb724b2b3fb76a46cbebf1a549
Julian Easterling <julian@julianscorner.com>
2024-06-13 18:21:24 -0400

    updated rustdesk to 1.2.3

--- dcjulian29.apps.rustdesk/tools/chocolateyInstall.ps1
+++ dcjulian29.apps.rustdesk/tools/chocolateyInstall.ps1
@@ -1,9 +1,12 @@
+$version = $env:chocolateyPackageVersion
+$sha256 = "23b661d7bc171cd500d5096456905283ffe06479582b62d3bd5066633935d43e"
+
 $installArgs = @{
   PackageName    = $env:chocolateyPackageName
   FileType       = "EXE"
   SilentArgs     = "--silent-install"
-  url            = "https://github.com/rustdesk/rustdesk/releases/download/1.2.2/rustdesk-1.2.2-x86_64.exe"
-  checksum       = "7d8790e65a906706a93734b91efa6dfdb732f9897e04707233fe48033bd5654e"
+  url            = "https://github.com/rustdesk/rustdesk/releases/download/$version/rustdesk-$version-x86_64.exe"
+  checksum       = $sha256
   checksumType   = "sha256"
   ValidExitCodes = @(0)
 }
