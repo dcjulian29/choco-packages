eed763dffc76f032f664da777c6332b363640c44
Julian Easterling <julian@julianscorner.com>
2024-11-17 21:03:54 -0500

    Added power mgmt settings

--- dcjulian29.devices.morbo/tools/chocolateyInstall.ps1
+++ dcjulian29.devices.morbo/tools/chocolateyInstall.ps1
@@ -10,3 +10,8 @@ netsh int ipv4 add excludedportrange protocol=udp startport=22000 numberofports=
 go install github.com/goreleaser/goreleaser/v2@latest
 go install github.com/spf13/cobra-cli@latest
 go install golang.org/x/lint/golint@latest
+
+Write-Output "standby never, hibernate never..."
+
+powercfg.exe /change standby-timeout-ac 0
+powercfg.exe /change hibernate-timeout-ac 0
