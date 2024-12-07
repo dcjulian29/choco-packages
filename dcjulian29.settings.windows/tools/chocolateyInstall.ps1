4320157289908172946cbf79996ca99b82e84e25
Julian Easterling <julian@julianscorner.com>
2024-12-07 18:42:58 -0500

    Updated windows settings package to run registry changes within files

--- dcjulian29.settings.windows/tools/chocolateyInstall.ps1
+++ dcjulian29.settings.windows/tools/chocolateyInstall.ps1
@@ -54,73 +54,21 @@ function installFont($Path) {
   }
 }
 
-### Console Settings
-
-$cmd = "$env:WINDIR\system32\reg.exe import $PSScriptRoot\console.reg"
+### Registry Settings
 
 if ([System.IntPtr]::Size -ne 4) {
     $cmd = "$cmd /reg:64"
 }
 
-Write-Output "Adding console settings to registry..."
+(Get-childItem -Path "."  -Filter "*.reg").FullName | ForEach-Object {
+  Write-Output "Adding to registry: $_"
+  cmd /c "$cmd $_"
+}
 
-cmd /c "$cmd"
-
-### Install My "Nerd" Font
+## Fonts
 
 (Get-ChildItem -Path "$PSScriptRoot\fonts").FullName | ForEach-Object {
   installFont $_
-}
-
-### Explorer Settings
-
-$cmd = "$env:WINDIR\system32\reg.exe import $PSScriptRoot\explorer.reg"
-
-if ([System.IntPtr]::Size -ne 4) {
-  $cmd = "$cmd /reg:64"
-}
-
-Write-Output "Adding explorer settings to registry..."
-
-cmd /c "$cmd"
-
-### IPv6 Tunnel settings
-
-$view = [Microsoft.Win32.RegistryView]::Registry32
-
-if (Get-ProcessorBits -eq 64) {
-  $view = [Microsoft.Win32.RegistryView]::Registry64
-}
-
-$key = [Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, $view)
-
-Write-Output "Disabling IPv6 tunnels..."
-
-$subKey = $key.OpenSubKey("SYSTEM\CurrentControlSet\services\TCPIP6\Parameters", $true)
-$subKey.SetValue("DisabledComponents", 1)
-
-### NTFS Settings
-
-$key = [Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, $view)
-
-Write-Output "Updating NTFS file system settings..."
-
-$subKey = $key.OpenSubKey("SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management", $true)
-$subKey.SetValue("DisablePagingExecutive", 1)
-
-$subKey = $key.OpenSubKey("SYSTEM\CurrentControlSet\Control\FileSystem", $true)
-$subKey.SetValue("NtfsDisableLastAccessUpdate", 1)
-$subKey.SetValue("NtfsDisableEncryption", 1)
-$subKey.SetValue("NtfsDisableCompression", 1)
-
-if ((Get-WmiObject win32_operatingsystem).BuildNumber -lt 10240) {
-  $key = [Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::CurrentUser, $view)
-
-  $subKey = $key.OpenSubKey("Software\Microsoft\Windows\CurrentVersion\Policies\Explorer", $true)
-  $subKey.SetValue("LinkResolveIgnoreLinkInfo", 1)
-  $subKey.SetValue("NoResolveSearch", 1)
-  $subKey.SetValue("NoResolveTrack", 1)
-  $subKey.SetValue("NoInternetOpenWith", 1)
 }
 
 ### Power Settings
@@ -137,4 +85,4 @@ powercfg.exe /change monitor-timeout-dc 15
 powercfg.exe /change standby-timeout-dc 20
 powercfg.exe /change hibernate-timeout-dc 30
 
-Write-Output "You need to reboot to complete these changex..."
+Write-Output "You need to reboot to complete these changes..."
