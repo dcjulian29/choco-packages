55865e56233e57846bc3aa11515126f6be4bba39
Julian Easterling <julian@julianscorner.com>
2024-12-09 00:12:16 -0500

    Separated Caskaydia Cove font to separate package that downloads the font, then installs it

---
+++ dcjulian29.settings.cascadiacodenerdfont/tools/chocolateyInstall.ps1
@@ -0,0 +1,61 @@
+$version = $env:chocolateyPackageVersion
+$url = "https://github.com/ryanoasis/nerd-fonts/releases/download/v$version/CascadiaCode.zip"
+$checksum = "045c12b3302989c291f92d0ba3bfcb6d37f633711fb8f22d822acb923bff0c94"
+$file_path = $env:TEMP + '\CaskaydiaCove.zip'
+$extract_path = $env:TEMP + '\CaskaydiaCove\'
+
+Get-ChocolateyWebFile `
+  -PackageName    $env:chocolateyPackageName `
+  -FileFullPath   $file_path `
+  -Url            $url `
+  -Url64bit       $url `
+  -Checksum       $checksum `
+  -Checksum64     $checksum `
+  -ChecksumType   'sha256' `
+  -ChecksumType64 'sha256'
+
+if (!(Test-Path $file_path)) {
+  throw "Can't download CaskaydiaCove Nerd Font"
+}
+
+Unzip-File -File $file_path -Destination $extract_path
+
+(Get-ChildItem -Path $extract_path).FullName | ForEach-Object {
+  $font_path = Get-Item (Resolve-Path $_)
+  $shell = New-Object -COM Shell.Application
+  $folder = $shell.namespace($font_path.DirectoryName)
+  $item = $folder.Items().Item($font_path.Name)
+  $font_name = $folder.GetDetailsOf($item, 21)
+
+  switch ($font_path.Extension) {
+    ".ttf" { $font_name = "$font_name (TrueType)" }
+    ".otf" { $font_name = "$font_name (OpenType)" }
+  }
+
+  Copy-Item -Path $font_path.FullName -Destination ("${env:windir}\Fonts\$($font_path.Name)") `
+    -Force -ErrorAction SilentlyContinue
+
+  if ((Test-Path "${env:windir}\Fonts\$($font_path.Name)")) {
+    Write-Output "Adding '$font_name' to registry....."
+
+    if ($null -ne (Get-ItemProperty -Name $font_name `
+          -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" `
+          -ErrorAction SilentlyContinue)) {
+      Remove-ItemProperty -Name $font_name `
+        -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -Force
+    }
+
+    New-ItemProperty -Name $font_name `
+      -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" `
+      -PropertyType string -Value $font_path.Name `
+      -Force -ErrorAction SilentlyContinue | Out-Null
+
+    if ((Get-ItemPropertyValue -Name $font_name `
+          -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts") -ne $font_path.Name) {
+      Write-Error "Adding Font to registry failed!"
+    }
+  }
+  else {
+    Write-Error "Unable to copy '$($font_path.Name)' to Windows Fonts folder!"
+  }
+}
--- dcjulian29.settings.windows/tools/chocolateyInstall.ps1
+++ dcjulian29.settings.windows/tools/chocolateyInstall.ps1
@@ -1,59 +1,3 @@
-function installFont($Path) {
-  $Path = Get-Item (Resolve-Path $Path)
-  $oShell = New-Object -COM Shell.Application
-  $folder = $oShell.namespace($Path.DirectoryName)
-  $item = $folder.Items().Item($Path.Name)
-  $fontName = $folder.GetDetailsOf($item, 21)
-
-  switch ($Path.Extension) {
-      ".ttf" { $fontName = "$fontName (TrueType)" }
-      ".otf" { $fontName = "$fontName (OpenType)" }
-  }
-
-  if (-not (Test-Path "${env:windir}\Fonts\$($Path.Name)")) {
-    Copy-Item -Path $Path.FullName -Destination ("${env:windir}\Fonts\$($Path.Name)") `
-      -ErrorAction SilentlyContinue
-
-    if ((Test-Path "${env:windir}\Fonts\$($Path.Name)")) {
-      Write-Output "Adding '$fontName' to registry....."
-
-      if ($null -ne (Get-ItemProperty -Name $fontName `
-          -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" `
-          -ErrorAction SilentlyContinue)) {
-        if ((Get-ItemPropertyValue -Name $fontName `
-            -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts") -eq $Path.Name) {
-          Write-Warning "'$fontName' is already installed."
-        } else {
-          Remove-ItemProperty -Name $fontName `
-            -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -Force
-          New-ItemProperty -Name $fontName `
-            -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" `
-            -PropertyType string -Value $Path.Name `
-            -Force -ErrorAction SilentlyContinue | Out-Null
-          if ((Get-ItemPropertyValue -Name $fontName `
-                -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts") `
-              -ne $Path.Name) {
-            Write-Error "Adding '$fontname' to registry failed!"
-          }
-        }
-      } else {
-        New-ItemProperty -Name $fontName `
-          -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" `
-          -PropertyType string -Value $Path.Name `
-          -Force -ErrorAction SilentlyContinue | Out-Null
-        If ((Get-ItemPropertyValue -Name $fontName `
-            -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts") -ne $Path.Name) {
-          Write-Error "Adding Font to registry failed!"
-        }
-      }
-    } else {
-      Write-Error "Unable to copy '$($Path.Name)' to Windows Fonts folder!"
-    }
-  } else {
-    Write-Warning "'$fontName' is already installed."
-  }
-}
-
 ### Registry Settings
 
 $reg = ""
@@ -69,12 +13,6 @@ Push-Location $PSScriptRoot
 }
 
 Pop-Location
-
-## Fonts
-
-(Get-ChildItem -Path "$PSScriptRoot\fonts").FullName | ForEach-Object {
-  installFont $_
-}
 
 ### Power Settings
 
