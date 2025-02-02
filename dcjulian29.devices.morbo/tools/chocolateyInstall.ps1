7fa58708442c256bc0efa8d7a7e21f4aa8c0f6a5
Julian Easterling <julian@julianscorner.com>
2025-02-02 10:35:58 -0500

    Removed a couple of packages not being used on Morbo

--- dcjulian29.devices.morbo/tools/chocolateyInstall.ps1
+++ dcjulian29.devices.morbo/tools/chocolateyInstall.ps1
@@ -5,11 +5,7 @@ netsh int ipv4 add excludedportrange protocol=tcp startport=8384 numberofports=1
 netsh int ipv4 add excludedportrange protocol=tcp startport=22000 numberofports=1
 netsh int ipv4 add excludedportrange protocol=udp startport=22000 numberofports=1
 
-# ~~~ Go language development stuff...
-
-go install github.com/goreleaser/goreleaser/v2@latest
-go install github.com/spf13/cobra-cli@latest
-go install golang.org/x/lint/golint@latest
+# ~~~ Prevent Windows from going into standby mode or hibernating
 
 Write-Output "standby never, hibernate never..."
 
