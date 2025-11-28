147305624227096ef21fd5a09af2c7d15958b98e
Julian Easterling <julian@julianscorner.com>
2025-11-28 14:02:04 -0500

    Updated Smartgit to 25.1.100

--- dcjulian29.apps.smartgit/tools/chocolateyInstall.ps1
+++ dcjulian29.apps.smartgit/tools/chocolateyInstall.ps1
@@ -2,16 +2,14 @@
 
 $version = $env:chocolateyPackageVersion -replace '\.', '_'
 $packageName = $env:chocolateyPackageName
-$package_zip = "smartgit-win-$version.zip"
-$package_setup = "smartgit-0$version-setup.exe"
-$url = "https://julianscorner.com/dl/$package_zip"
+$url = "https://julianscorner.com/dl/smartgit-$version.zip"
 
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $PSScriptRoot
   fileType       = "exe"
   url64bit       = $url
-  checksum64     = "bb45105b2364a361bf6a2b8bcd36f15394c3368d00ece15342dc9d183c9e7a0b"
+  checksum64     = "7d1dc0fa5e1d0da58f1ae419d6f62a56f84d8dec35bc704f6c55c051da09d9b2"
   checksumType64 = "sha256"
 }
 
@@ -19,7 +17,7 @@ Install-ChocolateyZipPackage @packageArgs
 
 $installArgs = @{
   packageName    = $packageName
-  file           = $(Join-Path -Path $PSScriptRoot -ChildPath $package_setup)
+  file           = $(Join-Path -Path $PSScriptRoot -ChildPath "smartgit-$version-setup.exe")
 
   silentArgs     = "/SILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
   validExitCodes = @(0)
