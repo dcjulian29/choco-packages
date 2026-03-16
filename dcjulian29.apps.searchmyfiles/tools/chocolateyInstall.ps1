b6d38a16e66b15a3023c253cc211963c6a159f74
Julian Easterling <julian@julianscorner.com>
2026-03-16 16:28:00 -0400

    Updated SearchMyFiles to 3.35

--- dcjulian29.apps.searchmyfiles/tools/chocolateyInstall.ps1
+++ dcjulian29.apps.searchmyfiles/tools/chocolateyInstall.ps1
@@ -4,9 +4,9 @@ $installArgs = @{
     SilentArgs     = "/S /EXTRACOMPONENTS=sshdump,udpdump,etwdump,wifidump /DESKTOPICON=no /QUICKLAUNCHICON=no"
     Url            = "https://www.nirsoft.net/utils/searchmyfiles.zip"
     Url64          = "https://www.nirsoft.net/utils/searchmyfiles-x64.zip"
-    checksum       = "b80c3e11a3a800ba9d3af922c7e34e0494197a580a7192308dea3e74646c8096"
+    checksum       = "aa05b725e6c3c32f6c9d35bd55025ce07ae30a2e4bc12d2115f45b2bc8f54a36"
     checksumType   = "sha256"
-    checksum64     = "51545892d6cc83391a3cd0d8bcc77911fee41c4a24fea2b219a6801a62317b16"
+    checksum64     = "f29ea7c66c8ed2af1069ec6209b587fde6205244df9c91cde8aa8e30db97f81a"
     checksum64Type = "sha256"
     UnzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
     ValidExitCodes = @(0, 3010)
