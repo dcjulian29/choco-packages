704e1f26a6c46c0bade3bfd4dcde85c48722f340
Julian Easterling <julian@julianscorner.com>
2026-03-24 15:54:38 -0400

    Make sure the terminal package settings directory exist

--- dcjulian29.settings.terminal/tools/chocolateyInstall.ps1
+++ dcjulian29.settings.terminal/tools/chocolateyInstall.ps1
@@ -1,3 +1,8 @@
-Copy-Item -Path "$PSScriptRoot\settings.json" `
-  -Destination "${env:LOCALAPPDATA}\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" `
+$folder = "${env:LOCALAPPDATA}\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+
+if (-not (Test-Path -Path $folder)) {
+  New-Item -Name $folder -ItemType Directory | Out-Null
+}
+
+Copy-Item -Path "$PSScriptRoot\settings.json" -Destination "$foldwe\settings.json" `
   -Force | Out-Null
